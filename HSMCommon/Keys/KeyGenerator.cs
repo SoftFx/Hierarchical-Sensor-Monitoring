c3da71ae045c2c0c42aeffbb399e79594b9b51a1
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2020-11-17 20:05:40 +0300

    Use SHA-256 instead of base64 convert to create product keys

--- HSMCommon/Keys/KeyGenerator.cs
+++ HSMCommon/Keys/KeyGenerator.cs
@@ -7,8 +7,8 @@ namespace HSMCommon.Keys
     {
         public static string GenerateProductKey(string productName)
         {
-            //return HashComputer.ComputeSha256Hash($"{productName}_{DateTime.Now.ToShortTimeString()}");
-            return Convert.ToBase64String(Encoding.ASCII.GetBytes($"{productName}_{DateTime.Now.ToShortTimeString()}_{DateTime.Now.ToShortDateString()}"));
+            return HashComputer.ComputeSha256Hash($"{productName}_{DateTime.Now.ToLongTimeString()}").Substring(0, 30);
+            //return Convert.ToBase64String(Encoding.ASCII.GetBytes($"{productName}_{DateTime.Now.ToShortTimeString()}_{DateTime.Now.ToShortDateString()}"));
         }
     }
 }
