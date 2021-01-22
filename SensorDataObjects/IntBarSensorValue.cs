fd727dbbbb8700f4e67413d55f85d06c53d99ccd
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-01-22 16:16:46 +0300

    .

--- SensorDataObjects/IntBarSensorValue.cs
+++ SensorDataObjects/IntBarSensorValue.cs
@@ -2,8 +2,6 @@
 {
     public class IntBarSensorValue
     {
-        public int IntValue { get; set; }
-        public string Key { get; set; }
-        public string Path { get; set; }
+        
     }
 }
