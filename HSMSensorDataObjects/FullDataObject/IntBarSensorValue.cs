8cabf31615a7480605a2d85a56d78dcd82777ff7
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-03-02 16:01:44 +0300

    modify bar data objects for more flexibility

--- HSMSensorDataObjects/FullDataObject/IntBarSensorValue.cs
+++ HSMSensorDataObjects/FullDataObject/IntBarSensorValue.cs
@@ -1,18 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using HSMSensorDataObjects.BarData;
 
 namespace HSMSensorDataObjects.FullDataObject
 {
-    public class IntBarSensorValue : SensorValueBase
+    public class IntBarSensorValue : BarSensorValueBase
     {
         public int Min { get; set; }
         public int Max { get; set; }
         public int Mean { get; set; }
         public List<PercentileValueInt> Percentiles { get; set; }
-        public int Count { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
 
         public IntBarSensorValue()
         {
