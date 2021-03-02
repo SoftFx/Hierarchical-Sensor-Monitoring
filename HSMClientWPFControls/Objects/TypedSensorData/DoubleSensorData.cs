564a6ac06ae5c5e8e69cbdab3fff485026449d7b
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-03-02 12:51:43 +0300

    add data objects project reference to client

--- HSMClientWPFControls/Objects/TypedSensorData/DoubleSensorData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HSMClientWPFControls.Objects.TypedSensorData
-{
-    public class DoubleSensorData
-    {
-        public double DoubleValue { get; set; }
-        public string Comment { get; set; }
-    }
-}
--- HSMClientWPFControls/Objects/TypedSensorData/IntSensorData.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HSMClientWPFControls.Objects.TypedSensorData
-{
-    public class IntSensorData
-    {
-        public int IntValue { get; set; }
-        public string Comment { get; set; }
-    }
-}
