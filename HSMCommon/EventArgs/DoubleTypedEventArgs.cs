b7108633bb9d3cbf0dd8f7d272623839293112b0
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-03-03 15:40:07 +0300

    small improvements

---
+++ HSMCommon/EventArgs/DoubleTypedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace HSMCommon.EventArgs
+{
+    public class DoubleTypedEventArgs<T1, T2> : System.EventArgs
+    {
+        public DoubleTypedEventArgs(T1 value1, T2 value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
+        }
+
+        public T2 Value2 { get; }
+
+        public T1 Value1 { get; }
+    }
+}
