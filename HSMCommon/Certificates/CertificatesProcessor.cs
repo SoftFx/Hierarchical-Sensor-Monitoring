b52af9eae5069d2bd39d41ada6166b5dafc29c62
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-03-16 15:37:55 +0300

    increase client sizes, remove unused certificates code

--- HSMCommon/Certificates/CertificatesProcessor.cs
+++ HSMCommon/Certificates/CertificatesProcessor.cs
@@ -49,31 +49,31 @@ namespace HSMCommon.Certificates
 
         //}
 
-        public static Org.BouncyCastle.X509.X509Certificate SignCertificate(Pkcs10CertificationRequest csr,
-            X509Certificate2 caCertificate,
-            string keyFilePath)
-        {
-            var randomGenerator = new CryptoApiRandomGenerator();
-            var random = new SecureRandom(randomGenerator);
-            X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
+        //public static Org.BouncyCastle.X509.X509Certificate SignCertificate(Pkcs10CertificationRequest csr,
+        //    X509Certificate2 caCertificate,
+        //    string keyFilePath)
+        //{
+        //    var randomGenerator = new CryptoApiRandomGenerator();
+        //    var random = new SecureRandom(randomGenerator);
+        //    X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
 
-            BigInteger serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(long.MaxValue),
-                random);
+        //    BigInteger serialNumber = BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(long.MaxValue),
+        //        random);
 
-            certGen.SetSerialNumber(serialNumber);
-            certGen.SetIssuerDN(CreateIssuerName(caCertificate.IssuerName));
-            certGen.SetNotBefore(DateTime.Today.Subtract(new TimeSpan(1,0,0,0)));
-            certGen.SetNotAfter(DateTime.Today.AddDays(5000));
-            certGen.SetSubjectDN(csr.GetCertificationRequestInfo().Subject);
-            certGen.SetPublicKey(csr.GetPublicKey());
+        //    certGen.SetSerialNumber(serialNumber);
+        //    certGen.SetIssuerDN(CreateIssuerName(caCertificate.IssuerName));
+        //    certGen.SetNotBefore(DateTime.Today.Subtract(new TimeSpan(1,0,0,0)));
+        //    certGen.SetNotAfter(DateTime.Today.AddDays(5000));
+        //    certGen.SetSubjectDN(csr.GetCertificationRequestInfo().Subject);
+        //    certGen.SetPublicKey(csr.GetPublicKey());
 
-            AsymmetricKeyParameter privateKey = ImportPrivateKey(keyFilePath);
+        //    AsymmetricKeyParameter privateKey = ImportPrivateKey(keyFilePath);
 
-            ISignatureFactory signatureFactory = new Asn1SignatureFactory("SHA256WITHRSA", privateKey, random);
+        //    ISignatureFactory signatureFactory = new Asn1SignatureFactory("SHA256WITHRSA", privateKey, random);
 
-            Org.BouncyCastle.X509.X509Certificate signedCertificate = certGen.Generate(signatureFactory);
-            return signedCertificate;
-        }
+        //    Org.BouncyCastle.X509.X509Certificate signedCertificate = certGen.Generate(signatureFactory);
+        //    return signedCertificate;
+        //}
 
         public static X509Certificate2 CreateSelfSignedCertificate(CertificateData data)
         {
@@ -89,93 +89,93 @@ namespace HSMCommon.Certificates
             var caCert = certRequest.CreateSelfSigned(DateTimeOffset.Now, expire);
             return caCert;
         }
-        public static X509Certificate2 CreateSelfSignedCertificateWithBouncyCastle(CertificateData certificateData, bool addPrivateKey)
-        {
-            IDictionary attributes = GetNameParametersTable(certificateData);
-            X509Name name = new X509Name(_orderedAttributes, attributes);
+        //public static X509Certificate2 CreateSelfSignedCertificateWithBouncyCastle(CertificateData certificateData, bool addPrivateKey)
+        //{
+        //    IDictionary attributes = GetNameParametersTable(certificateData);
+        //    X509Name name = new X509Name(_orderedAttributes, attributes);
 
-            var kpgen = new RsaKeyPairGenerator();
-            var randomGenerator = new CryptoApiRandomGenerator();
-            var random = new SecureRandom(randomGenerator);
-            AsymmetricCipherKeyPair subjectKeyPair = default(AsymmetricCipherKeyPair);
-            var keyGenerationParameters = new KeyGenerationParameters(random, 2048);
-            kpgen.Init(keyGenerationParameters);
-            subjectKeyPair = kpgen.GenerateKeyPair();
-            AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
-            ISignatureFactory signatureFactory = new Asn1SignatureFactory("SHA256WITHRSA", issuerKeyPair.Private, random);
+        //    var kpgen = new RsaKeyPairGenerator();
+        //    var randomGenerator = new CryptoApiRandomGenerator();
+        //    var random = new SecureRandom(randomGenerator);
+        //    AsymmetricCipherKeyPair subjectKeyPair = default(AsymmetricCipherKeyPair);
+        //    var keyGenerationParameters = new KeyGenerationParameters(random, 2048);
+        //    kpgen.Init(keyGenerationParameters);
+        //    subjectKeyPair = kpgen.GenerateKeyPair();
+        //    AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
+        //    ISignatureFactory signatureFactory = new Asn1SignatureFactory("SHA256WITHRSA", issuerKeyPair.Private, random);
 
-            var cerKp = kpgen.GenerateKeyPair();
+        //    var cerKp = kpgen.GenerateKeyPair();
 
-            X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
+        //    X509V3CertificateGenerator certGen = new X509V3CertificateGenerator();
 
-            certGen.SetSerialNumber(BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(long.MaxValue), random));
-            certGen.SetIssuerDN(name);
-            certGen.SetNotBefore(DateTime.Today.Subtract(new TimeSpan(1, 0, 0, 0)));
-            certGen.SetNotAfter(DateTime.Today.AddDays(5000));
-            certGen.SetSubjectDN(name);
-            certGen.SetPublicKey(cerKp.Public);
-            certGen.AddExtension(X509Extensions.BasicConstraints, true, new BasicConstraints(false));
-            certGen.AddExtension(X509Extensions.AuthorityKeyIdentifier, true,
-                new AuthorityKeyIdentifier(SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(cerKp.Public)));
+        //    certGen.SetSerialNumber(BigIntegers.CreateRandomInRange(BigInteger.One, BigInteger.ValueOf(long.MaxValue), random));
+        //    certGen.SetIssuerDN(name);
+        //    certGen.SetNotBefore(DateTime.Today.Subtract(new TimeSpan(1, 0, 0, 0)));
+        //    certGen.SetNotAfter(DateTime.Today.AddDays(5000));
+        //    certGen.SetSubjectDN(name);
+        //    certGen.SetPublicKey(cerKp.Public);
+        //    certGen.AddExtension(X509Extensions.BasicConstraints, true, new BasicConstraints(false));
+        //    certGen.AddExtension(X509Extensions.AuthorityKeyIdentifier, true,
+        //        new AuthorityKeyIdentifier(SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(cerKp.Public)));
 
-            var certificate = certGen.Generate(signatureFactory);
+        //    var certificate = certGen.Generate(signatureFactory);
 
-            if (addPrivateKey)
-            {
-                return AddPrivateKey(certificate, subjectKeyPair, random);
-            }
+        //    if (addPrivateKey)
+        //    {
+        //        return AddPrivateKey(certificate, subjectKeyPair, random);
+        //    }
 
-            System.Security.Cryptography.X509Certificates.X509Certificate initialCert = DotNetUtilities.ToX509Certificate(certificate);
-            return new X509Certificate2(initialCert);
-        }
+        //    System.Security.Cryptography.X509Certificates.X509Certificate initialCert = DotNetUtilities.ToX509Certificate(certificate);
+        //    return new X509Certificate2(initialCert);
+        //}
 
-        public static X509Certificate2 AddPrivateKey(Org.BouncyCastle.X509.X509Certificate certificate,
-            AsymmetricCipherKeyPair subjectKeyPair, SecureRandom random)
-        {
-            var store = new Pkcs12Store();
-            string friendlyName = certificate.SubjectDN.ToString();
-            X509Certificate2 cert = new X509Certificate2();
-            var certificateEntry = new X509CertificateEntry(certificate);
-            store.SetCertificateEntry(friendlyName, certificateEntry);
-            store.SetKeyEntry(friendlyName, new AsymmetricKeyEntry(subjectKeyPair.Private), new[] { certificateEntry });
-            var stream = new MemoryStream();
-            store.Save(stream, "".ToCharArray(), random);
-            var convertedCertificate = new X509Certificate2(stream.ToArray(), "",
-                X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
-            return convertedCertificate;
-        }
+        //public static X509Certificate2 AddPrivateKey(Org.BouncyCastle.X509.X509Certificate certificate,
+        //    AsymmetricCipherKeyPair subjectKeyPair, SecureRandom random)
+        //{
+        //    var store = new Pkcs12Store();
+        //    string friendlyName = certificate.SubjectDN.ToString();
+        //    X509Certificate2 cert = new X509Certificate2();
+        //    var certificateEntry = new X509CertificateEntry(certificate);
+        //    store.SetCertificateEntry(friendlyName, certificateEntry);
+        //    store.SetKeyEntry(friendlyName, new AsymmetricKeyEntry(subjectKeyPair.Private), new[] { certificateEntry });
+        //    var stream = new MemoryStream();
+        //    store.Save(stream, "".ToCharArray(), random);
+        //    var convertedCertificate = new X509Certificate2(stream.ToArray(), "",
+        //        X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
+        //    return convertedCertificate;
+        //}
 
-        public static X509Certificate2 AddPrivateKey(Org.BouncyCastle.X509.X509Certificate certificate,
-            AsymmetricCipherKeyPair subjectKeyPair)
-        {
-            var randomGenerator = new CryptoApiRandomGenerator();
-            var random = new SecureRandom(randomGenerator);
-            return AddPrivateKey(certificate, subjectKeyPair, random);
-        }
-        public static Pkcs10CertificationRequest CreateCertificateSignRequest(CertificateData certificateData, out AsymmetricCipherKeyPair subjectKP)
-        {
-            IDictionary attributes = GetNameParametersTable(certificateData);
+        //public static X509Certificate2 AddPrivateKey(Org.BouncyCastle.X509.X509Certificate certificate,
+        //    AsymmetricCipherKeyPair subjectKeyPair)
+        //{
+        //    var randomGenerator = new CryptoApiRandomGenerator();
+        //    var random = new SecureRandom(randomGenerator);
+        //    return AddPrivateKey(certificate, subjectKeyPair, random);
+        //}
+        //public static Pkcs10CertificationRequest CreateCertificateSignRequest(CertificateData certificateData, out AsymmetricCipherKeyPair subjectKP)
+        //{
+        //    IDictionary attributes = GetNameParametersTable(certificateData);
 
-            var kpgen = new RsaKeyPairGenerator();
+        //    var kpgen = new RsaKeyPairGenerator();
 
-            var randomGenerator = new CryptoApiRandomGenerator();
-            var random = new SecureRandom(randomGenerator);
-            var keyGenerationParameters = new KeyGenerationParameters(random, 2048);
+        //    var randomGenerator = new CryptoApiRandomGenerator();
+        //    var random = new SecureRandom(randomGenerator);
+        //    var keyGenerationParameters = new KeyGenerationParameters(random, 2048);
 
-            kpgen.Init(keyGenerationParameters);
-            AsymmetricCipherKeyPair subjectKeyPair = default(AsymmetricCipherKeyPair);
-            subjectKeyPair = kpgen.GenerateKeyPair();
-            AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
-            ISignatureFactory signatureFactory = new Asn1SignatureFactory("SHA256WITHRSA", issuerKeyPair.Private, random);
+        //    kpgen.Init(keyGenerationParameters);
+        //    AsymmetricCipherKeyPair subjectKeyPair = default(AsymmetricCipherKeyPair);
+        //    subjectKeyPair = kpgen.GenerateKeyPair();
+        //    AsymmetricCipherKeyPair issuerKeyPair = subjectKeyPair;
+        //    ISignatureFactory signatureFactory = new Asn1SignatureFactory("SHA256WITHRSA", issuerKeyPair.Private, random);
 
-            X509Name subject = new X509Name(new ArrayList(attributes.Keys), attributes);
+        //    X509Name subject = new X509Name(new ArrayList(attributes.Keys), attributes);
 
-            Pkcs10CertificationRequest result = new Pkcs10CertificationRequest(signatureFactory, subject,
-                subjectKeyPair.Public, null);
+        //    Pkcs10CertificationRequest result = new Pkcs10CertificationRequest(signatureFactory, subject,
+        //        subjectKeyPair.Public, null);
 
-            subjectKP = subjectKeyPair;
-            return result;
-        }
+        //    subjectKP = subjectKeyPair;
+        //    return result;
+        //}
 
         public static X509Certificate2 CreateAndSignCertificate(string subject, RSA key, X509Certificate2 caCertificate)
         {
@@ -226,25 +226,25 @@ namespace HSMCommon.Certificates
             }
         }
 
-        public static X509Certificate2 ReadCertificate(string crtFilePath, string keyFilePath)
-        {
-            X509Certificate2 cert = new X509Certificate2(crtFilePath, "",
-                X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
+        //public static X509Certificate2 ReadCertificate(string crtFilePath, string keyFilePath)
+        //{
+        //    X509Certificate2 cert = new X509Certificate2(crtFilePath, "",
+        //        X509KeyStorageFlags.Exportable | X509KeyStorageFlags.MachineKeySet | X509KeyStorageFlags.PersistKeySet);
 
-            AsymmetricKeyParameter keyParam = ImportPrivateKey(keyFilePath);
-            var store = new Pkcs12Store();
-            string friendlyName = cert.Subject.ToString();
-            var entry = new X509CertificateEntry(Org.BouncyCastle.Security.DotNetUtilities.FromX509Certificate(cert));
-            store.SetCertificateEntry(friendlyName, entry);
-            store.SetKeyEntry(friendlyName, new AsymmetricKeyEntry(keyParam),new []{entry} );
+        //    AsymmetricKeyParameter keyParam = ImportPrivateKey(keyFilePath);
+        //    var store = new Pkcs12Store();
+        //    string friendlyName = cert.Subject.ToString();
+        //    var entry = new X509CertificateEntry(Org.BouncyCastle.Security.DotNetUtilities.FromX509Certificate(cert));
+        //    store.SetCertificateEntry(friendlyName, entry);
+        //    store.SetKeyEntry(friendlyName, new AsymmetricKeyEntry(keyParam),new []{entry} );
 
-            var stream = new MemoryStream();
-            store.Save(stream, "".ToArray(), new SecureRandom());
+        //    var stream = new MemoryStream();
+        //    store.Save(stream, "".ToArray(), new SecureRandom());
 
-            var convertedCertificate = new X509Certificate2(stream.ToArray(), "", X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
+        //    var convertedCertificate = new X509Certificate2(stream.ToArray(), "", X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
 
-            return convertedCertificate;
-        }
+        //    return convertedCertificate;
+        //}
 
         #region Store methods
 
@@ -326,25 +326,25 @@ namespace HSMCommon.Certificates
             return new X509Name(orderedList, attributes);
         }
 
-        private static AsymmetricKeyParameter ImportPrivateKey(string filePath)
-        {
-            StreamReader streamReader = new StreamReader(filePath);
-            PemReader pemReader = new PemReader(streamReader);
-            var keyPair = pemReader.ReadObject();
+        //private static AsymmetricKeyParameter ImportPrivateKey(string filePath)
+        //{
+        //    StreamReader streamReader = new StreamReader(filePath);
+        //    PemReader pemReader = new PemReader(streamReader);
+        //    var keyPair = pemReader.ReadObject();
 
-            AsymmetricKeyParameter keyParameter;
-            AsymmetricCipherKeyPair cipherKeyPair = keyPair as AsymmetricCipherKeyPair;
-            if (cipherKeyPair != null)
-            {
-                keyParameter = cipherKeyPair.Private;
-            }
-            else
-            {
-                keyParameter = keyPair as AsymmetricKeyParameter;
-            }
+        //    AsymmetricKeyParameter keyParameter;
+        //    AsymmetricCipherKeyPair cipherKeyPair = keyPair as AsymmetricCipherKeyPair;
+        //    if (cipherKeyPair != null)
+        //    {
+        //        keyParameter = cipherKeyPair.Private;
+        //    }
+        //    else
+        //    {
+        //        keyParameter = keyPair as AsymmetricKeyParameter;
+        //    }
 
-            return keyParameter;
-        }
+        //    return keyParameter;
+        //}
 
         public static string GetSubjectString(CertificateData data)
         {
