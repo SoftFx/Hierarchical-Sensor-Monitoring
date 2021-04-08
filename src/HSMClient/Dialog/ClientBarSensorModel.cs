8e5e9e613b77b78d237c378a328f1a4dcaf16258
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-04-08 12:09:56 +0300

    use old version of library for boxplots

--- src/HSMClient/Dialog/ClientBarSensorModel.cs
+++ src/HSMClient/Dialog/ClientBarSensorModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -11,6 +12,7 @@ using HSMClientWPFControls.Model.SensorDialog;
 using HSMClientWPFControls.Objects;
 using HSMClientWPFControls.ViewModel;
 using HSMSensorDataObjects.TypedDataObject;
+using LiveCharts.Defaults;
 using OxyPlot.Axes;
 using OxyPlot.Series;
 
@@ -19,9 +21,13 @@ namespace HSMClient.Dialog
     class ClientBarSensorModel : ClientDialogTimerModel, IBarSensorModel
     {
         private Collection<BoxPlotItem> _items;
+        private Collection<OhlcPoint> _points;
+        private Collection<string> _labels;
         public ClientBarSensorModel(ISensorHistoryConnector connector, MonitoringSensorViewModel sensor) : base(connector, sensor)
         {
             Items = new Collection<BoxPlotItem>();
+            Points = new Collection<OhlcPoint>();
+            Labels = new Collection<string>();
             Count = 10;
             Title = _path;
         }
@@ -37,6 +43,82 @@ namespace HSMClient.Dialog
                 OnPropertyChanged(nameof(Items));
             }
         }
+
+        public Collection<OhlcPoint> Points
+        {
+            get => _points;
+            set
+            {
+                _points = value;
+                OnPropertyChanged(nameof(Points));
+            }
+        }
+
+        public Collection<string> Labels
+        {
+            get => _labels;
+            set
+            {
+                _labels = value;
+                OnPropertyChanged(nameof(Labels));
+            }
+        }
+        //protected override void OnTimerTick()
+        //{
+        //    List<SensorHistoryItem> list = _connector.GetSensorHistory(_product, _path, _name, Count);
+        //    if (list.Count < 1)
+        //        return;
+
+        //    list.Reverse();
+        //    var type = list[0].Type;
+        //    Collection<OhlcPoint> points = new Collection<OhlcPoint>();
+        //    Collection<string> labels = new Collection<string>();
+        //    switch (type)
+        //    {
+        //        case SensorTypes.BarIntSensor:
+        //        {
+        //            var serializedList = list.Select(l => JsonSerializer.Deserialize<IntBarSensorData>(l.SensorValue))
+        //                .ToList();
+        //            List<IntBarSensorData> finalList = new List<IntBarSensorData>();
+        //            for (int i = 0; i < serializedList.Count; ++i)
+        //            {
+        //                if (i < serializedList.Count - 1)
+        //                {
+        //                    if (serializedList[i].StartTime != serializedList[i + 1].StartTime)
+        //                    {
+        //                        finalList.Add(serializedList[i]);
+        //                        continue;
+        //                    }
+
+        //                    if (serializedList[i].EndTime >= serializedList[i + 1].EndTime)
+        //                    {
+        //                        finalList.Add(serializedList[i]);
+        //                        ++i;
+        //                    }
+        //                    else
+        //                    {
+        //                        finalList.Add(serializedList[i + 1]);
+        //                        i += 2;
+        //                    }
+
+        //                    break;
+        //                }
+        //            }
+
+        //            foreach (var item in finalList)
+        //            {
+        //                int firstQuant = item.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.25) < double.Epsilon).Value;
+        //                int thirdQuant = item.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.75) < double.Epsilon).Value; 
+        //                points.Add(new OhlcPoint(thirdQuant, item.Max, thirdQuant, item.Min));
+        //                labels.Add($"{item.StartTime:T}");
+        //            }
+        //            break;
+        //        }
+        //    }
+
+        //    Points = points;
+        //    Labels = labels;
+        //}
         protected override void OnTimerTick()
         {
             List<SensorHistoryItem> list = _connector.GetSensorHistory(_product, _path, _name, Count);
@@ -50,68 +132,69 @@ namespace HSMClient.Dialog
             switch (type)
             {
                 case SensorTypes.BarIntSensor:
-                {
-                    var serializedList = list.Select(l => JsonSerializer.Deserialize<IntBarSensorData>(l.SensorValue)).ToList();
-                    List<IntBarSensorData> finalList = new List<IntBarSensorData>();
-                    for (int i = 0; i < serializedList.Count; ++i)
                     {
-                        if (i < serializedList.Count - 1)
+                        var serializedList = list.Select(l => JsonSerializer.Deserialize<IntBarSensorData>(l.SensorValue)).ToList();
+                        List<IntBarSensorData> finalList = new List<IntBarSensorData>();
+                        for (int i = 0; i < serializedList.Count; ++i)
                         {
-                            if (serializedList[i].StartTime != serializedList[i + 1].StartTime)
+                            if (i < serializedList.Count - 1)
                             {
-                                finalList.Add(serializedList[i]);
-                                continue;
-                            }
+                                if (serializedList[i].StartTime != serializedList[i + 1].StartTime)
+                                {
+                                    finalList.Add(serializedList[i]);
+                                    continue;
+                                }
 
-                            if (serializedList[i].EndTime >= serializedList[i + 1].EndTime)
-                            {
-                                finalList.Add(serializedList[i]);
-                                ++i;
-                            }
-                            else
-                            {
-                                finalList.Add(serializedList[i + 1]);
-                                i += 2;
+                                if (serializedList[i].EndTime >= serializedList[i + 1].EndTime)
+                                {
+                                    finalList.Add(serializedList[i]);
+                                    ++i;
+                                }
+                                else
+                                {
+                                    finalList.Add(serializedList[i + 1]);
+                                    i += 2;
+                                }
                             }
                         }
+                        foreach (var boxValue in finalList)
+                        {
+                            int firstQuant = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.25) < double.Epsilon).Value;
+                            int thirdQuant = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.75) < double.Epsilon).Value;
+                            int median = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.5) < double.Epsilon).Value;
+                            double lowerWhisker = firstQuant - (1.5 * (thirdQuant - firstQuant));
+                            double upperWhisker = thirdQuant + (1.5 * (thirdQuant - firstQuant));
+
+                            //DateTime time = new DateTime(1970, 1, 1).AddMilliseconds(boxValue.StartTime.ToUniversalTime().ToTimestamp().Seconds);
+                            double x = DateTimeAxis.ToDouble(boxValue.StartTime);
+                            BoxPlotItem plotItem = new BoxPlotItem(x, lowerWhisker, firstQuant, median,
+                                thirdQuant, upperWhisker);
+                            plotItem.Mean = boxValue.Mean;
+
+                            boxes.Add(plotItem);
+                        }
+
+                        break;
                     }
-                    foreach (var boxValue in finalList)
-                    {
-                        int firstQuant = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.25) < double.Epsilon).Value;
-                        int thirdQuant = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.75) < double.Epsilon).Value;
-                        int median = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.5) < double.Epsilon).Value;
-                        double lowerWhisker = firstQuant - (1.5 * (thirdQuant - firstQuant));
-                        double upperWhisker = thirdQuant + (1.5 * (thirdQuant - firstQuant));
-
-                        //DateTime time = new DateTime(1970, 1, 1).AddMilliseconds(boxValue.StartTime.ToUniversalTime().ToTimestamp().Seconds);
-                        double x = DateTimeAxis.ToDouble(boxValue.StartTime);
-                        BoxPlotItem plotItem = new BoxPlotItem(x, lowerWhisker, firstQuant, median,
-                            thirdQuant, upperWhisker);
-
-                        boxes.Add(plotItem);
-                    }
-
-                    break;
-                }
                 case SensorTypes.BarDoubleSensor:
-                {
-                    foreach (var box in list)
                     {
-                        var boxValue = JsonSerializer.Deserialize<IntBarSensorData>(box.SensorValue);
+                        foreach (var box in list)
+                        {
+                            var boxValue = JsonSerializer.Deserialize<IntBarSensorData>(box.SensorValue);
 
-                        double firstQuant = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.25) < double.Epsilon).Value;
-                        double thirdQuant = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.75) < double.Epsilon).Value;
-                        double median = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.5) < double.Epsilon).Value;
-                        double lowerWhisker = firstQuant - (int)(1.5 * (thirdQuant - firstQuant));
-                        double upperWhisker = thirdQuant - (int)(1.5 * (thirdQuant - firstQuant));
+                            double firstQuant = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.25) < double.Epsilon).Value;
+                            double thirdQuant = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.75) < double.Epsilon).Value;
+                            double median = boxValue.Percentiles.FirstOrDefault(p => Math.Abs(p.Percentile - 0.5) < double.Epsilon).Value;
+                            double lowerWhisker = firstQuant - (int)(1.5 * (thirdQuant - firstQuant));
+                            double upperWhisker = thirdQuant - (int)(1.5 * (thirdQuant - firstQuant));
 
-                        BoxPlotItem plotItem = new BoxPlotItem(DateTimeAxis.ToDouble(box.Time), lowerWhisker, firstQuant, median,
-                            thirdQuant, upperWhisker);
+                            BoxPlotItem plotItem = new BoxPlotItem(DateTimeAxis.ToDouble(box.Time), lowerWhisker, firstQuant, median,
+                                thirdQuant, upperWhisker);
 
-                        boxes.Add(plotItem);
-                    }
+                            boxes.Add(plotItem);
+                        }
 
-                    break;
+                        break;
                     }
             }
 
--- src/HSMClientWPFControls/View/SensorDialog/BarSensorView.xaml.cs
+++ src/HSMClientWPFControls/View/SensorDialog/BarSensorView.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,8 +13,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HSMClientWPFControls.Annotations;
 using HSMClientWPFControls.Model.SensorDialog;
 using HSMClientWPFControls.ViewModel.SensorDialog;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 
 namespace HSMClientWPFControls.View.SensorDialog
 {
@@ -20,6 +27,8 @@ namespace HSMClientWPFControls.View.SensorDialog
     /// </summary>
     public partial class BarSensorView : SensorControl
     {
+        public SeriesCollection Series;
+        private BarSensorViewModel _vm;
         public BarSensorView()
         {
             InitializeComponent();
@@ -27,9 +36,58 @@ namespace HSMClientWPFControls.View.SensorDialog
             //BoxPlotSeries.Width = 0.0005;
         }
 
+        private void ViewModel_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Points" || e.PropertyName == "Labels")
+            {
+                UpdateView();
+            }
+        }
+
+        private void UpdateView()
+        {
+            if (_vm == null)
+                return;
+
+            if (!Dispatcher.CheckAccess())
+            {
+                Dispatcher.Invoke(UpdateView);
+                return;
+            }
+
+            Series = new SeriesCollection()
+            {
+                new CandleSeries
+                {
+                    Values = new ChartValues<OhlcPoint>(_vm.Points)
+                }
+            };
+
+            //Chart.Series = Series;
+        }
+
+        private void InitializeSeries(Collection<OhlcPoint> points)
+        {
+            if (!Dispatcher.CheckAccess())
+            {
+                Dispatcher.Invoke(new Action<Collection<OhlcPoint>>(InitializeSeries), points);
+                return;
+            }
+
+            Series = new SeriesCollection()
+            {
+                new CandleSeries
+                {
+                    Values = new ChartValues<OhlcPoint>(points)
+                }
+            };
+        }
         public override DialogViewModel ConstructDefaultViewModel(ISensorDialogModel model)
         {
-            return new BarSensorViewModel(model);
+            var viewModel = new BarSensorViewModel(model);
+            //_vm = viewModel;
+            //_vm.PropertyChanged += ViewModel_PropertyChanged;
+            return viewModel;
         }
     }
 }
