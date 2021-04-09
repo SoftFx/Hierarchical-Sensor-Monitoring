6cf0d12298b724ab53a365b26469fb11d5d030b0
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-04-09 17:17:53 +0300

    add one more abstraction

---
+++ src/HSMServer/Products/IProductManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using HSMServer.DataLayer.Model;
+
+namespace HSMServer.Products
+{
+    public interface IProductManager
+    {
+        List<Product> Products { get; }
+        void RemoveProduct(string name);
+        void AddProduct(string name);
+        string GetProductNameByKey(string key);
+        Product GetProductByName(string name);
+        List<string> GetProductSensors(string productName);
+        bool IsSensorRegistered(string productName, string path);
+        void AddSensor(string productName, string path);
+        void AddSensorIfNotRegistered(string productName, string path);
+    }
+}
