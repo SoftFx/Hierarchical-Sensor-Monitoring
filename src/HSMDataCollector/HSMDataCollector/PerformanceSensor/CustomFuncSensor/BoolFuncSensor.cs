9c4e97f7bbca5d1244df6fefcfc538010ed3e640
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-04-16 17:51:56 +0300

    alive sensor created

---
+++ src/HSMDataCollector/HSMDataCollector/PerformanceSensor/CustomFuncSensor/BoolFuncSensor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using HSMDataCollector.Core;
+using HSMSensorDataObjects;
+using HSMSensorDataObjects.FullDataObject;
+using Newtonsoft.Json;
+
+namespace HSMDataCollector.PerformanceSensor.CustomFuncSensor
+{
+    internal class BoolFuncSensor : FuncSensorBase<bool>
+    {
+        public BoolFuncSensor(Func<bool> function, string path, string productKey, IValuesQueue queue, int timeout = 150000) : base(function, path, productKey, queue, timeout)
+        {
+        }
+
+        protected override byte[] GetBytesData(SensorValueBase data)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetStringData(SensorValueBase data)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override CommonSensorValue ConvertValue(bool value)
+        {
+            CommonSensorValue result = new CommonSensorValue();
+            BoolSensorValue boolValue = new BoolSensorValue() {BoolValue = value, Path = Path, Key = ProductKey, Time = DateTime.Now};
+            result.TypedValue = JsonConvert.SerializeObject(boolValue);
+            result.SensorType = SensorType.BooleanSensor;
+            return result;
+        }
+    }
+}
