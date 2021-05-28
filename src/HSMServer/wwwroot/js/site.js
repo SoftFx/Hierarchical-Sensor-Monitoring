7a8485bb24cd1cd149d42e492c6a85e4643f9999
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-05-28 13:17:04 +0300

    apply some styles for login form

--- src/HSMServer/wwwroot/js/site.js
+++ src/HSMServer/wwwroot/js/site.js
@@ -2,28 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-function initializeTree() {
-        $('#jstree').jstree();
-
-        $('#updateTime').empty();
-        $('#updateTime').append('Update Time: ' + new Date().toUTCString());
-
-        $('#jstree').on('activate_node.jstree', function (e, data) {
-            if (data == undefined || data.node == undefined || data.node.id == undefined)
-                return;
-            //alert('clicked node: ' + data.node.id);
-
-            $('[id^="list_"]').css("display", "none"); //start with
-
-            if (document.getElementById('list_' + data.node.id) == null)
-                $('#list_sensors_header').css('display', 'none');
-            else
-                $('#list_sensors_header').css('display', 'block');
-
-            $('#list_' + data.node.id).css('display', 'block');
-        });
-}
-
-
-
