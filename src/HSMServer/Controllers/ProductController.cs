c998506849324cf32a400590ee4ba1158117e3d7
Aliaksandra Bachura <aliaksandra.bachura@soft-fx.lv>
2021-07-05 13:47:13 +0300

    Error page redesigned.

--- src/HSMServer/Controllers/ProductController.cs
+++ src/HSMServer/Controllers/ProductController.cs
@@ -49,7 +49,6 @@ namespace HSMServer.Controllers
 
         public IActionResult EditProduct([FromQuery(Name = "Product")] string productKey)
         {
-            throw new Exception(";slmfs'fksdf");
             var product = _monitoringCore.GetProduct(productKey);
             var users = _userManager.GetViewers(productKey);
 
