9815b60b49a6ca415a0a2545953710cc752e0e15
Aliaksei Dashko <aliaksei.dashko@soft-fx.lv>
2021-07-14 11:40:13 +0300

    HSMCppWrapper public interface import bug fix

--- src/HSMCppWrapper/include/HSMCppWrapper.h
+++ src/HSMCppWrapper/include/HSMCppWrapper.h
@@ -1,10 +1,9 @@
 #pragma once
 
+#define HSMWRAPPER_API __declspec(dllimport)
+
 #include "DataCollector.h"
 #include "SensorStatus.h"
 #include "HSMSensor.h"
 #include "HSMBarSensor.h"
 #include "HSMLastValueSensor.h"
-
-
-
