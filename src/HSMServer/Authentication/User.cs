c27b6086338b36c459626a60163fe5ed08a873a8
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-07-21 14:51:37 +0300

    fix possible serializationError

--- src/HSMServer/Authentication/User.cs
+++ src/HSMServer/Authentication/User.cs
@@ -8,28 +8,28 @@ namespace HSMServer.Authentication
 {
     public class User : ClaimsPrincipal
     {
-        public Guid Id { get; }
+        public Guid Id { get; set; }
         public bool IsAdmin { get; set; }
-        public string UserName { get; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public string CertificateThumbprint { get; set; }
         public string CertificateFileName { get; set; }
         public List<KeyValuePair<string, ProductRoleEnum>> ProductsRoles { get; set; }
         public User(string userName) : this()
         {
+            Id = Guid.NewGuid();
             UserName = userName;
         }
         public User()
         {
-            Id = Guid.NewGuid();
             ProductsRoles = new List<KeyValuePair<string, ProductRoleEnum>>();
         }
 
-        public User(string userName, string thumbprint) : this()
-        {
-            UserName = userName;
-            CertificateThumbprint = thumbprint;
-        }
+        //public User(string userName, string thumbprint) : this()
+        //{
+        //    UserName = userName;
+        //    CertificateThumbprint = thumbprint;
+        //}
 
         public User(User user)
         {
