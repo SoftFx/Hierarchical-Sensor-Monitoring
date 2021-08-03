ca1369a374e4fd092bdfb868fcd308d219515441
Aliaksandra Bachura <aliaksandra.bachura@soft-fx.lv>
2021-08-03 15:18:39 +0300

    Refactoring + bug fixing.

--- src/HSMServer/wwwroot/js/home/tree.js
+++ src/HSMServer/wwwroot/js/home/tree.js
@@ -14,18 +14,3 @@ function initializeClickTree() {
         displayList(data);
     });
 }
-
-function displayList(data) {
-    $('[id^="list_"]').css("display", "none");
-
-    if (document.getElementById('list_' + data.node.id) == null) {
-        $('#list_sensors_header').css('display', 'none');
-        $('#noData').css('display', 'block');
-    }
-    else {
-        $('#list_sensors_header').css('display', 'block');
-        $('#noData').css('display', 'none');
-    }
-
-    $('#list_' + data.node.id).css('display', 'block');
-}
