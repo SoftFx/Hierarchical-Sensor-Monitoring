0f84c34efae8652a22a4e68803d3ce100c4c67ab
vitali.vasilevich <vitali.vasilevich@soft-fx.lv>
2021-10-19 19:58:19 +0300

    Change the tree for a user when the user's permissions change

---
+++ src/HSMServer.Core/Authentication/UserObserver/IUserObserver.cs
@@ -0,0 +1,12 @@
+﻿using HSMServer.Core.Model.Authentication;
+
+namespace HSMServer.Core.Authentication.UserObserver
+{
+    /// <summary>
+    /// Observer interface for the Observer design pattern
+    /// </summary>
+    public interface IUserObserver
+    {
+        void UserUpdated(User user);
+    }
+}
--- src/HSMServer/Controllers/AccountController.cs
+++ src/HSMServer/Controllers/AccountController.cs
@@ -10,6 +10,7 @@ using HSMServer.Core.Registration;
 using HSMServer.Filters;
 using HSMServer.Model.Validators;
 using HSMServer.Model.ViewModel;
+using HSMServer.SignalR;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
@@ -19,7 +20,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using HSMServer.SignalR;
 
 namespace HSMServer.Controllers
 {
