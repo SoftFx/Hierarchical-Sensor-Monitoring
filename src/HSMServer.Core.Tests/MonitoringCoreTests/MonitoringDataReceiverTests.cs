5373192b98040348a775f453231c91abf65ac4c1
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2021-11-26 17:06:53 +0300

    Added tests for Add double, string, intBar, doubleBar, file, sileBytes sensors values

--- src/HSMServer.Core.Tests/MonitoringCoreTests/MonitoringDataReceiverTests.cs
+++ src/HSMServer.Core.Tests/MonitoringCoreTests/MonitoringDataReceiverTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HSMSensorDataObjects.BarData;
 using HSMSensorDataObjects.FullDataObject;
 using HSMServer.Core.Authentication;
 using HSMServer.Core.Cache;
@@ -117,6 +118,198 @@ namespace HSMServer.Core.Tests
 
             var sensorInfoFromDB = _databaseFixture.DatabaseAdapter.GetSensorInfo(_databaseFixture.TestProduct.Name, intSensorValue.Path);
             SensorValuesTester.TestSensorInfoFromDB(intSensorValue, sensorInfoFromDB);
+        }
+
+        [Fact]
+        public async void AddDoubleSensorValueTest()
+        {
+            var doubleSensorValue = new DoubleSensorValue()
+            {
+                DoubleValue = 123.123,
+                Key = _databaseFixture.TestProduct.Key,
+                Path = nameof(DoubleSensorValue),
+                Description = $"{nameof(DoubleSensorValue)} {nameof(DoubleSensorValue.Description)}",
+                Comment = $"{nameof(DoubleSensorValue)} {nameof(DoubleSensorValue.Comment)}",
+                Time = DateTime.UtcNow,
+            };
+
+            _monitoringCore.AddSensorValue(doubleSensorValue);
+
+            await Task.Delay(100);
+
+            var sensorDataFromCache =
+                _valuesCache.GetValues(new List<string>(1) { _databaseFixture.TestProduct.Name })
+                            ?.FirstOrDefault(s => s.SensorType == HSMSensorDataObjects.SensorType.DoubleSensor);
+            SensorValuesTester.TestSensorDataFromCache(doubleSensorValue, sensorDataFromCache);
+
+            var sensorHistoryDataFromDB = _databaseFixture.DatabaseAdapter.GetOneValueSensorValue(_databaseFixture.TestProduct.Name, doubleSensorValue.Path);
+            SensorValuesTester.TestSensorHistoryDataFromDB(doubleSensorValue, sensorHistoryDataFromDB);
+
+            var sensorInfoFromDB = _databaseFixture.DatabaseAdapter.GetSensorInfo(_databaseFixture.TestProduct.Name, doubleSensorValue.Path);
+            SensorValuesTester.TestSensorInfoFromDB(doubleSensorValue, sensorInfoFromDB);
+        }
+
+        [Fact]
+        public async void AddStringSensorValueTest()
+        {
+            var stringSensorValue = new StringSensorValue()
+            {
+                StringValue = nameof(StringSensorValue.StringValue),
+                Key = _databaseFixture.TestProduct.Key,
+                Path = nameof(StringSensorValue),
+                Description = $"{nameof(StringSensorValue)} {nameof(StringSensorValue.Description)}",
+                Comment = $"{nameof(StringSensorValue)} {nameof(StringSensorValue.Comment)}",
+                Time = DateTime.UtcNow,
+            };
+
+            _monitoringCore.AddSensorValue(stringSensorValue);
+
+            await Task.Delay(100);
+
+            var sensorDataFromCache =
+                _valuesCache.GetValues(new List<string>(1) { _databaseFixture.TestProduct.Name })
+                            ?.FirstOrDefault(s => s.SensorType == HSMSensorDataObjects.SensorType.StringSensor);
+            SensorValuesTester.TestSensorDataFromCache(stringSensorValue, sensorDataFromCache);
+
+            var sensorHistoryDataFromDB = _databaseFixture.DatabaseAdapter.GetOneValueSensorValue(_databaseFixture.TestProduct.Name, stringSensorValue.Path);
+            SensorValuesTester.TestSensorHistoryDataFromDB(stringSensorValue, sensorHistoryDataFromDB);
+
+            var sensorInfoFromDB = _databaseFixture.DatabaseAdapter.GetSensorInfo(_databaseFixture.TestProduct.Name, stringSensorValue.Path);
+            SensorValuesTester.TestSensorInfoFromDB(stringSensorValue, sensorInfoFromDB);
+        }
+
+        [Fact]
+        public async void AddIntBarSensorValueTest()
+        {
+            var intBarSensorValue = new IntBarSensorValue()
+            {
+                StartTime = DateTime.UtcNow.AddSeconds(-10),
+                EndTime = DateTime.UtcNow.AddSeconds(10),
+                Count = 10,
+                LastValue = 3,
+                Min = -3,
+                Max = 7,
+                Mean = 1,
+                Percentiles = new List<PercentileValueInt>(2) { new PercentileValueInt() { Percentile = 73.69, Value = 23 }, new PercentileValueInt() { Percentile = 6, Value = 7 } },
+                Key = _databaseFixture.TestProduct.Key,
+                Path = nameof(IntBarSensorValue),
+                Description = $"{nameof(IntBarSensorValue)} {nameof(IntBarSensorValue.Description)}",
+                Comment = $"{nameof(IntBarSensorValue)} {nameof(IntBarSensorValue.Comment)}",
+                Time = DateTime.UtcNow,
+            };
+
+            _monitoringCore.AddSensorValue(intBarSensorValue);
+
+            await Task.Delay(100);
+
+            var sensorDataFromCache =
+                _valuesCache.GetValues(new List<string>(1) { _databaseFixture.TestProduct.Name })
+                            ?.FirstOrDefault(s => s.SensorType == HSMSensorDataObjects.SensorType.IntegerBarSensor);
+            SensorValuesTester.TestSensorDataFromCache(intBarSensorValue, sensorDataFromCache);
+
+            var sensorHistoryDataFromDB = _databaseFixture.DatabaseAdapter.GetOneValueSensorValue(_databaseFixture.TestProduct.Name, intBarSensorValue.Path);
+            SensorValuesTester.TestSensorHistoryDataFromDB(intBarSensorValue, sensorHistoryDataFromDB);
+
+            var sensorInfoFromDB = _databaseFixture.DatabaseAdapter.GetSensorInfo(_databaseFixture.TestProduct.Name, intBarSensorValue.Path);
+            SensorValuesTester.TestSensorInfoFromDB(intBarSensorValue, sensorInfoFromDB);
+        }
+
+        [Fact]
+        public async void AddDoubleBarSensorValueTest()
+        {
+            var doubleBarSensorValue = new DoubleBarSensorValue()
+            {
+                StartTime = DateTime.UtcNow.AddSeconds(-10),
+                EndTime = DateTime.UtcNow.AddSeconds(10),
+                Count = 10,
+                LastValue = 3.01,
+                Min = -3.45,
+                Max = 7.33,
+                Mean = 1.09,
+                Percentiles = new List<PercentileValueDouble>(2) { new PercentileValueDouble() { Percentile = 123.123, Value = 23.23 }, new PercentileValueDouble() { Percentile = 2, Value = 5 } },
+                Key = _databaseFixture.TestProduct.Key,
+                Path = nameof(DoubleBarSensorValue),
+                Description = $"{nameof(DoubleBarSensorValue)} {nameof(DoubleBarSensorValue.Description)}",
+                Comment = $"{nameof(DoubleBarSensorValue)} {nameof(DoubleBarSensorValue.Comment)}",
+                Time = DateTime.UtcNow,
+            };
+
+            _monitoringCore.AddSensorValue(doubleBarSensorValue);
+
+            await Task.Delay(100);
+
+            var sensorDataFromCache =
+                _valuesCache.GetValues(new List<string>(1) { _databaseFixture.TestProduct.Name })
+                            ?.FirstOrDefault(s => s.SensorType == HSMSensorDataObjects.SensorType.DoubleBarSensor);
+            SensorValuesTester.TestSensorDataFromCache(doubleBarSensorValue, sensorDataFromCache);
+
+            var sensorHistoryDataFromDB = _databaseFixture.DatabaseAdapter.GetOneValueSensorValue(_databaseFixture.TestProduct.Name, doubleBarSensorValue.Path);
+            SensorValuesTester.TestSensorHistoryDataFromDB(doubleBarSensorValue, sensorHistoryDataFromDB);
+
+            var sensorInfoFromDB = _databaseFixture.DatabaseAdapter.GetSensorInfo(_databaseFixture.TestProduct.Name, doubleBarSensorValue.Path);
+            SensorValuesTester.TestSensorInfoFromDB(doubleBarSensorValue, sensorInfoFromDB);
+        }
+
+        [Fact]
+        public async void AddFileSensorBytesValueTest()
+        {
+            var fileSensorBytesValue = new FileSensorBytesValue()
+            {
+                Extension = "csv",
+                FileContent = new byte[] { 125, 30, 98 },
+                FileName = nameof(FileSensorBytesValue),
+                Key = _databaseFixture.TestProduct.Key,
+                Path = nameof(FileSensorBytesValue),
+                Description = $"{nameof(FileSensorBytesValue)} {nameof(FileSensorBytesValue.Description)}",
+                Comment = $"{nameof(FileSensorBytesValue)} {nameof(FileSensorBytesValue.Comment)}",
+                Time = DateTime.UtcNow,
+            };
+
+            _monitoringCore.AddSensorValue(fileSensorBytesValue);
+
+            await Task.Delay(100);
+
+            var sensorDataFromCache =
+                _valuesCache.GetValues(new List<string>(1) { _databaseFixture.TestProduct.Name })
+                            ?.FirstOrDefault(s => s.SensorType == HSMSensorDataObjects.SensorType.FileSensorBytes);
+            SensorValuesTester.TestSensorDataFromCache(fileSensorBytesValue, sensorDataFromCache);
+
+            var sensorHistoryDataFromDB = _databaseFixture.DatabaseAdapter.GetOneValueSensorValue(_databaseFixture.TestProduct.Name, fileSensorBytesValue.Path);
+            SensorValuesTester.TestSensorHistoryDataFromDB(fileSensorBytesValue, sensorHistoryDataFromDB);
+
+            var sensorInfoFromDB = _databaseFixture.DatabaseAdapter.GetSensorInfo(_databaseFixture.TestProduct.Name, fileSensorBytesValue.Path);
+            SensorValuesTester.TestSensorInfoFromDB(fileSensorBytesValue, sensorInfoFromDB);
+        }
+
+        [Fact]
+        public async void AddFileSensorValueTest()
+        {
+            var fileSensorValue = new FileSensorValue()
+            {
+                Extension = "csv",
+                FileContent = $"{nameof(FileSensorValue)} {nameof(FileSensorValue.FileContent)}",
+                FileName = nameof(FileSensorValue),
+                Key = _databaseFixture.TestProduct.Key,
+                Path = nameof(FileSensorValue),
+                Description = $"{nameof(FileSensorValue)} {nameof(FileSensorValue.Description)}",
+                Comment = $"{nameof(FileSensorValue)} {nameof(FileSensorValue.Comment)}",
+                Time = DateTime.UtcNow,
+            };
+
+            _monitoringCore.AddSensorValue(fileSensorValue);
+
+            await Task.Delay(100);
+
+            var sensorDataFromCache =
+                _valuesCache.GetValues(new List<string>(1) { _databaseFixture.TestProduct.Name })
+                            ?.FirstOrDefault(s => s.SensorType == HSMSensorDataObjects.SensorType.FileSensor);
+            SensorValuesTester.TestSensorDataFromCache(fileSensorValue, sensorDataFromCache);
+
+            var sensorHistoryDataFromDB = _databaseFixture.DatabaseAdapter.GetOneValueSensorValue(_databaseFixture.TestProduct.Name, fileSensorValue.Path);
+            SensorValuesTester.TestSensorHistoryDataFromDB(fileSensorValue, sensorHistoryDataFromDB);
+
+            var sensorInfoFromDB = _databaseFixture.DatabaseAdapter.GetSensorInfo(_databaseFixture.TestProduct.Name, fileSensorValue.Path);
+            SensorValuesTester.TestSensorInfoFromDB(fileSensorValue, sensorInfoFromDB);
         }
     }
 }
--- src/HSMServer.Core.Tests/MonitoringCoreTests/SensorValuesTester.cs
+++ src/HSMServer.Core.Tests/MonitoringCoreTests/SensorValuesTester.cs
@@ -1,4 +1,5 @@
-﻿using HSMSensorDataObjects;
+﻿using System.Text.Json;
+using HSMSensorDataObjects;
 using HSMSensorDataObjects.FullDataObject;
 using HSMServer.Core.Model.Sensor;
 using Xunit;
@@ -30,18 +31,158 @@ namespace HSMServer.Core.Tests
             Assert.EndsWith($". Value = {expected.IntValue}, comment = {expected.Comment}.", actual.StringValue);
         }
 
+        internal static void TestSensorDataFromCache(DoubleSensorValue expected, SensorData actual)
+        {
+            TestSensorDataFromCache(expected, SensorType.DoubleSensor, actual);
+
+            Assert.Equal(expected.DoubleValue.ToString(), actual.ShortStringValue);
+            Assert.EndsWith($". Value = {expected.DoubleValue}, comment = {expected.Comment}.", actual.StringValue);
+        }
+
+        internal static void TestSensorDataFromCache(StringSensorValue expected, SensorData actual)
+        {
+            TestSensorDataFromCache(expected, SensorType.StringSensor, actual);
+
+            Assert.Equal(expected.StringValue, actual.ShortStringValue);
+            Assert.EndsWith($". Value = {expected.StringValue}, comment = {expected.Comment}.", actual.StringValue);
+        }
+
+        internal static void TestSensorDataFromCache(IntBarSensorValue expected, SensorData actual)
+        {
+            TestSensorDataFromCache(expected, SensorType.IntegerBarSensor, actual);
+
+            Assert.Equal($"Min = {expected.Min}, Mean = {expected.Mean}, Max = {expected.Max}, Count = {expected.Count}, Last = {expected.LastValue}.", actual.ShortStringValue);
+            Assert.EndsWith($". Value: Min = {expected.Min}, Mean = {expected.Mean}, Max = {expected.Max}, Count = {expected.Count}, Last = {expected.LastValue}. Comment = {expected.Comment}.", actual.StringValue);
+        }
+
+        internal static void TestSensorDataFromCache(DoubleBarSensorValue expected, SensorData actual)
+        {
+            TestSensorDataFromCache(expected, SensorType.DoubleBarSensor, actual);
+
+            Assert.Equal($"Min = {expected.Min}, Mean = {expected.Mean}, Max = {expected.Max}, Count = {expected.Count}, Last = {expected.LastValue}.", actual.ShortStringValue);
+            Assert.EndsWith($". Value: Min = {expected.Min}, Mean = {expected.Mean}, Max = {expected.Max}, Count = {expected.Count}, Last = {expected.LastValue}. Comment = {expected.Comment}.", actual.StringValue);
+        }
+
+        internal static void TestSensorDataFromCache(FileSensorBytesValue expected, SensorData actual)
+        {
+            TestSensorDataFromCache(expected, SensorType.FileSensorBytes, actual);
+
+            Assert.Equal($"File size: {expected.FileContent.Length} bytes. File name: {expected.FileName}.{expected.Extension}.", actual.ShortStringValue);
+            Assert.EndsWith($". File size: {expected.FileContent.Length} bytes. File name: {expected.FileName}.{expected.Extension}. Comment = {expected.Comment}.", actual.StringValue);
+        }
+
+        internal static void TestSensorDataFromCache(FileSensorValue expected, SensorData actual)
+        {
+            TestSensorDataFromCache(expected, SensorType.FileSensor, actual);
+
+            Assert.Equal($"File size: {expected.FileContent.Length} bytes. File name: {expected.FileName}.{expected.Extension}.", actual.ShortStringValue);
+            Assert.EndsWith($". File size: {expected.FileContent.Length} bytes. File name: {expected.FileName}.{expected.Extension}. Comment = {expected.Comment}.", actual.StringValue);
+        }
+
         internal static void TestSensorHistoryDataFromDB(BoolSensorValue expected, SensorHistoryData actual)
         {
-            TestSensorHistoryDataFromDB(expected, SensorType.BooleanSensor, actual);
+            var typedData = new
+            {
+                expected.Comment,
+                expected.BoolValue,
+            };
 
-            Assert.Contains(expected.BoolValue.ToString().ToLower(), actual.TypedData);
+            TestSensorHistoryDataFromDB(expected, SensorType.BooleanSensor, typedData, actual);
         }
 
         internal static void TestSensorHistoryDataFromDB(IntSensorValue expected, SensorHistoryData actual)
         {
-            TestSensorHistoryDataFromDB(expected, SensorType.IntSensor, actual);
+            var typedData = new
+            {
+                expected.IntValue,
+                expected.Comment,
+            };
 
-            Assert.Contains(expected.IntValue.ToString().ToLower(), actual.TypedData);
+            TestSensorHistoryDataFromDB(expected, SensorType.IntSensor, typedData, actual);
+        }
+
+        internal static void TestSensorHistoryDataFromDB(DoubleSensorValue expected, SensorHistoryData actual)
+        {
+            var typedData = new
+            {
+                expected.DoubleValue,
+                expected.Comment,
+            };
+
+            TestSensorHistoryDataFromDB(expected, SensorType.DoubleSensor, typedData, actual);
+        }
+
+        internal static void TestSensorHistoryDataFromDB(StringSensorValue expected, SensorHistoryData actual)
+        {
+            var typedData = new
+            {
+                expected.StringValue,
+                expected.Comment,
+            };
+
+            TestSensorHistoryDataFromDB(expected, SensorType.StringSensor, typedData, actual);
+        }
+
+        internal static void TestSensorHistoryDataFromDB(IntBarSensorValue expected, SensorHistoryData actual)
+        {
+            var typedData = new
+            {
+                expected.Comment,
+                expected.Min,
+                expected.Max,
+                expected.Mean,
+                expected.Percentiles,
+                expected.Count,
+                expected.StartTime,
+                expected.EndTime,
+                expected.LastValue,
+            };
+
+            TestSensorHistoryDataFromDB(expected, SensorType.IntegerBarSensor, typedData, actual);
+        }
+
+        internal static void TestSensorHistoryDataFromDB(DoubleBarSensorValue expected, SensorHistoryData actual)
+        {
+            var typedData = new
+            {
+                expected.Comment,
+                expected.Min,
+                expected.Max,
+                expected.Mean,
+                expected.Count,
+                expected.Percentiles,
+                expected.StartTime,
+                expected.EndTime,
+                expected.LastValue,
+            };
+
+            TestSensorHistoryDataFromDB(expected, SensorType.DoubleBarSensor, typedData, actual);
+        }
+
+        internal static void TestSensorHistoryDataFromDB(FileSensorBytesValue expected, SensorHistoryData actual)
+        {
+            var typedData = new
+            {
+                expected.Comment,
+                expected.Extension,
+                expected.FileContent,
+                expected.FileName,
+            };
+
+            TestSensorHistoryDataFromDB(expected, SensorType.FileSensorBytes, typedData, actual);
+        }
+
+        internal static void TestSensorHistoryDataFromDB(FileSensorValue expected, SensorHistoryData actual)
+        {
+            var typedData = new
+            {
+                expected.Comment,
+                expected.Extension,
+                expected.FileContent,
+                expected.FileName,
+            };
+
+            TestSensorHistoryDataFromDB(expected, SensorType.FileSensor, typedData, actual);
         }
 
         internal static void TestSensorInfoFromDB(BoolSensorValue expected, SensorInfo actual) =>
@@ -49,6 +190,24 @@ namespace HSMServer.Core.Tests
 
         internal static void TestSensorInfoFromDB(IntSensorValue expected, SensorInfo actual) =>
             TestSensorInfoFromDB(expected, SensorType.IntSensor, actual);
+
+        internal static void TestSensorInfoFromDB(DoubleSensorValue expected, SensorInfo actual) =>
+            TestSensorInfoFromDB(expected, SensorType.DoubleSensor, actual);
+
+        internal static void TestSensorInfoFromDB(StringSensorValue expected, SensorInfo actual) =>
+            TestSensorInfoFromDB(expected, SensorType.StringSensor, actual);
+
+        internal static void TestSensorInfoFromDB(IntBarSensorValue expected, SensorInfo actual) =>
+            TestSensorInfoFromDB(expected, SensorType.IntegerBarSensor, actual);
+
+        internal static void TestSensorInfoFromDB(DoubleBarSensorValue expected, SensorInfo actual) =>
+            TestSensorInfoFromDB(expected, SensorType.DoubleBarSensor, actual);
+
+        internal static void TestSensorInfoFromDB(FileSensorBytesValue expected, SensorInfo actual) =>
+            TestSensorInfoFromDB(expected, SensorType.FileSensorBytes, actual);
+
+        internal static void TestSensorInfoFromDB(FileSensorValue expected, SensorInfo actual) =>
+            TestSensorInfoFromDB(expected, SensorType.FileSensor, actual);
 
 
         private static void TestSensorDataFromCache(SensorValueBase expected, SensorType expectedType, SensorData actual)
@@ -65,12 +224,13 @@ namespace HSMServer.Core.Tests
             Assert.NotEqual(default, actual.Time);
         }
 
-        private static void TestSensorHistoryDataFromDB(SensorValueBase expected, SensorType expectedType, SensorHistoryData actual)
+        private static void TestSensorHistoryDataFromDB(SensorValueBase expected, SensorType expectedType, object expectedTypeData, SensorHistoryData actual)
         {
             Assert.NotNull(actual);
             Assert.Equal(expectedType, actual.SensorType);
             Assert.Equal(expected.Time.ToUniversalTime(), actual.Time);
             Assert.Contains(expected.Comment, actual.TypedData);
+            Assert.Equal(JsonSerializer.Serialize(expectedTypeData), actual.TypedData);
         }
 
         private static void TestSensorInfoFromDB(SensorValueBase expected, SensorType expectedType, SensorInfo actual)
@@ -80,10 +240,10 @@ namespace HSMServer.Core.Tests
             Assert.Equal(expected.Path, actual.Path);
             Assert.Equal(_databaseFixture.TestProduct.Name, actual.ProductName);
             Assert.Equal(expected.Path, actual.SensorName);
-            Assert.Equal(expectedType, actual.SensorType);
             Assert.Equal(default, actual.ExpectedUpdateInterval);
             Assert.Empty(actual.ValidationParameters);
             Assert.Null(actual.Unit);
+            Assert.Equal(expectedType, actual.SensorType);
         }
     }
 }
