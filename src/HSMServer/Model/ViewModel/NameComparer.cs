e6fabbcc9f78f85655ae386ce67ecef2525f9f77
Aliaksandra Bachura <aliaksandra.bachura@soft-fx.lv>
2021-12-16 15:56:37 +0300

    Fixed comparers.

--- src/HSMServer/Model/ViewModel/NameComparer.cs
+++ src/HSMServer/Model/ViewModel/NameComparer.cs
@@ -6,6 +6,15 @@ namespace HSMServer.Model.ViewModel
     {
         public override int Compare(NodeViewModel x, NodeViewModel y)
         {
+            if (x == null && y == null)
+                return 0;
+
+            if (x == null)
+                return -1;
+
+            if (y == null)
+                return 1;
+
             return x.Name.CompareTo(y.Name);
         }
     }
@@ -14,6 +23,15 @@ namespace HSMServer.Model.ViewModel
     {
         public override int Compare(SensorViewModel x, SensorViewModel y)
         {
+            if (x == null && y == null)
+                return 0;
+
+            if (x == null)
+                return -1;
+
+            if (y == null)
+                return 1;
+
             return x.Name.CompareTo(y.Name);
         }
     }
