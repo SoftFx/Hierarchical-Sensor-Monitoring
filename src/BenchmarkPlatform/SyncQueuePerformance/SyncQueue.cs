5d7313acbc0e1c9bff34210d3fec00561e7de388
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2022-03-23 11:18:24 +0300

    HSM-188: Benchmark for SensorUpdatesQueue has been added

---
+++ src/BenchmarkPlatform/SyncQueuePerformance/SyncQueue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BenchmarkPlatform.SyncQueuePerformance
+{
+    internal class SyncQueue
+    {
+        private readonly Queue _syncQueue;
+        private bool _run = false;
+
+
+        internal Action<int> NewItemEvent;
+
+
+        internal SyncQueue()
+        {
+            _syncQueue = Queue.Synchronized(new Queue());
+
+            _run = true;
+
+            ThreadPool.QueueUserWorkItem(_ => RunManageThread());
+        }
+
+
+        internal void AddItem(int data)
+        {
+            _syncQueue.Enqueue(data);
+        }
+
+
+        private async void RunManageThread()
+        {
+            while (_run)
+            {
+                object data = null;
+
+                lock (_syncQueue.SyncRoot)
+                {
+                    if (_syncQueue.Count > 0)
+                        data = _syncQueue.Dequeue();
+                }
+
+                if (data is null)
+                    await Task.Delay(10);
+                else
+                    NewItemEvent?.Invoke((int)data);
+            }
+        }
+
+        internal void Stop()
+        {
+            _run = false;
+        }
+    }
+}
