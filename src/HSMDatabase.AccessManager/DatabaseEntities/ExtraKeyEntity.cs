6055d45a0170937355dedf72570c3ababc5d20a2
alexsamail <44644986+alexsamail@users.noreply.github.com>
2022-03-25 12:19:22 +0300

    Entities updated. (#45)
    * Entities updated.
    
    * Entities refactored.
    
    * AccessKeyEntity renamed.
    
    Co-authored-by: Aliaksandra Bachura <aliaksandra.bachura@soft-fx.lv>

--- src/HSMDatabase.AccessManager/DatabaseEntities/ExtraKeyEntity.cs
+++ src/HSMDatabase.AccessManager/DatabaseEntities/ExtraKeyEntity.cs
@@ -1,8 +1,16 @@
-﻿namespace HSMDatabase.AccessManager.DatabaseEntities
+﻿using System;
+
+namespace HSMDatabase.AccessManager.DatabaseEntities
 {
-    public class ExtraKeyEntity
+    public sealed record AccessKeyEntity
     {
-        public string Name { get; set; }
-        public string Key { get; set; }
+        public string Id { get; init; }
+        public string AuthorId { get; init; }
+        public string ProductId { get; init; }
+        public bool IsLocked { get; init; }
+        public byte KeyRole { get; init; }
+        public string DisplayName { get; init; }
+        public long CreationTime { get; init; }
+        public long ExpirationTime { get; init; }
     }
 }
