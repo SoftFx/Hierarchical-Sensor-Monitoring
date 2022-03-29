eabe38d38ba1b08737871bcdeeb3af38a0155750
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2022-03-29 16:00:02 +0300

    Added view components for Tree and TreeNode

---
+++ src/HSMServer/Components/TreeNodeViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using HSMServer.Model.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HSMServer.Components
+{
+    public class TreeNodeViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(NodeViewModel node) => View("TreeNode", node);
+    }
+}
---
+++ src/HSMServer/Components/TreeViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using HSMServer.Model.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HSMServer.Components
+{
+    public class TreeViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(TreeViewModel tree) => View("Tree", tree);
+    }
+}
