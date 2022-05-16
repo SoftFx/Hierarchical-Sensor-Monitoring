b00f7722b0002829290f248b45df4c8347c9380a
Aliaksandra Bachura <aliaksandra.bachura@soft-fx.lv>
2022-05-16 15:02:56 +0300

    Refactoring.

--- src/HSMDatabase.LevelDB/DatabaseImplementations/EnvironmentDatabaseWorker.cs
+++ src/HSMDatabase.LevelDB/DatabaseImplementations/EnvironmentDatabaseWorker.cs
@@ -12,10 +12,20 @@ namespace HSMDatabase.LevelDB.DatabaseImplementations
     {
         private readonly LevelDBDatabaseAdapter _database;
         private readonly Logger _logger;
+
+        private readonly byte[] _accessKeyListKey;
+        private readonly byte[] _productListKey;
+
         public EnvironmentDatabaseWorker(string name)
         {
             _database = new LevelDBDatabaseAdapter(name);
             _logger = LogManager.GetCurrentClassLogger();
+
+            var key = PrefixConstants.GetAccessKeyListKey();
+            _accessKeyListKey = Encoding.UTF8.GetBytes(key);
+
+            key = PrefixConstants.GetProductsListKey();
+            _productListKey = Encoding.UTF8.GetBytes(key);
         }
 
         #region Products
@@ -23,11 +33,9 @@ namespace HSMDatabase.LevelDB.DatabaseImplementations
         //ToDo: use like ID
         public void AddProductToList(string productName)
         {
-            var key = PrefixConstants.GetProductsListKey();
-            byte[] bytesKey = Encoding.UTF8.GetBytes(key);
             try
             {
-                var currentList = _database.TryRead(bytesKey, out var value)
+                var currentList = _database.TryRead(_productListKey, out var value)
                     ? JsonSerializer.Deserialize<List<string>>(Encoding.UTF8.GetString(value))   
                     : new List<string>();
 
@@ -36,7 +44,8 @@ namespace HSMDatabase.LevelDB.DatabaseImplementations
 
                 string stringData = JsonSerializer.Serialize(currentList);
                 byte[] bytesValue = Encoding.UTF8.GetBytes(stringData);
-                _database.Put(bytesKey, bytesValue);
+
+                _database.Put(_productListKey, bytesValue);
             }
             catch (Exception e)
             {
@@ -46,12 +55,10 @@ namespace HSMDatabase.LevelDB.DatabaseImplementations
 
         public List<string> GetProductsList()
         {
-            string listKey = PrefixConstants.GetProductsListKey();
-            byte[] bytesKey = Encoding.UTF8.GetBytes(listKey);
             var result = new List<string>();
             try
             {
-                var products = _database.TryRead(bytesKey, out byte[] value) ?
+                var products = _database.TryRead(_productListKey, out byte[] value) ?
                     JsonSerializer.Deserialize<List<string>>(Encoding.UTF8.GetString(value))
                     : new List<string>();
 
@@ -190,11 +197,9 @@ namespace HSMDatabase.LevelDB.DatabaseImplementations
         //ToDo: use like ID
         public void RemoveProductFromList(string productName)
         {
-            var key = PrefixConstants.GetProductsListKey();
-            byte[] bytesKey = Encoding.UTF8.GetBytes(key);
             try
             {
-                var currentList = _database.TryRead(bytesKey, out byte[] value)
+                var currentList = _database.TryRead(_productListKey, out byte[] value)
                     ? JsonSerializer.Deserialize<List<string>>(Encoding.UTF8.GetString(value))
                     : new List<string>();
 
@@ -202,7 +207,7 @@ namespace HSMDatabase.LevelDB.DatabaseImplementations
 
                 string stringData = JsonSerializer.Serialize(currentList);
                 byte[] bytesValue = Encoding.UTF8.GetBytes(stringData);
-                _database.Put(bytesKey, bytesValue);
+                _database.Put(_productListKey, bytesValue);
             }
             catch (Exception e)
             {
@@ -216,18 +221,15 @@ namespace HSMDatabase.LevelDB.DatabaseImplementations
 
         public void AddAccessKeyToList(string id) 
         {
-            var key = PrefixConstants.GetAccessKeyListKey();
-            byte[] bytesKey = Encoding.UTF8.GetBytes(key);
             try
             {
                 var currentList = GetAccessKeyList();
-
                 if (!currentList.Contains(id))
                     currentList.Add(id);
 
                 string stringData = JsonSerializer.Serialize(currentList);
                 byte[] bytesValue = Encoding.UTF8.GetBytes(stringData);
-                _database.Put(bytesKey, bytesValue);
+                _database.Put(_accessKeyListKey, bytesValue);
             }
             catch (Exception e)
             {
@@ -237,12 +239,10 @@ namespace HSMDatabase.LevelDB.DatabaseImplementations
 
         public List<string> GetAccessKeyList()
         {
-            string listKey = PrefixConstants.GetAccessKeyListKey();
-            byte[] bytesKey = Encoding.UTF8.GetBytes(listKey);
             var result = new List<string>();
             try
             {
-                var keys = _database.TryRead(bytesKey, out byte[] value) ?
+                var keys = _database.TryRead(_accessKeyListKey, out byte[] value) ?
                     JsonSerializer.Deserialize<List<string>>(Encoding.UTF8.GetString(value))
                     : new List<string>();
 
@@ -258,17 +258,14 @@ namespace HSMDatabase.LevelDB.DatabaseImplementations
 
         public void RemoveAccessKeyFromList(string id)
         {
-            var key = PrefixConstants.GetAccessKeyListKey();
-            byte[] bytesKey = Encoding.UTF8.GetBytes(key);
             try
             {
                 var currentList = GetAccessKeyList();
-
                 currentList.Remove(id);
 
                 string stringData = JsonSerializer.Serialize(currentList);
                 byte[] bytesValue = Encoding.UTF8.GetBytes(stringData);
-                _database.Put(bytesKey, bytesValue);
+                _database.Put(_accessKeyListKey, bytesValue);
             }
             catch (Exception e)
             {
