89a4c7a24e779e8cc3f4f6e27eded51c3350700a
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2022-06-17 16:50:54 +0300

    Added children for ProductRoleFilter for every type of action argument

---
+++ src/HSMServer/Filters/ProductRoleFilters/ProductRoleFilterBase.cs
@@ -0,0 +1,58 @@
+﻿using HSMServer.Constants;
+using HSMServer.Core.Model.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HSMServer.Filters.ProductRoleFilters
+{
+    /// <summary>
+    /// The attribute denies access to some actions for a user who is neither admin or has role from _roles
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public abstract class ProductRoleFilterBase : Attribute, IActionFilter
+    {
+        private readonly List<ProductRoleEnum> _roles;
+        private readonly RedirectToActionResult _redirectToHomeIndex =
+            new(ViewConstants.IndexAction, ViewConstants.HomeController, null);
+
+        protected abstract string ArgumentName { get; }
+
+
+        public ProductRoleFilterBase(params ProductRoleEnum[] parameters)
+        {
+            _roles = new List<ProductRoleEnum>(parameters);
+        }
+
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var user = context.HttpContext.User as User;
+            if (user?.IsAdmin ?? false) //Admins have all possible access
+                return;
+
+            if (TryGetProductId(context, out var productId))
+                foreach (var role in _roles)
+                    if (user.ProductsRoles.Any(r => r.Key == productId && r.Value == role))
+                        return;
+
+            context.Result = _redirectToHomeIndex;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context) { }
+
+        protected abstract string GetProductId(object arg, ActionExecutingContext context = null);
+
+        private bool TryGetProductId(ActionExecutingContext context, out string productId)
+        {
+            productId = null;
+
+            if (context.ActionArguments.TryGetValue(ArgumentName, out var arg))
+                productId = GetProductId(arg, context);
+
+            return !string.IsNullOrEmpty(productId);
+        }
+    }
+}
---
+++ src/HSMServer/Filters/ProductRoleFilters/ProductRoleFilterByProductId.cs
@@ -0,0 +1,17 @@
+﻿using HSMServer.Core.Model.Authentication;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace HSMServer.Filters.ProductRoleFilters
+{
+    public sealed class ProductRoleFilterByProductId : ProductRoleFilterBase
+    {
+        protected override string ArgumentName => "productId";
+
+
+        public ProductRoleFilterByProductId(params ProductRoleEnum[] roles) : base(roles) { }
+
+
+        protected override string GetProductId(object arg, ActionExecutingContext _) =>
+            arg is string productId ? productId : null;
+    }
+}
