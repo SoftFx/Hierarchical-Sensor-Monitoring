6638dba0b651ea4631f2ff6cf08894268ef4e7c6
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2022-06-17 15:46:10 +0300

    Added ProductRoleFilter for actions (new/edit/remove) in AccessKeysController

--- src/HSMServer/Filters/ProductRoleFilter.cs
+++ src/HSMServer/Filters/ProductRoleFilter.cs
@@ -1,5 +1,8 @@
 ﻿using HSMServer.Constants;
+using HSMServer.Controllers;
 using HSMServer.Core.Model.Authentication;
+using HSMServer.Helpers;
+using HSMServer.Model.AccessKeysViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
@@ -15,9 +18,13 @@ namespace HSMServer.Filters
     public class ProductRoleFilter : Attribute, IActionFilter
     {
         private readonly List<ProductRoleEnum> _roles;
-        private readonly string _productIdArgument = "productId";
         private readonly RedirectToActionResult _redirectToHomeIndex =
             new(ViewConstants.IndexAction, ViewConstants.HomeController, null);
+
+        private readonly string _encodedProductIdArgument = "encodedProductId";
+        private readonly string _productIdArgument = "productId";
+        private readonly string _selectedKeyArgument = "selectedKey";
+        private readonly string _keyArgument = "key";
 
 
         public ProductRoleFilter(params ProductRoleEnum[] parameters)
@@ -32,7 +39,7 @@ namespace HSMServer.Filters
             if (user?.IsAdmin ?? false) //Admins have all possible access
                 return;
 
-            if (context.ActionArguments.TryGetValue(_productIdArgument, out var productIdArg) && productIdArg is string productId)
+            if (TryGetProductId(context, out var productId))
                 foreach (var role in _roles)
                     if (user.ProductsRoles.Any(r => r.Key == productId && r.Value == role))
                         return;
@@ -41,5 +48,27 @@ namespace HSMServer.Filters
         }
 
         public void OnActionExecuted(ActionExecutedContext context) { }
+
+        private bool TryGetProductId(ActionExecutingContext context, out string productId)
+        {
+            if (context.ActionArguments.TryGetValue(_productIdArgument, out var productIdArg) && productIdArg is string)
+                productId = productIdArg as string;
+            else if (context.ActionArguments.TryGetValue(_encodedProductIdArgument, out productIdArg) && productIdArg is string encodedProductId)
+                productId = SensorPathHelper.Decode(encodedProductId);
+            else if (context.ActionArguments.TryGetValue(_keyArgument, out var keyArg) && keyArg is EditAccessKeyViewModel key)
+                productId = SensorPathHelper.Decode(key.EncodedProductId);
+            else if (context.ActionArguments.TryGetValue(_selectedKeyArgument, out var selectedKeyArg) && selectedKeyArg is string selectedKey)
+            {
+                var accessKey = (context.Controller as AccessKeysController).TreeValuesCache.GetAccessKey(Guid.Parse(selectedKey));
+                productId = accessKey.ProductId;
+            }
+            else
+            {
+                productId = null;
+                return false;
+            }
+
+            return true;
+        }
     }
 }
