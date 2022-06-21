fda3b0a177e153eb0662c345936260744be975b4
Aliaksandra Bachura <aliaksandra.bachura@soft-fx.lv>
2022-06-21 14:05:22 +0300

    fix

--- src/HSMServer.Core.Tests/Infrastructure/ModelsTester.cs
+++ src/HSMServer.Core.Tests/Infrastructure/ModelsTester.cs
@@ -99,7 +99,7 @@ namespace HSMServer.Core.Tests.Infrastructure
             Assert.NotNull(actual);
             Assert.Equal(expected.AuthorId, actual.AuthorId);
             Assert.Equal(expected.Id, actual.ProductId);
-            Assert.Equal(KeyPermissions.CanAddProducts
+            Assert.Equal(KeyPermissions.CanAddNodes
                 | KeyPermissions.CanAddSensors | KeyPermissions.CanSendSensorData, actual.Permissions);
             Assert.Equal(CommonConstants.DefaultAccessKey, actual.DisplayName);
             Assert.Equal(DateTime.MaxValue, actual.ExpirationTime);
