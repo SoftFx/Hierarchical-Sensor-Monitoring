2d356846dfa213e73cd7695a6ccae97a5329c9e7
Sonya Gurik <aliaksandra.bachura@soft-fx.lv>
2022-07-14 16:38:03 +0300

    BarValuesStorage<T> in progress...

--- src/server/HSMServer.Core/Model/Storages/BarValuesStorages/BarValuesStorage.cs
+++ src/server/HSMServer.Core/Model/Storages/BarValuesStorages/BarValuesStorage.cs
@@ -1,4 +1,31 @@
-﻿namespace HSMServer.Core.Model
+﻿using System;
+
+namespace HSMServer.Core.Model
 {
-    public abstract class BarValuesStorage<T> : ValuesStorage<T> where T : BarBaseValue { }
+    public abstract class BarValuesStorage<T> : ValuesStorage<T> where T : BarBaseValue , IDisposable
+    {
+        private T _lastValue;
+
+
+        internal override T AddValue(T value)
+        {
+            if (_lastValue != null && _lastValue.OpenTime != value.OpenTime)
+            {
+                var addedValue = _lastValue;
+                base.AddValue(_lastValue);
+                _lastValue = value;
+
+                return addedValue;
+            }
+
+            return null;
+        }
+
+
+        public void Dispose()
+        {
+            if (_lastValue != null) 
+                base.AddValue(_lastValue);
+        }
+    }
 }
