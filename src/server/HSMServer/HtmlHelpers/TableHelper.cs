a3df50066ff92452958b983d17244449e31c02b0
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2022-07-26 14:59:44 +0300

    Changed tooltip for expected update interval

--- src/server/HSMServer/HtmlHelpers/TableHelper.cs
+++ src/server/HSMServer/HtmlHelpers/TableHelper.cs
@@ -551,7 +551,7 @@ namespace HSMServer.HtmlHelpers
             result.Append($"<tr><td>Path</td><td>{sensorInfo.Path}</td></tr>");
             result.Append($"<tr><td>Sensor type</td><td>{sensorInfo.SensorType}</td></tr>");
             result.Append("<tr><td>Expected update interval<i class='fas fa-question-circle' " +
-                          "title='Time format: dd.hh:mm:ss min value 00:05:00'></i></td><td><input disabled type='text' " +
+                          "title='Time format: dd.hh:mm:ss min value 00:01:00'></i></td><td><input disabled type='text' " +
                           $"class='form-control' style='max-width:300px' id='interval_{encodedId}' " +
                           $"value='{sensorInfo.ExpectedUpdateInterval}'></td></tr>");
             result.Append("<tr><td>Description</td><td><input disabled type='text' class='form-control' style='max-width:300px'" +
