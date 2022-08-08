b9b04fec167831c9f8a984817ffe966dc57402f1
PalinaSh <palina.sh@gmail.com>
2022-08-08 10:32:01 +0300

    Removed migration and unused nugets (#94)
    * Fixed some tests in cache tests
    
    * Fixed tests for update/remove sensors and remove sensor values
    
    * Added Type property in BaseValue
    
    * Fixed tests for add new sensor value in TreeValuesCache
    
    * Fixed Sensor Model's tests
    
    * Removed unused old SensorModel
    
    * Removed unused code in old db logic and related with SensorDataEntity
    
    * Replaced SensorStringPropertyBuilder tests by SensorValues tests
    
    * Removed SensorDataEntity and old code that were using this model.
    Added tests for converting API sensor values to server sensor values methods
    
    * Removed SensorHistoryData, ExtendedBarData, unused entities and tests
    
    * Removed unused converter for united sensor value
    
    * Removed SensorStatusExtension
    
    * Removed old tests for MonitoringCore.AddValue method and GetSensorHistory methods
    
    * Added tests for GetSensorValues methods in TreeValuesCache
    
    * Refactored entities factory in tests
    
    * Removed code for migration old style sensors to new style (and creating policies)
    
    * Removed unused nugets from HSMServer project
    
    Co-authored-by: palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>

--- src/database/HSMDatabase.AccessManager/DatabaseEntities/SensorEntity.cs
+++ src/database/HSMDatabase.AccessManager/DatabaseEntities/SensorEntity.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text.Json.Serialization;
+﻿using System.Collections.Generic;
 
 namespace HSMDatabase.AccessManager.DatabaseEntities
 {
     public class SensorEntity
     {
-        [Obsolete("Remove this property after sensor entities migration")]
-        [NonSerialized]
-        public long ExpectedUpdateIntervalTicks;
-
-
         public string Id { get; init; }
 
         public string ProductId { get; init; }
@@ -30,9 +23,5 @@ namespace HSMDatabase.AccessManager.DatabaseEntities
         public byte State { get; init; }
 
         public List<string> Policies { get; init; }
-
-
-        [JsonIgnore]
-        public bool IsConverted { get; set; }
     }
 }
