afd29308bb17cb0373aeeffeffde30ce7fd023c2
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2022-09-07 19:36:24 +0300

    NodeNotificationState -> NodeStateViewModel. Refactoring

---
+++ src/server/HSMServer.Core/Model/Authentication/NodeStateViewModel.cs
@@ -0,0 +1,35 @@
+﻿namespace HSMServer.Core.Model
+{
+    public class NodeStateViewModel
+    {
+        public bool IsAnyNotificationsEnabled { get; private set; }
+
+        public bool IsAllNotificationsEnabled { get; private set; }
+
+        public bool IsAnyNotificationsIgnored { get; private set; }
+
+        public int FilteredSensorsCount { get; set; }
+
+
+        public NodeStateViewModel() => Reset();
+
+
+        public void ChangeEnableState(bool isNotificationsEnabled)
+        {
+            IsAnyNotificationsEnabled |= isNotificationsEnabled;
+            IsAllNotificationsEnabled &= isNotificationsEnabled;
+        }
+
+        public void ChangeIgnoreState(bool isNotificationsIgnored)
+        {
+            IsAnyNotificationsIgnored &= isNotificationsIgnored;
+        }
+
+        internal void Reset()
+        {
+            IsAnyNotificationsEnabled = false;
+            IsAllNotificationsEnabled = true;
+            IsAnyNotificationsIgnored = true;
+        }
+    }
+}
