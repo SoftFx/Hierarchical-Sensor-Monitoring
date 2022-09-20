21807feee676d4c59f4998cc5ce33d26119da01c
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2022-09-20 15:09:09 +0300

    TAM-298. HSM site, Tree: Status has incorrect message in tooltip

--- src/server/HSMServer/Model/TreeViewModels/UpdatedSensorDataViewModel.cs
+++ src/server/HSMServer/Model/TreeViewModels/UpdatedSensorDataViewModel.cs
@@ -9,17 +9,20 @@ namespace HSMServer.Model.TreeViewModels
 
         public string Value { get; }
 
-        public string StatusColorClass { get; }
+        public string Status { get; }
 
         public string UpdatedTimeStr { get; }
 
         public string ValidationError { get; }
+
+        public string StatusColorClass { get; }
 
 
         public UpdatedSensorDataViewModel(SensorNodeViewModel sensor)
         {
             Id = sensor.EncodedId;
             Value = sensor.ShortStringValue;
+            Status = sensor.Status.ToString();
             StatusColorClass = sensor.Status.ToCssIconClass();
             UpdatedTimeStr = sensor.UpdateTime != DateTime.MinValue
                 ? $"updated {sensor.GetTimeAgo(DateTime.UtcNow - sensor.UpdateTime)}"
