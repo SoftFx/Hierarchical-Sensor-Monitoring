0f91906479f618c8232822485dcfa7bb88369a6d
AndrewKhloptsau <andrei.khloptsau@soft-fx.lv>
2022-11-24 19:12:25 +0300

    Try get more info

--- src/server/HSMServer/Middleware/LoggingExceptionMiddleware.cs
+++ src/server/HSMServer/Middleware/LoggingExceptionMiddleware.cs
@@ -31,17 +31,17 @@ namespace HSMServer.Middleware
                 {
                     var request = context.Request;
 
-                    request.EnableBuffering();
+                    _logger.Error($"Path {request.Path}, remote port: {context.Connection.RemotePort}, content size = {request.Body.Length}");
 
-                    var arr = new byte[1000];
-                    var cnt = await context.Request.Body.ReadAsync(arr, 0, 1000);
 
-                    var str64 = Base64.ToBase64String(arr, 0, cnt);
+                    //var arr = new byte[1000];
+                    //var cnt = await context.Request.Body.ReadAsync(arr, 0, 1000);
 
-                    _logger.Error($"Path {context.Request.Path}, {str64}");
-                    _logger.Error(ex, Encoding.UTF8.GetString(arr, 0, cnt));
+                    //var str64 = Base64.ToBase64String(arr, 0, cnt);
+
+                    
+                    //_logger.Error(ex, Encoding.UTF8.GetString(arr, 0, cnt));
                 }
-
                 throw;
             }
         }
