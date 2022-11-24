724ac073a477bab3993497d551fb9c5e4cb4cb35
AndrewKhloptsau <andrei.khloptsau@soft-fx.lv>
2022-11-24 18:51:55 +0300

    Try to read only 1000 first chars

---
+++ src/server/HSMServer/Middleware/LoggingExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using HSMCommon.Constants;
+using Microsoft.AspNetCore.Http;
+using NLog;
+using Org.BouncyCastle.Utilities.Encoders;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HSMServer.Middleware
+{
+    internal sealed class LoggingExceptionMiddleware
+    {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private readonly RequestDelegate _next;
+
+        public LoggingExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                if (context.Connection.LocalPort == ConfigurationConstants.SensorsPort)
+                {
+                    var request = context.Request;
+
+                    request.EnableBuffering();
+
+                    var arr = new byte[1000];
+                    var cnt = await context.Request.Body.ReadAsync(arr, 0, 1000);
+
+                    var str64 = Base64.ToBase64String(arr, 0, cnt);
+
+                    _logger.Error($"Path {context.Request.Path}, {str64}");
+                    _logger.Error(ex, Encoding.UTF8.GetString(arr, 0, cnt));
+                }
+
+                throw;
+            }
+        }
+    }
+}
