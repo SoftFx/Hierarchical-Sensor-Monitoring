b81ed965c9fe5999b8fe5ca02d353148e70cdaf2
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2022-12-05 05:20:13 +0300

    refactoring EditProductViewModel.cs

--- src/server/HSMServer/Model/ViewModel/EditProductViewModel.cs
+++ src/server/HSMServer/Model/ViewModel/EditProductViewModel.cs
@@ -10,14 +10,28 @@ namespace HSMServer.Model.ViewModel
 {
     public class EditProductViewModel
     {
+        #region Get Properties
         public string ProductName { get; }
+        
         public string ProductId { get; }
+        
         public string EncodedProductId { get; }
-        public List<(UserViewModel, ProductRoleEnum)> UsersRights { get; }
+        
         public List<AccessKeyViewModel> AccessKeys { get; }
-        public TelegramSettingsViewModel Telegram { get; }
+        
         public ISet<UserViewModel> NotAdminUsers { get; }
+        
         public ISet<UserViewModel> UsedUsers { get;}
+        
+        #endregion
+
+        #region Set Properties
+        
+        public TelegramSettingsViewModel Telegram { get; set; }
+
+        public List<(UserViewModel, ProductRoleEnum)> UsersRights { get; set; }
+        
+        #endregion
 
         public EditProductViewModel(ProductNodeViewModel product,
             List<(User, ProductRoleEnum)> usersRights,
@@ -26,14 +40,17 @@ namespace HSMServer.Model.ViewModel
             ProductName = product.Name;
             ProductId = product.Id;
             EncodedProductId = product.EncodedId;
+            
             UsersRights = usersRights.Select(x => (new UserViewModel(x.Item1), x.Item2)).ToList();
             AccessKeys = product.GetEditProductAccessKeys();
             Telegram = product.TelegramSettings;
+            
             UsedUsers = UsersRights.Select(ur => ur.Item1).ToImmutableHashSet();
             NotAdminUsers = notAdminUsers.Select(x => new UserViewModel(x)).ToHashSet();
             RemoveUsedUsers(NotAdminUsers, UsedUsers);
         }
-        private void RemoveUsedUsers(ISet<UserViewModel> users, ISet<UserViewModel> usedUsers)
+        
+        private static void RemoveUsedUsers(ISet<UserViewModel> users, IEnumerable<UserViewModel> usedUsers)
         {
             users.ExceptWith(usedUsers);
         }
