b224908852ecb56dff5021e09f5ff97d4d5725a0
eugenevanifatev <eugenevanifatev@gmail.com>
2022-12-05 13:02:07 +0300

    TAM-189 Add tests (#135)
    * TAM-189 Add tests
    
    * TAM-189 refactoring
    
    * TAM-189 refactoring
    
    * fixed initialization parameters don't suit StoreInfo class
    
    Co-authored-by: Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>

---
+++ src/tests/HSMServer.Core.Tests/UpdatesQueueTests/UpdatesQueueTests.cs
@@ -0,0 +1,128 @@
+﻿using HSMServer.Core.Model;
+using HSMServer.Core.SensorsUpdatesQueue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Requests;
+using Xunit;
+
+namespace HSMServer.Core.Tests.UpdatesQueueTests
+{
+    public class UpdatesQueueTests
+    {
+        private const int DelayTime = 100;
+
+        [Fact]
+        [Trait("Category", "Add Item(s)")]
+        public async Task AddItemTest()
+        {
+            StoreInfo receivedInfo = default;
+            void GetItem(List<StoreInfo> items)
+            {
+                receivedInfo = items[0];
+            }
+
+            StoreInfo storeInfo = BuildStoreInfo(0);
+            var updatesQueue = new UpdatesQueue();
+            updatesQueue.AddItem(storeInfo);
+            updatesQueue.NewItemsEvent += GetItem;
+
+            await Task.Delay(DelayTime);
+            Assert.Equal(storeInfo, receivedInfo);
+        }
+
+        [Fact]
+        [Trait("Category", "Add Item(s)")]
+        public async Task AddEmptyItemTest()
+        {
+            StoreInfo receivedInfo = default;
+            void GetItem(List<StoreInfo> items)
+            {
+                receivedInfo = items[0];
+            }
+
+            StoreInfo storeInfo = new("", "/");
+            var updatesQueue = new UpdatesQueue();
+            updatesQueue.AddItem(storeInfo);
+            updatesQueue.NewItemsEvent += GetItem;
+
+            await Task.Delay(DelayTime);
+            Assert.Equal(storeInfo, receivedInfo);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [Trait("Category", "Add Item(s)")]
+        public async Task AddItemsTest(int count)
+        {
+            List<StoreInfo> receivedInfo = new(count);
+            void GetItem(List<StoreInfo> items)
+            {
+                receivedInfo.AddRange(items);
+            }
+
+            List<StoreInfo> items = AddItemsToList(count);
+
+            var updatesQueue = new UpdatesQueue();
+            updatesQueue.AddItems(items);
+            updatesQueue.NewItemsEvent += GetItem;
+
+            await Task.Delay(DelayTime);
+            Assert.Equal(items.Count, receivedInfo.Count);
+            for (int i = 0; i < items.Count; i++)
+               Assert.Equal(items[i], receivedInfo[i]);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(100)]
+        [Trait("Category", "Add Item(s)")]
+        public async Task AddEmptyItemsTest(int count)
+        {
+            List<StoreInfo> receivedInfo = new(count);
+            void GetItem(List<StoreInfo> storeInfo)
+            {
+                receivedInfo.AddRange(storeInfo);
+            }
+
+            List<StoreInfo> items = new(count);
+            for (int i = 0; i < count; i++)
+            {
+                StoreInfo storeInfo = new("", "/");
+                items.Add(storeInfo);
+            }
+
+            var updatesQueue = new UpdatesQueue();
+            updatesQueue.AddItems(items);
+            updatesQueue.NewItemsEvent += GetItem;
+
+            await Task.Delay(DelayTime);
+            Assert.Equal(items.Count, receivedInfo.Count);
+            for (int i = 0; i < items.Count; i++)
+                Assert.Equal(items[i], receivedInfo[i]);
+        }
+
+        private static List<StoreInfo> AddItemsToList(int count)
+        {
+            List<StoreInfo> items = new(count);
+
+            for (int i = 0; i < count; i++)
+            {
+                StoreInfo storeInfo = BuildStoreInfo(i);
+                items.Add(storeInfo);
+            }
+
+            return items;
+        }
+
+        private static StoreInfo BuildStoreInfo(int value)
+        {
+            return new("", "/") { BaseValue = new IntegerValue() { Value = value } };
+        }
+    }
+}
