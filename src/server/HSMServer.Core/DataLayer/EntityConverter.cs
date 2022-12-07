f05504fcdd988466f8dcc846dd7a04f46de89dd1
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2022-12-07 15:54:52 +0300

    Removed code for build BaseValue type from old sensor values (from monitoring db)

--- src/server/HSMServer.Core/DataLayer/EntityConverter.cs
+++ src/server/HSMServer.Core/DataLayer/EntityConverter.cs
@@ -5,9 +5,6 @@ namespace HSMServer.Core.DataLayer
 {
     public static class EntityConverter
     {
-        private const string SensorDataTypedDataPropertyName = "TypedData";
-
-
         public static BaseValue ConvertToSensorValue<T>(this byte[] entity) where T : BaseValue
         {
             if (entity == null)
@@ -15,9 +12,7 @@ namespace HSMServer.Core.DataLayer
 
             var rootElement = JsonDocument.Parse(entity).RootElement;
 
-            return rootElement.TryGetProperty(SensorDataTypedDataPropertyName, out _)
-                ? SensorValuesFactory.BuildValue<T>(rootElement)
-                : JsonSerializer.Deserialize<T>(rootElement);
+            return JsonSerializer.Deserialize<T>(rootElement);
         }
     }
 }
