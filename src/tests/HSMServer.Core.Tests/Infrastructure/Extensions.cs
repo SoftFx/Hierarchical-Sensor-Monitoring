2e731f6c65dfc81dadab57beef33e0d53b71cdc1
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2022-12-07 16:57:44 +0300

    Fixed tests

--- src/tests/HSMServer.Core.Tests/Infrastructure/Extensions.cs
+++ src/tests/HSMServer.Core.Tests/Infrastructure/Extensions.cs
@@ -1,9 +1,13 @@
 ﻿using HSMCommon.Constants;
+using System.Linq;
 
 namespace HSMServer.Core.Tests.Infrastructure
 {
     internal static class StringExtensions
     {
         internal static string GetSensorName(this string path) => path?.Split(CommonConstants.SensorPathSeparator)?[^1];
+
+        internal static string WithoutFirstSlash(this string path) =>
+            path.FirstOrDefault() == CommonConstants.SensorPathSeparator ? path[1..] : path;
     }
 }
