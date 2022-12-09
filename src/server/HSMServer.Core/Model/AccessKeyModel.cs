ba5000fdec0b4eb24c8046bb12f6a6fe72732402
Savely Volodkov <s.volodkov@syberry.com>
2022-12-09 16:39:33 +0300

    changed field to property

--- src/server/HSMServer.Core/Model/AccessKeyModel.cs
+++ src/server/HSMServer.Core/Model/AccessKeyModel.cs
@@ -24,9 +24,10 @@ namespace HSMServer.Core.Model
 
     public class AccessKeyModel
     {
-        public static readonly KeyPermissions FullPermissions = (KeyPermissions)(1 << Enum.GetValues<KeyPermissions>().Length) - 1;
-        
         internal static InvalidAccessKey InvalidKey { get; } = new();
+
+        
+        public static KeyPermissions FullPermissions { get; } = (KeyPermissions)(1 << Enum.GetValues<KeyPermissions>().Length) - 1;
 
         
         public Guid Id { get; }
