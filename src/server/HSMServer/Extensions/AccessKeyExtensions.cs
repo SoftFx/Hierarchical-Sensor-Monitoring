45d2db7253756961a6bde26be6c8d2d33334b0a9
Savely Volodkov <s.volodkov@syberry.com>
2022-12-12 17:45:51 +0300

    some ui fixes

--- src/server/HSMServer/Extensions/AccessKeyExtensions.cs
+++ src/server/HSMServer/Extensions/AccessKeyExtensions.cs
@@ -7,12 +7,12 @@ public static class AccessKeyExtensions
     internal static string ToCssIconClass(this KeyState state) =>
         state switch
         {
-            KeyState.Active => "check key-icon-active",
-            KeyState.Expired => "exclamation key-icon-expired",
-            KeyState.Blocked => "xmark key-icon-blocked",
-            _ => "dot",
+            KeyState.Active => "fa-regular fa-circle-check key-icon-active",
+            KeyState.Expired => "fa fa-exclamation-circle key-icon-expired",
+            KeyState.Blocked => "fa-regular fa-circle-xmark key-icon-blocked",
+            _ => "fa-regular fa-circle-dot",
         };
 
     internal static string ToIcon(this KeyState state) =>
-        $"fa-regular fa-circle-{state.ToCssIconClass()}";
+        $"{state.ToCssIconClass()}";
 }
