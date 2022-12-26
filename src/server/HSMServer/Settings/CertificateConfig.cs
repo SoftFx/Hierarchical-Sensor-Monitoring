58aa7be7f0df90abc4228bb0ab56fb17fd2830b6
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2022-12-26 13:30:41 +0300

    refactoring

--- src/server/HSMServer/Settings/CertificateConfig.cs
+++ src/server/HSMServer/Settings/CertificateConfig.cs
@@ -3,5 +3,6 @@ namespace HSMServer.Settings;
 public class CertificateConfig
 {
     public string Name { get; set; }
+    
     public string Key { get; set; }
 }
--- src/server/HSMServer/Settings/KestrelConfig.cs
+++ src/server/HSMServer/Settings/KestrelConfig.cs
@@ -3,5 +3,6 @@ namespace HSMServer.Settings;
 public class KestrelConfig
 {
     public int SensorPort { get; set; }
+    
     public int SitePort { get; set; }
 }
