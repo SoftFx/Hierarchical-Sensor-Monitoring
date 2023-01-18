ade57fa6a77de4af2fa778b1dfdac788d1cec781
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-01-18 12:01:31 +0300

    refactoring

--- src/server/HSMServer/Model/ViewModel/ProductViewModel.cs
+++ src/server/HSMServer/Model/ViewModel/ProductViewModel.cs
@@ -8,12 +8,17 @@ namespace HSMServer.Model.ViewModel
     public record ProductViewModel
     {
         public Guid Id { get; }
+        
         public string EncodedId { get; }
+        
         public string Key { get; }
+        
         public string Name { get; }
+        
         public DateTime CreationDate { get; }
+        
         public string ManagerName { get; }
-
+        
         public ProductViewModel(string manager, ProductModel product)
         {
             Id = product.Id;
