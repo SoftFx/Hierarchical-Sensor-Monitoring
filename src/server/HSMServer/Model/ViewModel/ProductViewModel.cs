da6770ae5301f897d78b2fcf989c72f1309e6d60
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-01-20 13:27:17 +0300

    added lastUpdate as tooltip

--- src/server/HSMServer/Model/ViewModel/ProductViewModel.cs
+++ src/server/HSMServer/Model/ViewModel/ProductViewModel.cs
@@ -9,19 +9,21 @@ namespace HSMServer.Model.ViewModel
     public record ProductViewModel
     {
         public Guid Id { get; }
-        
+
         public string EncodedId { get; }
-        
+
         public string Key { get; }
-        
+
         public string Name { get; }
-        
+
+        public string ShortLastUpdateTime { get; }
+
         public DateTime CreationDate { get; }
-        
+
         public DateTime LastUpdateDate { get; }
 
         public List<string> ManagerNames { get; }
-        
+
         public ProductViewModel(List<string> managerNames, ProductModel product)
         {
             Id = product.Id;
@@ -29,8 +31,33 @@ namespace HSMServer.Model.ViewModel
             Key = product.AccessKeys.FirstOrDefault().Value?.Id.ToString();
             Name = product.DisplayName;
             CreationDate = product.CreationDate;
-            LastUpdateDate = product.LastUpdateDate;
+            LastUpdateDate = product.LastUpdateDate == DateTime.MinValue ? CreationDate : product.LastUpdateDate;
+            ShortLastUpdateTime = GetTimeAgo(this);
             ManagerNames = managerNames;
+        }
+
+        private static string GetTimeAgo(ProductViewModel productViewModel)
+        {
+            string UnitsToString(double value, string unit)
+            {
+                int intValue = Convert.ToInt32(value);
+                return intValue > 1 ? $"{intValue} {unit}s" : $"1 {unit}";
+            }
+
+            var time = new TimeSpan((DateTime.UtcNow - productViewModel.LastUpdateDate).Ticks);
+
+            if (time.TotalDays > 30)
+                return "> a month ago";
+            else if (time.TotalDays >= 1)
+                return $"> {UnitsToString(time.TotalDays, "day")} ago";
+            else if (time.TotalHours >= 1)
+                return $"> {UnitsToString(time.TotalHours, "hour")} ago";
+            else if (time.TotalMinutes >= 1)
+                return $"{UnitsToString(time.TotalMinutes, "minute")} ago";
+            else if (time.TotalSeconds < 60)
+                return "< 1 minute ago";
+
+            return "no info";
         }
     }
 }
