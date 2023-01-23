f177c4845d11ce2176bd45d91adb1347e5a69398
pawgyrocopter <94412374+pawgyrocopter@users.noreply.github.com>
2023-01-23 12:55:37 +0300

    toast changed (#202)
    Co-authored-by: Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>

--- src/server/HSMServer/wwwroot/src/index.js
+++ src/server/HSMServer/wwwroot/src/index.js
@@ -6,7 +6,7 @@ window.bootstrap = bootstrap;
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 window.ClipboardJS = require('clipboard');
-bootstrap.Toast.Default.delay = 5000;
+bootstrap.Toast.Default.delay = 3000;
 
 import * as moment from 'moment';
 window.moment = moment;
