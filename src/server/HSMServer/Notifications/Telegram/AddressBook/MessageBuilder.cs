ba316d84f10444088ed9e0298461ff3820da0118
AndrewKhloptsau <andrei.khloptsau@soft-fx.lv>
2023-02-07 15:55:24 +0300

    CDict has been added

--- src/server/HSMServer/Notifications/Telegram/AddressBook/MessageBuilder.cs
+++ src/server/HSMServer/Notifications/Telegram/AddressBook/MessageBuilder.cs
@@ -3,32 +3,94 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace HSMServer.Notifications
 {
+    internal sealed class CQueue : ConcurrentQueue<string>
+    {
+        private const int MaxMessagesCount = 20;
+
+        internal long TotalCount { get; private set; }
+
+
+        internal void Push(string message)
+        {
+            TotalCount++;
+
+            while (Count > MaxMessagesCount)
+                TryDequeue(out _);
+
+            Enqueue(message);
+        }
+
+
+        internal new void Clear()
+        {
+            base.Clear();
+
+            TotalCount = 0L;
+        }
+    }
+
+    internal sealed class CDict<T> : ConcurrentDictionary<string, T> where T : class, new()
+    {
+        public new T this[string key] => GetOrAdd(key);
+
+        internal T GetOrAdd(string key)
+        {
+            if (!TryGetValue(key, out T value))
+            {
+                base[key] = new T();
+
+                return base[key];
+            }
+
+            return value;
+        }
+    }
+
     internal sealed class MessageBuilder
     {
-        private const int MaxSensorMessages = 20;
+        //private const int MaxSensorMessages = 20;
 
-        private readonly ConcurrentDictionary<Guid, ConcurrentDictionary<string, List<MessageInfo>>> _messages = new();
+        //private readonly ConcurrentDictionary<Guid, ConcurrentDictionary<string, List<MessageInfo>>> _messages = new();
+
+        private readonly CDict<CDict<CDict<CDict<CQueue>>>> _messageTree = new();
 
         internal DateTime LastSentTime { get; private set; } = DateTime.UtcNow;
 
 
         internal void AddMessage(BaseSensorModel sensor)
         {
-            var productId = sensor.RootProductId;
+            //var productId = sensor.RootProductId;
 
-            if (!_messages.ContainsKey(productId))
-                _messages[productId] = new ConcurrentDictionary<string, List<MessageInfo>>();
+            //if (!_messages.ContainsKey(productId))
+            //    _messages[productId] = new ConcurrentDictionary<string, List<MessageInfo>>();
 
-            var nodePath = GetNodePath(sensor.Path);
-            if (!_messages[productId].ContainsKey(nodePath))
-                _messages[productId].TryAdd(nodePath, new List<MessageInfo>());
+            var product = sensor.ParentProduct.DisplayName;
+            var nodePath = sensor.ParentProduct.Path;
 
-            _messages[productId][nodePath].Add(GenerateMessageInfo(sensor));
+            //var nodePath = GetNodePath(sensor.Path);
+            //if (!_messages[productId].ContainsKey(nodePath))
+            //    _messages[productId].TryAdd(nodePath, new List<MessageInfo>());
+
+
+            var pathDict = _messageTree[product][nodePath];
+
+            var messages = sensor.ValidationResult.Message;
+            var result = $"{sensor.ValidationResult.Result}";
+
+            pathDict[result][messages].Push(sensor.DisplayName);
+            //_messages[productId][nodePath].Add(GenerateMessageInfo(sensor));
         }
+
+        //private void PushMessage(CDict<CDict<CQueue>> pathDict, BaseSensorModel model)
+        //{
+
+        //}
+
 
         internal string GetAggregateMessage()
         {
@@ -39,7 +101,7 @@ namespace HSMServer.Notifications
             {
                 var productName = messages?.FirstOrDefault().Value?.FirstOrDefault().ProductName;
                 builder.AppendLine(productName);
-             
+
                 foreach (var (nodepath, messagesQueue) in messages)
                 {
                     foreach (var messageInfo in messagesQueue)
@@ -55,7 +117,7 @@ namespace HSMServer.Notifications
                     foreach (var (message, output) in response)
                     {
                         var outputSensors = GenerateOutputSensors(output);
-                        
+
                         builder.Append($"    {(string.IsNullOrEmpty(nodepath) ? "/" : $"{nodepath}")}: {outputSensors} -> {message}");
                     }
 
@@ -92,7 +154,7 @@ namespace HSMServer.Notifications
         private static MessageInfo GenerateMessageInfo(BaseSensorModel sensor)
         {
             var builder = new StringBuilder(1 << 2);
- 
+
             builder.Append($"{sensor.ValidationResult.Result}");
             if (!sensor.ValidationResult.IsSuccess)
                 builder.Append($" ({sensor.ValidationResult.Message})");
@@ -117,15 +179,15 @@ namespace HSMServer.Notifications
 
         private readonly struct MessageInfo
         {
-            internal DateTime SensorValueTime { get; init; }
+            //internal DateTime SensorValueTime { get; init; }
 
-            internal string SensorPath { get; init; }
+            //internal string SensorPath { get; init; }
 
-            internal string ProductName { get; init; }
+            //internal string ProductName { get; init; }
 
             internal string Message { get; init; }
-            
-            internal string SensorDisplayName { get; init; }
+
+            internal string SensorDName { get; init; }
         }
     }
 }
