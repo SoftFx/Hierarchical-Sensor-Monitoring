8ca2b1989eb62d880dffe4f9841e2810b10057d9
palina.shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2023-02-14 13:12:22 +0300

    logger manager refactoring

--- src/collector/HSMDataCollector/Logging/LoggerManager.cs
+++ src/collector/HSMDataCollector/Logging/LoggerManager.cs
@@ -8,7 +8,7 @@ namespace HSMDataCollector.Logging
 {
     internal sealed class LoggerManager
     {
-        private const string DefaultConfigPath = "collector.nlog.config";
+        private static readonly LoggerOptions _defaultOptions = new LoggerOptions().FillConfigPath();
 
 
         internal Logger Logger { get; private set; }
@@ -18,14 +18,26 @@ namespace HSMDataCollector.Logging
 
         internal void InitializeLogger(LoggerOptions options)
         {
-            var configPath = options?.ConfigPath;
-            if (string.IsNullOrEmpty(configPath))
-                configPath = Path.Combine(AppContext.BaseDirectory, DefaultConfigPath);
+            options = options?.FillConfigPath() ?? _defaultOptions;
 
-            var factory = new LogFactory(new XmlLoggingConfiguration(configPath));
+            var factory = new LogFactory(new XmlLoggingConfiguration(options.ConfigPath));
 
             Logger = factory.GetLogger(nameof(DataCollector));
-            WriteDebug = options?.WriteDebug ?? false;
+            WriteDebug = options.WriteDebug;
+        }
+    }
+
+
+    internal static class LoggerOptionsExtension
+    {
+        internal static LoggerOptions FillConfigPath(this LoggerOptions options)
+        {
+            const string DefaultConfigPath = "collector.nlog.config";
+
+            if (string.IsNullOrEmpty(options.ConfigPath))
+                options.ConfigPath = Path.Combine(AppContext.BaseDirectory, DefaultConfigPath);
+
+            return options;
         }
     }
 }
