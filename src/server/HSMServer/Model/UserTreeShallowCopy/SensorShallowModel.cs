9ca9d394fd2aa3c36d2dbe33d3933b61c107fa28
Savely Volodkov <s.volodkov@syberry.com>
2023-02-24 15:15:52 +0300

    To display icon or not now calculates inside SensorShallowModel.cs

--- src/server/HSMServer/Model/UserTreeShallowCopy/SensorShallowModel.cs
+++ src/server/HSMServer/Model/UserTreeShallowCopy/SensorShallowModel.cs
@@ -1,4 +1,5 @@
-﻿using HSMServer.Core.Model;
+﻿using System;
+using HSMServer.Core.Model;
 using HSMServer.Model.Authentication;
 using HSMServer.Model.TreeViewModel;
 
@@ -10,11 +11,18 @@ namespace HSMServer.Model.UserTreeShallowCopy
 
         public override bool IsGroupsEnable { get; }
         
+        public bool IsAccountIgnoreIconShow { get; }
+        
+        public bool IsGroupIgnoreIconShow { get; }
+        
         internal SensorShallowModel(SensorNodeViewModel data, User user) : base(data, user)
         {
             IsAccountsEnable = user.Notifications.IsSensorEnabled(data.Id);
             IsGroupsEnable = data.RootProduct.Notifications.IsSensorEnabled(data.Id);
             IsIgnoredState = data.State == SensorState.Ignored;
+
+            IsGroupIgnoreIconShow = data.RootProduct.Notifications.IgnoredSensors.TryGetValue(data.Id, out var accountTime) && accountTime != DateTime.MaxValue;
+            IsAccountIgnoreIconShow = user.Notifications.IgnoredSensors.TryGetValue(data.Id, out var groupTime) && groupTime != DateTime.MaxValue;
         }
     }
 }
