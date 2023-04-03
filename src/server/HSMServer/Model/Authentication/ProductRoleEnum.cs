518c84b930a1eae82cbb9d50ad2fdd2b6e46ae90
Palina Shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2023-04-03 13:41:36 +0300

    Cjanged UI for editing user roles in edit folder page

--- src/server/HSMServer/Model/Authentication/ProductRoleEnum.cs
+++ src/server/HSMServer/Model/Authentication/ProductRoleEnum.cs
@@ -1,8 +1,12 @@
-﻿namespace HSMServer.Model.Authentication
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HSMServer.Model.Authentication
 {
     public enum ProductRoleEnum
     {
+        [Display(Name = "Manager")]
         ProductManager = 0,
+        [Display(Name = "Viewer")]
         ProductViewer = 1
     }
 }
