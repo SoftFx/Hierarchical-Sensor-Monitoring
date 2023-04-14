04584590a0b153361a91d9a2b89c80225dbcf453
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2023-04-14 16:26:01 +0300

    Collector: some fixes have been added. CollevtorAlive sensor has been renamed to CollectorHeartbeat

--- src/collector/HSMDataCollector/DefaultSensors/Other/ProductInfoSensor.cs
+++ src/collector/HSMDataCollector/DefaultSensors/Other/ProductInfoSensor.cs
@@ -1,6 +1,6 @@
+using HSMDataCollector.Options;
 using System;
 using System.Threading.Tasks;
-using HSMDataCollector.Options;
 
 namespace HSMDataCollector.DefaultSensors.Other
 {
@@ -8,21 +8,21 @@ namespace HSMDataCollector.DefaultSensors.Other
     {
         private readonly string _version;
         private readonly DateTime _startTime;
-        
+
         protected override string SensorName => "Product Version";
 
-        
+
         public ProductInfoSensor(ProductInfoOptions options) : base(options)
         {
             _version = options.Version;
             _startTime = options.StartTime;
         }
-        
+
+
         internal override Task<bool> Start()
         {
-            SendValue($"Version: {_version}, Start: {_startTime}");
+            SendValue($"Version: {_version}", comment: $"Start: {_startTime}");
             return base.Start();
         }
-
     }
 }
