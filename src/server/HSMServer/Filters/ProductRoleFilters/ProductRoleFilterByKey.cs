618cc1ca1277a6ddcc8934de4c12da061c4cae2d
Palina Shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2023-04-27 12:10:14 +0300

    Added folder role filters for folder actions

--- src/server/HSMServer/Filters/ProductRoleFilters/ProductRoleFilterByKey.cs
+++ src/server/HSMServer/Filters/ProductRoleFilters/ProductRoleFilterByKey.cs
@@ -14,7 +14,7 @@ namespace HSMServer.Filters.ProductRoleFilters
         public ProductRoleFilterByKey(params ProductRoleEnum[] roles) : base(roles) { }
 
 
-        protected override Guid? GetProductId(object arg, ActionExecutingContext _) =>
+        protected override Guid? GetEntityId(object arg, ActionExecutingContext _) =>
             arg is EditAccessKeyViewModel key ? SensorPathHelper.DecodeGuid(key.EncodedProductId) : null;
     }
 }
