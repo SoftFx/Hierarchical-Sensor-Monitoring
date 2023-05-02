037a7ea072a56dc7e6a1d5736b6c4742335d1f6c
Palina Shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2023-05-02 16:12:17 +0300

    TAM-701. HSM, Site: logic for enable grafana integration for sensor should be added

--- src/server/HSMServer/Model/UserTreeShallowCopy/SensorShallowModel.cs
+++ src/server/HSMServer/Model/UserTreeShallowCopy/SensorShallowModel.cs
@@ -7,25 +7,27 @@ namespace HSMServer.Model.UserTreeShallowCopy
 {
     public sealed class SensorShallowModel : BaseNodeShallowModel<SensorNodeViewModel>
     {
+        public override bool IsGrafanaEnabled { get; }
+
         public override bool IsAccountsEnable { get; }
+
+        public override bool IsAccountsIgnore { get; }
 
         public override bool IsGroupsEnable { get; }
 
-
-        public bool IsAccountIgnoreIconShow { get; }
-
-        public bool IsGroupIgnoreIconShow { get; }
+        public override bool IsGroupsIgnore { get; }
 
 
         internal SensorShallowModel(SensorNodeViewModel data, User user) : base(data, user)
         {
+            IsGrafanaEnabled = data.Integration.HasFlag(Integration.Grafana);
             IsAccountsEnable = user.Notifications.IsSensorEnabled(data.Id);
             IsGroupsEnable = data.RootProduct.Notifications.IsSensorEnabled(data.Id);
 
             _mutedValue = data.State == SensorState.Muted;
 
-            IsGroupIgnoreIconShow = data.RootProduct.Notifications.IgnoredSensors.TryGetValue(data.Id, out var accountTime) && accountTime != DateTime.MaxValue;
-            IsAccountIgnoreIconShow = user.Notifications.IgnoredSensors.TryGetValue(data.Id, out var groupTime) && groupTime != DateTime.MaxValue;
+            IsGroupsIgnore = data.RootProduct.Notifications.IgnoredSensors.TryGetValue(data.Id, out var accountTime) && accountTime != DateTime.MaxValue;
+            IsAccountsIgnore = user.Notifications.IgnoredSensors.TryGetValue(data.Id, out var groupTime) && groupTime != DateTime.MaxValue;
         }
     }
 }
