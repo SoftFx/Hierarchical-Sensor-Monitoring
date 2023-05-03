e14d954915dd6b0f00ec95a12c6b834582f2ea98
Palina Shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2023-05-03 16:18:28 +0300

    Added input group for data alerts

---
+++ src/server/HSMServer/Model/DataAlerts/DataAlertViewModelBase.cs
@@ -0,0 +1,53 @@
+﻿using HSMServer.Extensions;
+using HSMServer.Model.TreeViewModel;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace HSMServer.Model.DataAlerts
+{
+    public enum Operation
+    {
+        [Display(Name = "<=")]
+        LessThanOrEqual,
+        [Display(Name = "<")]
+        LessThan,
+        [Display(Name = ">")]
+        GreatedThan,
+        [Display(Name = ">=")]
+        GreaterThanOrEqual,
+    }
+
+
+    public abstract class DataAlertViewModelBase
+    {
+        private readonly List<SensorStatus> _statuses = new() { SensorStatus.Error, SensorStatus.Warning };
+
+
+        protected abstract List<string> Properties { get; }
+
+        protected abstract List<Operation> Actions { get; }
+
+
+        public string Property { get; set; }
+
+        public Operation Action { get; set; }
+
+        public string Value { get; set; }
+
+        public SensorStatus Status { get; set; }
+
+        public string Comment { get; set; }
+
+
+        public List<SelectListItem> PropertiesItems => Properties.Select(p => new SelectListItem(p, p)).ToList();
+
+        public List<SelectListItem> ActionsItems => Actions.Select(a => new SelectListItem(a.GetDisplayName(), $"{a}")).ToList();
+
+        public List<SelectListItem> StatusesItems => _statuses.Select(s => new SelectListItem(s.GetDisplayName(), $"{s}")).ToList();
+
+
+        public DataAlertViewModelBase() { }
+    }
+}
---
+++ src/server/HSMServer/Model/DataAlerts/IntegerDataAlertViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace HSMServer.Model.DataAlerts
+{
+    public sealed class IntegerDataAlertViewModel : DataAlertViewModelBase
+    {
+        protected override List<string> Properties => new() { "Value" };
+
+        protected override List<Operation> Actions => new()
+        {
+            Operation.LessThanOrEqual,
+            Operation.LessThan,
+            Operation.GreatedThan,
+            Operation.GreaterThanOrEqual,
+        };
+
+
+        public IntegerDataAlertViewModel() { }
+    }
+}
