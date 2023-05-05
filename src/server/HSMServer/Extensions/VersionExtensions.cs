4b861b9558cd02bfca11fa0fb906c2d3045c19e5
Savely Volodkov <s.volodkov@syberry.com>
2023-05-05 14:50:24 +0300

    Added version extension

---
+++ src/server/HSMServer/Extensions/VersionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace HSMServer.Extensions;
+
+public static class VersionExtensions
+{
+    public static string RemoveTailZeroes(this Version version) => 
+        version.Revision == 0 ? version.ToString(3) : version.ToString();
+}
