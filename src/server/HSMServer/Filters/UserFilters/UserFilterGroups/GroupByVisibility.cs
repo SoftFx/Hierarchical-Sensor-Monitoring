97bccdad2db574239ad037bb2c1cb4ac9994c9f3
pawgyrocopter <94412374+pawgyrocopter@users.noreply.github.com>
2023-05-17 13:55:32 +0300

    Icons filter (#301)
    * Added show icons filter
    
    * Renamings
    
    ---------
    
    Co-authored-by: Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>

--- src/server/HSMServer/Filters/UserFilters/UserFilterGroups/GroupByVisibility.cs
+++ src/server/HSMServer/Filters/UserFilters/UserFilterGroups/GroupByVisibility.cs
@@ -2,12 +2,14 @@
 {
     public class GroupByVisibility : UserFilterGroupBase
     {
-        internal override FilterProperty[] Properties => new[] { Empty };
+        internal override FilterProperty[] Properties => new[] { Empty, Icons };
 
         internal override FilterGroupType Type => FilterGroupType.ByVisibility;
 
 
         public FilterProperty Empty { get; init; } = new("Empty sensors", true);
+        
+        public FilterProperty Icons { get; init; } = new("Icons", true);
 
         public GroupByVisibility() { }
 
