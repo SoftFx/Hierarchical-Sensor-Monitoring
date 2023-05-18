a416e116dc80cb756a97d4471510d8e7f137d247
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-05-18 14:33:49 +0300

    ValidationAttribute removed

--- src/server/HSMServer/Attributes/AccessKeyPermissionsValidationAttribute.cs
+++ src/server/HSMServer/Attributes/AccessKeyPermissionsValidationAttribute.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using HSMServer.Core.Model;
+﻿using HSMServer.Core.Model;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using HSMServer.Core.Cache;
-using HSMServer.Model.ViewModel;
 
 namespace HSMServer.Attributes
 {
@@ -11,36 +7,5 @@ namespace HSMServer.Attributes
     {
         public override bool IsValid(object value) =>
             value is KeyPermissions permissions && permissions != 0;
-    }
-
-    public class RequiredKeyPermissionsAttribute : ValidationAttribute
-    {
-        private readonly ValidationResult _validationError = new (ValidationErrorMessage);
-        private readonly KeyPermissions _permissions;
-
-        
-        public const string ValidationErrorMessage = "There is no suitable access key for this operation";
-        
-        
-        public RequiredKeyPermissionsAttribute(KeyPermissions permissions)
-        {
-            _permissions = permissions;
-        }
-        
-        protected override ValidationResult IsValid(object value, ValidationContext context)
-        {
-            var result = false;
-            
-            if (value is Guid id)
-            {
-                var cache = (ITreeValuesCache) context.GetService(typeof(ITreeValuesCache));
-                var product = cache?.GetProduct(id);
-                
-                if (product != null)
-                    result = product.AccessKeys.Values.Any(x => x.IsValid(_permissions, out var message));
-            }
-
-            return result ? ValidationResult.Success : _validationError;
-        }
     }
 }
