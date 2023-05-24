102599a9bd5e9b685518be05a4de9a6874155b8c
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2023-05-24 12:44:34 +0300

    Saving for error request has been added

--- src/server/HSMServer/Middleware/LoggingExceptionMiddleware.cs
+++ src/server/HSMServer/Middleware/LoggingExceptionMiddleware.cs
@@ -2,12 +2,16 @@
 using HSMSensorDataObjects;
 using Microsoft.AspNetCore.Http;
 using NLog;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HSMServer.Middleware
 {
     internal sealed class LoggingExceptionMiddleware
     {
+        private const int MaxSizeForDeserializeContent = int.MaxValue;
+
         private readonly Logger _logger = LogManager.GetCurrentClassLogger();
         private readonly RequestDelegate _next;
 
@@ -17,10 +21,13 @@ namespace HSMServer.Middleware
             _next = next;
         }
 
+
         public async Task InvokeAsync(HttpContext context)
         {
             try
             {
+                context.Request.EnableBuffering();
+
                 await _next(context);
             }
             catch
@@ -31,6 +38,18 @@ namespace HSMServer.Middleware
                     request.Headers.TryGetValue(nameof(BaseRequest.Key), out var key);
 
                     _logger.Error($"Path {request.Path}, access key = {key}, remote id, port: {context.Connection.RemoteIpAddress}:{context.Connection.RemotePort}, content size = {request.ContentLength}");
+
+                    if (request.ContentLength <= MaxSizeForDeserializeContent)
+                    {
+                        request.Body.Position = 0;
+
+                        using StreamReader reader = new(request.Body, Encoding.UTF8, true, 1024, true);
+                        var bodyStr = await reader.ReadToEndAsync();
+
+                        _logger.Error($"Error request: {bodyStr}");
+
+                        request.Body.Position = 0;
+                    }
                 }
 
                 throw;
