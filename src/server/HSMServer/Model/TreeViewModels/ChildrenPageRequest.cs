6f9fab62bf5f537e65cc6d53e9ac6b55f7cf8f82
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-06-16 14:51:04 +0300

    Code clean up.

--- src/server/HSMServer/Model/TreeViewModels/ChildrenPageRequest.cs
+++ src/server/HSMServer/Model/TreeViewModels/ChildrenPageRequest.cs
@@ -2,5 +2,5 @@ namespace HSMServer.Model.TreeViewModels;
 
 public record ChildrenPageRequest(string TypeId, int CurrentPage, int PageSize)
 {
-    public bool IsNodes => TypeId.Replace("grid", string.Empty).Replace("list", string.Empty) == "Nodes";
+    public bool IsNodes => TypeId.Contains("Nodes");
 }
