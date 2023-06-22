37efa6dcfaff711d150f0b548b0efd3bbdae2746
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-06-22 13:17:30 +0300

    JournalValueEntity removed.

--- src/database/HSMDatabase.AccessManager/DatabaseEntities/JournalValueEntity.cs
+++
@@ -1,8 +0,0 @@
-namespace HSMDatabase.AccessManager.DatabaseEntities;
-
-public record JournalValueEntity
-{
-    public Key JournalKey { get; init; }
-    
-    public string Value { get; init; }
-}
