7f4ca3199b4ec3e5cec75f77790c7ebb96eb55bd
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-06-27 11:43:11 +0300

    new property added for the key

--- src/database/HSMDatabase.AccessManager/DatabaseEntities/TreeEntity/JournalEntity.cs
+++ src/database/HSMDatabase.AccessManager/DatabaseEntities/TreeEntity/JournalEntity.cs
@@ -11,33 +11,52 @@ public readonly struct Key
 {
     private const int GuidSize = 16;
 
+    private const int StructSize = GuidSize + sizeof(long) + sizeof(UpdateType);
+
 
     public Guid Id { get; init; }
 
     public long Time { get; init; }
 
+    public UpdateType UpdateType { get; init; }
     
-    public Key(Guid guid, long time)
+    
+    public Key(Guid id, long time)
     {
-        Id = guid;
+        Id = id;
         Time = time;
+    }
+
+    public Key(Guid id, long time, UpdateType updateType)
+    {
+        Id = id;
+        Time = time;
+        UpdateType = updateType;
     }
 
     public byte[] GetBytes()
     {
-        
-        Span<byte> result = stackalloc byte[GuidSize + sizeof(long)];
+        Span<byte> result = stackalloc byte[StructSize];
+        result[^1] = (byte)UpdateType;
 
         return Id.TryWriteBytes(result) && BitConverter.TryWriteBytes(result[16..], Time) ? result.ToArray() : Array.Empty<byte>();
     }
     
     public static Key FromBytes(byte[] bytes)
     {
-        if (bytes == null || bytes.Length != 24)
+        if (bytes == null || bytes.Length != StructSize)
             return default;
 
         var id = new Guid(new ReadOnlySpan<byte>(bytes[..16]));
         var time = BitConverter.ToInt64(bytes, 16);
-        return new Key(id, time);
+        var updateType = bytes[^1];
+        
+        return new Key(id, time, (UpdateType)updateType);
     }
+}
+
+public enum UpdateType : byte
+{
+    Changes,
+    Actions
 }
