77f3b620e061491f3d51615559be5d3ce239f5ef
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-07-07 18:48:55 +0300

    Long value now stored reversed.

--- src/database/HSMDatabase.AccessManager/DatabaseEntities/TreeEntity/JournalEntity.cs
+++ src/database/HSMDatabase.AccessManager/DatabaseEntities/TreeEntity/JournalEntity.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Text;
-
 namespace HSMDatabase.AccessManager.DatabaseEntities;
 
 public enum RecordType : byte
@@ -38,7 +36,15 @@ public readonly struct JournalKey
     {
         Span<byte> result = stackalloc byte[StructSize];
         result[GuidSize] = (byte)Type;
-        return Id.TryWriteBytes(result) && BitConverter.TryWriteBytes(result[(GuidSize + TypeSize)..], Time) ? result.ToArray() : Array.Empty<byte>();
+        
+        if (!Id.TryWriteBytes(result))
+            return Array.Empty<byte>();
+
+        if (!BitConverter.TryWriteBytes(result[(GuidSize + TypeSize)..], Time))
+            return Array.Empty<byte>();
+        
+        result[(GuidSize + TypeSize)..].Reverse();
+        return result.ToArray();
     }
 
     public static JournalKey FromBytes(byte[] bytes)
@@ -48,6 +54,7 @@ public readonly struct JournalKey
 
         var id = new Guid(bytes[..GuidSize]);
         var journalType = bytes[GuidSize];
+        Array.Reverse(bytes, GuidSize + TypeSize, StructSize - (GuidSize + TypeSize));
         var time = BitConverter.ToInt64(bytes, GuidSize + TypeSize);
 
         return new JournalKey(id, time, (RecordType)journalType);
