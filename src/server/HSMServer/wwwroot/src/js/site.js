00ba977316adcb52b91ad258099bfb8e97693366
pawgyrocopter <94412374+pawgyrocopter@users.noreply.github.com>
2023-07-26 14:13:56 +0300

    TAM-961 HSM, Policy: Send test message should added as toast (#399)
    * Added test toast message.
    
    * Refactoring.
    
    * Refactoring.
    
    * Refactoring.
    
    * Added default build for regular sensors.
    
    * Refactoring.
    
    ---------
    
    Co-authored-by: Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>

--- src/server/HSMServer/wwwroot/src/js/site.js
+++ src/server/HSMServer/wwwroot/src/js/site.js
@@ -10,8 +10,9 @@
     showToast("Copied!");
 }
 
-window.showToast = function (message) {
+window.showToast = function (message, header = 'Info') {
     document.getElementById('toast_body').innerHTML = message;
+    document.getElementById('toast_header').innerHTML = header;
     let currentToast = document.getElementById('liveToast')
     let currentToastInstance = bootstrap.Toast.getOrCreateInstance(currentToast)
     currentToastInstance.show();
