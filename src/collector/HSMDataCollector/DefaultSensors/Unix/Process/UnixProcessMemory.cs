e3fae8edaa6f51e18736ca2dfc4f0688346f6e01
AndrewKhloptsau <86781681+AndrewKhloptsau@users.noreply.github.com>
2023-07-31 17:16:55 +0300

    HSM 3.20.10 (#413)
    * TAM-1017 Edit product/folder page; Telegram tab: developer console error (#410)
    
    * Fixed console error in devtools, removed min status level from edit folder page.
    
    * Version up 3.20.10
    
    ---------
    
    Co-authored-by: Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
    
    * TAM-1018: Status alert by default has been added
    
    * New bar infrastructure (#411)
    
    * New infrastructure for PublicMonitoringBar logic has been added
    
    * TAM-899: Status code for Connection result has been added
    
    * Public interface has been simplified
    
    * Old bar implementation has been removed
    
    * Percentiles have been changed to const values
    
    * Precision for bars has been added
    
    * Some fixes have been added
    
    ---------
    
    Co-authored-by: AndreiKhloptsau <andrei.khloptsau@soft.fx-lv>
    
    * Collector 3.1.7 with direct link has been added
    
    ---------
    
    Co-authored-by: pawgyrocopter <94412374+pawgyrocopter@users.noreply.github.com>
    Co-authored-by: Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
    Co-authored-by: AndreiKhloptsau <andrei.khloptsau@soft.fx-lv>

--- src/collector/HSMDataCollector/DefaultSensors/Unix/Process/UnixProcessMemory.cs
+++ src/collector/HSMDataCollector/DefaultSensors/Unix/Process/UnixProcessMemory.cs
@@ -3,7 +3,7 @@ using HSMDataCollector.Options;
 
 namespace HSMDataCollector.DefaultSensors.Unix
 {
-    internal sealed class UnixProcessMemory : BarMonitoringSensorBase<DoubleMonitoringBar, double>
+    internal sealed class UnixProcessMemory : CollectableBarMonitoringSensorBase<DoubleMonitoringBar, double>
     {
         protected override string SensorName => "Process memory MB";
 
