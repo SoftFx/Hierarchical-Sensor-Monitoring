bfe47d39407c12226b9310caecdc7ac1a9b4cb72
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-08-02 12:36:47 +0300

    Icon changed + remove some buttons from modebar.

--- src/server/HSMServer/wwwroot/src/js/plotting.js
+++ src/server/HSMServer/wwwroot/src/js/plotting.js
@@ -1,19 +1,20 @@
 ﻿window.displayGraph = function(graphData, graphType, graphElementId, graphName) {
     let convertedData = convertToGraphData(graphData, graphType, graphName);
     let zoomData = getPreviousZoomData(graphElementId);
-    var icon1 = {
+    var plotIcon = {
         'width': 500,
         'height': 600,
-        'path': 'M224 512c35.32 0 63.97-28.65 63.97-64H160.03c0 35.35 28.65 64 63.97 64zm215.39-149.71c-19.32-20.76-55.47-51.99-55.47-154.29 0-77.7-54.48-139.9-127.94-155.16V32c0-17.67-14.32-32-31.98-32s-31.98 14.33-31.98 32v20.84C118.56 68.1 64.08 130.3 64.08 208c0 102.3-36.15 133.53-55.47 154.29-6 6.45-8.66 14.16-8.61 21.71.11 16.4 12.98 32 32.1 32h383.8c19.12 0 32-15.6 32.1-32 .05-7.55-2.61-15.27-8.61-21.71z'
+        'path': 'M352 96c0 14.3-3.1 27.9-8.8 40.2L396 227.4c-23.7 25.3-54.2 44.1-88.5 53.6L256 192h0 0l-68 117.5c21.5 6.8 44.3 10.5 68.1 10.5c70.7 0 133.8-32.7 174.9-84c11.1-13.8 31.2-16 45-5s16 31.2 5 45C428.1 341.8 347 384 256 384c-35.4 0-69.4-6.4-100.7-18.1L98.7 463.7C94 471.8 87 478.4 78.6 482.6L23.2 510.3c-5 2.5-10.9 2.2-15.6-.7S0 501.5 0 496V440.6c0-8.4 2.2-16.7 6.5-24.1l60-103.7C53.7 301.6 41.8 289.3 31.2 276c-11.1-13.8-8.8-33.9 5-45s33.9-8.8 45 5c5.7 7.1 11.8 13.8 18.2 20.1l69.4-119.9c-5.6-12.2-8.8-25.8-8.8-40.2c0-53 43-96 96-96s96 43 96 96zm21 297.9c32.6-12.8 62.5-30.8 88.9-52.9l43.7 75.5c4.2 7.3 6.5 15.6 6.5 24.1V496c0 5.5-2.9 10.7-7.6 13.6s-10.6 3.2-15.6 .7l-55.4-27.7c-8.4-4.2-15.4-10.8-20.1-18.9L373 393.9zM256 128a32 32 0 1 0 0-64 32 32 0 1 0 0 64z'
     }
     
     var serviceButtonName = 'Show service status';
     var config = { 
         responsive: true,
+        displaylogo: false,
         modeBarButtonsToAdd: [
             {
                 name: serviceButtonName, //changing name doesn't work
-                icon: icon1,
+                icon: plotIcon,
                 click: function(gd) {
                     let graph = $(`#${graphElementId}`)[0];
                     let graphLength = graph._fullData.length;
@@ -45,6 +46,14 @@
                     }
                 }},
         ],
+        modeBarButtonsToRemove: [
+            'pan',
+            'lasso2d',
+            'pan2d',
+            'select2d',
+            'autoScale2d',
+            'resetScale2d'
+        ]
     }
 
     if (graphType === "9")
