3af22057d02c5985a912a94dde1af6db1cfb85ca
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2023-08-08 13:55:26 +0300

    Windows prototypes have been added

---
+++ src/collector/HSMDataCollector/Prototypes/Bases/BarBaseMonitoringPrototype.cs
@@ -0,0 +1,38 @@
+﻿using HSMDataCollector.Options;
+using HSMDataCollector.SensorsMetainfo;
+
+namespace HSMDataCollector.Prototypes
+{
+    internal abstract class BarBaseMonitoringPrototype<MetainfoType, OptionsType> : Prototype<MetainfoType, OptionsType>
+        where MetainfoType : MonitoringSensorMetainfo, new()
+        where OptionsType : BarSensorOptions
+    {
+        protected const string SystemPath = ".Default";
+
+
+        protected abstract string SensorName { get; }
+
+        protected virtual string Category { get; }
+
+
+        protected BarBaseMonitoringPrototype()
+        {
+            Path = BuildPath(SystemPath, Category, SensorName);
+        }
+
+
+        protected override MetainfoType Apply(MetainfoType info, OptionsType options)
+        {
+            info.CollectBarPeriod = options.CollectBarPeriod;
+            info.BarPeriod = options.BarPeriod;
+            info.Precision = options.Precision;
+
+            return info;
+        }
+    }
+
+
+    internal abstract class BarBaseMonitoringPrototype : BarBaseMonitoringPrototype<MonitoringSensorMetainfo, BarSensorOptions>
+    {
+    }
+}
--- src/collector/HSMDataCollector/Prototypes/DiskMonitoringPrototypes.cs
+++ src/collector/HSMDataCollector/Prototypes/DiskMonitoringPrototypes.cs
@@ -1,17 +1,25 @@
-﻿using System;
+﻿using HSMDataCollector.Prototypes;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
 namespace HSMDataCollector.Options
 {
-    internal sealed class DiskMonitoringPrototype : MonitoringPrototype<DiskSensorOptions>
+    internal sealed class DiskMonitoringPrototype : <DiskSensorOptions>
     {
         protected override string NodePath { get; } = "Disk monitoring";
 
 
+       
+    }
+
+    internal abstract class DisksMonitoringPrototype : BarBaseMonitoringPrototype<>
+    {
+        protected override string Category => "Disk monitoring";
+
         internal IEnumerable<DiskSensorOptions> GetAllDisksOptions(DiskSensorOptions userOptions)
         {
-            var diskOptions = GetAndFill(userOptions);
+            var diskOptions = Get(userOptions);
 
             foreach (var drive in DriveInfo.GetDrives())
             {
