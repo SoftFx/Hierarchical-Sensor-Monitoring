a5b751a28f05c4ea7339b536ff8689d7da6aaca3
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2023-08-29 14:52:21 +0300

    Ccollections have been moved to Common

---
+++ src/server/HSMCommon/Collections/CDict.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Concurrent;
+
+namespace HSMCommon.Collections
+{
+    public sealed class CDict<T> : CDictBase<string, T> where T : new() { }
+
+
+    public abstract class CDictBase<T, U> : ConcurrentDictionary<T, U> where U : new()
+    {
+        public new U this[T key] => GetOrAdd(key);
+
+
+        public U GetOrAdd(T key)
+        {
+            if (!TryGetValue(key, out U value))
+            {
+                base[key] = new U();
+
+                return base[key];
+            }
+
+            return value;
+        }
+    }
+}
