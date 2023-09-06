3eaf548f9144cd215f4cf519b4f99d055182f87d
pawgyrocopter <94412374+pawgyrocopter@users.noreply.github.com>
2023-09-06 17:27:27 +0300

    PLotting imrove. (#476)
    * Show/Hide service heart beat plot changed to Show/Hide service alive plot
    
    * Added new way of building dots for enum plot.
    
    * Refactoring.
    
    * Introduced new endpoint for getting real and plot types for sensor.
    
    * Added new icons for service status and service alive plots.
    
    * Refactoring.
    
    * Added server method for getting sensor types.
    
    * Added some imrovements for getting plot id.
    
    * Now background path is shown, current sensorid for backroung is now stored.
    
    * Refactoring.
    
    ---------
    
    Co-authored-by: Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>

--- src/server/HSMServer/wwwroot/src/js/plotting.js
+++ src/server/HSMServer/wwwroot/src/js/plotting.js
@@ -1,4 +1,15 @@
-﻿import {BarPLot, BoolPlot, DoublePlot, EnumPlot, IntegerPlot, Plot, TimeSpanPlot} from "./plots";
+﻿import {
+    BarPLot,
+    BoolPlot,
+    DoublePlot,
+    EnumPlot,
+    IntegerPlot,
+    Plot,
+    ServiceAliveIcon,
+    ServiceStatusIcon,
+    TimeSpanPlot
+} from "./plots";
+
 
 window.barGraphData = {
     plot: undefined,
@@ -46,26 +57,17 @@ window.removeBarPlot = function (name, isInit = false) {
     }
 }
 
-window.displayGraph = function (graphData, graphType, graphElementId, graphName) {
+window.displayGraph = function (graphData, sensorTypes, graphElementId, graphName) {
     barGraphData.graph.id = graphElementId
     barGraphData.graph.self = $(`#${graphElementId}`)[0];
-    let plot = convertToGraphData(graphData, graphType, graphName);
-    let zoomData = getPreviousZoomData(graphElementId);
-    var plotIcon = {
-        'width': 500,
-        'height': 600,
-        'path': 'M352 96c0 14.3-3.1 27.9-8.8 40.2L396 227.4c-23.7 25.3-54.2 44.1-88.5 53.6L256 192h0 0l-68 117.5c21.5 6.8 44.3 10.5 68.1 10.5c70.7 0 133.8-32.7 174.9-84c11.1-13.8 31.2-16 45-5s16 31.2 5 45C428.1 341.8 347 384 256 384c-35.4 0-69.4-6.4-100.7-18.1L98.7 463.7C94 471.8 87 478.4 78.6 482.6L23.2 510.3c-5 2.5-10.9 2.2-15.6-.7S0 501.5 0 496V440.6c0-8.4 2.2-16.7 6.5-24.1l60-103.7C53.7 301.6 41.8 289.3 31.2 276c-11.1-13.8-8.8-33.9 5-45s33.9-8.8 45 5c5.7 7.1 11.8 13.8 18.2 20.1l69.4-119.9c-5.6-12.2-8.8-25.8-8.8-40.2c0-53 43-96 96-96s96 43 96 96zm21 297.9c32.6-12.8 62.5-30.8 88.9-52.9l43.7 75.5c4.2 7.3 6.5 15.6 6.5 24.1V496c0 5.5-2.9 10.7-7.6 13.6s-10.6 3.2-15.6 .7l-55.4-27.7c-8.4-4.2-15.4-10.8-20.1-18.9L373 393.9zM256 128a32 32 0 1 0 0-64 32 32 0 1 0 0 64z'
-    }
 
-    var serviceButtonName = 'Show/Hide service status plot';
-    var heartBeatButtonName = 'Show/Hide service heart beat plot';
-    var config = {
+    let plot = convertToGraphData(graphData, sensorTypes, graphName);
+    let zoomData = getPreviousZoomData(graphElementId);
+
+    let config = {
         responsive: true,
         displaylogo: false,
-        modeBarButtonsToAdd: [
-            getAddPlotButton(serviceButtonName, true, plotIcon, graphElementId, graphName),
-            getAddPlotButton(heartBeatButtonName, false, plotIcon, graphElementId, graphName),
-        ],
+        modeBarButtonsToAdd: getModeBarButtons(graphName, graphElementId),
         modeBarButtonsToRemove: [
             'pan',
             'lasso2d',
@@ -76,7 +78,7 @@ window.displayGraph = function (graphData, graphType, graphElementId, graphName)
         ]
     }
     let layout;
-    if (graphType === "9" || graphType === "7")
+    if (sensorTypes.plotType === 9 || sensorTypes.plotType === 7)
         layout = plot.getLayout();
     else {
         if (zoomData === undefined || zoomData === null)
@@ -126,35 +128,77 @@ function getPreviousZoomData(graphElementId) {
     return window.sessionStorage.getItem(graphElementId);
 }
 
-function convertToGraphData(graphData, graphType, graphName) {
+function convertToGraphData(graphData, sensorTypes, graphName) {
     let escapedData = JSON.parse(graphData);
 
-    switch (graphType) {
-        case "0":
+    switch (sensorTypes.plotType) {
+        case 0:
             return new BoolPlot(escapedData);
-        case "1":
+        case 1:
             return new IntegerPlot(escapedData);
-        case "2":
+        case 2:
             return new DoublePlot(escapedData);
-        case "4":
+        case 4:
             return new BarPLot(escapedData, graphName);
-        case "5":
+        case 5:
             return new BarPLot(escapedData, graphName);
-        case "7":
+        case 7:
             return new TimeSpanPlot(escapedData);
-        case "9":
+        case 9:
+            if (sensorTypes.realType === 0)
+                return new EnumPlot(escapedData, false)
+
             return new EnumPlot(escapedData, true);
         default:
             return undefined;
     }
 }
 
-function getAddPlotButton(name, isStatusService, icon, graphElementId, graphName) {
+function getBackgroundSensorId(id, isStatusService){
+    return $.ajax({
+        type: 'GET',
+        url: `${getBackgroundId}?currentId=${id}&isStatusService=${isStatusService}`,
+        cache: false,
+        async: false,
+    });
+}
+
+function getDataForPlotButton(graphName, isStatusService) {
+    let {from, to} = getFromAndTo(graphName);
+    let body = Data(to, from, 1, graphName)
+    return $.ajax({
+        type: 'POST',
+        data: JSON.stringify(body),
+        url: `${getSensorStatus}?isStatusService=${isStatusService}`,
+        contentType: 'application/json',
+        dataType: 'html',
+        cache: false,
+        async: true,
+    });
+}
+
+function getModeBarButtons(id, graphId){
+    let modeBarButtons = [];
+    let serviceButtonName = 'Show/Hide service status plot';
+    let heartBeatButtonName = 'Show/Hide service alive plot';
+  
+    $.when(getBackgroundSensorId(id, true), getBackgroundSensorId(id, false)).done(function(status, alive){
+        if(!jQuery.isEmptyObject(status[0]))
+            modeBarButtons.push(addPlotButton(serviceButtonName, true, ServiceStatusIcon, graphId, status[0].id, status[0].path))
+
+        if(!jQuery.isEmptyObject(alive[0]))
+            modeBarButtons.push(addPlotButton(heartBeatButtonName, false, ServiceAliveIcon, graphId, alive[0].id, alive[0].path))
+    });
+
+    return modeBarButtons;
+}
+
+function addPlotButton(name, isStatusService, icon, graphId, id, path){
     return {
-        name: name, //changing name doesn't work
+        name: name,
         icon: icon,
         click: function (gd) {
-            let graph = $(`#${graphElementId}`)[0];
+            let graph = $(`#${graphId}`)[0];
             let plots = graph._fullData;
             if (plots.length > 1) {
                 let indexToDelete = undefined;
@@ -166,31 +210,27 @@ function getAddPlotButton(name, isStatusService, icon, graphElementId, graphName
                 }
 
                 if (indexToDelete !== undefined)
-                    Plotly.deleteTraces(graphElementId, indexToDelete);
-            } 
-            else {
-                let {from, to} = getFromAndTo(graphName);
-                let body = Data(to, from, 1, graphName)
-                $.ajax({
-                    type: 'POST',
-                    data: JSON.stringify(body),
-                    url: `${getSensorStatus}?isStatusService=${isStatusService}`,
-                    contentType: 'application/json',
-                    dataType: 'html',
-                    cache: false,
-                    async: true,
-                    success: function (data) {
-                        let escapedData = JSON.parse(data);
-                        let ranges = graph._fullLayout.yaxis.range;
-                        let heatPlot = new EnumPlot(escapedData, isStatusService)
-                        Plotly.addTraces(graphElementId, heatPlot.getPlotData(name, ranges[0], ranges[1]));
-                        Plotly.update(graphElementId, {}, {hovermode: 'x'});
-                    }
-                })
+                    Plotly.deleteTraces(graphId, indexToDelete);
+            } else {
+                getDataForPlotButton(id, isStatusService).done(function (data){
+                    let escapedData = JSON.parse(data);
+                    let ranges = graph._fullLayout.yaxis.range;
+                    let heatPlot = new EnumPlot(escapedData, isStatusService)
+                    let updateLayout = {
+                        title: heatPlot.getTitle(path),
+                        hovermode: 'x'
+                    };
+
+                    Plotly.addTraces(graphId, heatPlot.getPlotData(name, ranges[0], ranges[1]));
+                    Plotly.update(graphId, {}, updateLayout);
+                });
             }
 
             if (graph._fullData.length === 1)
-                Plotly.update(graphElementId, {}, {hovermode: 'closest'});
+                Plotly.update(graphId, {}, {
+                    hovermode: 'closest',
+                    title: {}
+                });
         }
     }
 }
@@ -203,5 +243,6 @@ export const ServiceStatus = {
     5: ['#FFB403', 'Continue Pending'],
     6: ['#809EFF', 'Pause Pending'],
     7: ['#0314FF', 'Paused'],
+    8: ['#666699', 'Timeout'],
     0: ['#000000', 'Unknown']
 }
