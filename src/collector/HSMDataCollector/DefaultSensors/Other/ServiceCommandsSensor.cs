b5f78e4533840d56b27e27b270ff53be27b47f05
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2023-09-06 16:39:31 +0300

    TAM-1176: ServiceCOmmandSensor Update command has been improved

--- src/collector/HSMDataCollector/DefaultSensors/Other/ServiceCommandsSensor.cs
+++ src/collector/HSMDataCollector/DefaultSensors/Other/ServiceCommandsSensor.cs
@@ -6,19 +6,33 @@ namespace HSMDataCollector.DefaultSensors
 {
     internal class ServiceCommandsSensor : SensorBase<string>, IServiceCommandsSensor
     {
+        private const string ServiceUpdateCommand = "Service update";
+
+
         public ServiceCommandsSensor(InstantSensorOptions options) : base(options) { }
 
 
         public void SendCustomCommand(string command, string initiator) =>
-            SendValue(command, comment: $"Initiator: {initiator}, Time: {DateTime.UtcNow.ToString(DefaultTimeFormat)}");
+            SendValue(command, comment: $"Initiator: {initiator}");
 
 
         public void SendRestart(string initiator) => SendCustomCommand("Service restart", initiator);
 
-        public void SendUpdate(string initiator) => SendCustomCommand("Service update", initiator);
-
         public void SendStart(string initiator) => SendCustomCommand("Service start", initiator);
 
         public void SendStop(string initiator) => SendCustomCommand("Service stop", initiator);
+
+
+        public void SendUpdate(string initiator) => SendCustomCommand(ServiceUpdateCommand, initiator);
+
+        public void SendUpdate(string initiator, Version newVersion, Version oldVersion = null) =>
+            SendUpdate(initiator, newVersion?.ToString(), oldVersion?.ToString());
+
+        public void SendUpdate(string initiator, string newVersion, string oldVersion = null)
+        {
+            var message = string.IsNullOrEmpty(oldVersion) ? $"to {newVersion}" : $"from {oldVersion} to {newVersion}";
+
+            SendCustomCommand($"{ServiceUpdateCommand} {message}", initiator);
+        }
     }
 }
