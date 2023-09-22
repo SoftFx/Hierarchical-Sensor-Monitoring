95bacca4239302105ec4ea865986ab1e5af8f6a7
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-09-22 18:26:39 +0300

    Added description for windows logs sensors.

--- src/collector/HSMDataCollector/Prototypes/Collections/WindowsLogPrototype.cs
+++ src/collector/HSMDataCollector/Prototypes/Collections/WindowsLogPrototype.cs
@@ -5,12 +5,17 @@ namespace HSMDataCollector.Prototypes.Collections
 {
     internal abstract class WindowsLogPrototype : InstantSensorOptionsPrototype<InstantSensorOptions>
     {
+        internal const string BaseDescription = "The sensor reads Windows Logs and sends all logs with {0} status. The information is read using " +
+                                                "[**Event log**](https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.eventlog?view=dotnet-plat-ext-7.0)";
+
         protected override string Category => "Windows OS info/Windows Logs";
+
+        protected abstract string Status { get; }
 
 
         protected WindowsLogPrototype() : base()
         {
-            Description = "some desc";
+            Description = string.Format(BaseDescription, Status);
             IsSingletonSensor = true;
             IsComputerSensor = true;
             
@@ -21,10 +26,14 @@ namespace HSMDataCollector.Prototypes.Collections
     internal class WindowsErrorLogsPrototype : WindowsLogPrototype
     {
         protected override string SensorName => "Windows Error Logs";
+
+        protected override string Status => "Error";
     }
     
     internal class WindowsWarningLogsPrototype : WindowsLogPrototype
     {
         protected override string SensorName => "Windows Warning Logs";
+
+        protected override string Status => "Warning";
     }
 }
