a47656a82fa9c4fc85a62d19b44e403dcf8e1f79
Palina Shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2023-09-25 14:50:52 +0300

    Fixed remark

--- src/server/HSMServer/Model/NotificationViewModels/ChatsViewModel.cs
+++ src/server/HSMServer/Model/NotificationViewModels/ChatsViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using HSMServer.Authentication;
-using HSMServer.Extensions;
 using HSMServer.Notifications;
-using System;
 using System.Collections.Generic;
 
 namespace HSMServer.Model.NotificationViewModels
@@ -30,88 +28,6 @@ namespace HSMServer.Model.NotificationViewModels
 
             PrivateChats = new PrivateChatsViewModel(privates);
             GroupChats = new GroupChatsViewModel(groups);
-        }
-    }
-
-    public abstract class ChatsViewModelBase
-    {
-        public List<TelegramChatViewModel> Chats { get; }
-
-        public abstract string Title { get; }
-
-        public abstract string NameColumn { get; }
-
-
-        internal ChatsViewModelBase(List<TelegramChatViewModel> chats)
-        {
-            Chats = chats;
-        }
-    }
-
-    public sealed class PrivateChatsViewModel : ChatsViewModelBase
-    {
-        public override string Title => "Accounts";
-
-        public override string NameColumn => "Username";
-
-
-        public PrivateChatsViewModel(List<TelegramChatViewModel> chats) : base(chats) { }
-    }
-
-    public sealed class GroupChatsViewModel : ChatsViewModelBase
-    {
-        public override string Title => "Groups";
-
-        public override string NameColumn => "Group name";
-
-
-        public GroupChatsViewModel(List<TelegramChatViewModel> chats) : base(chats) { }
-    }
-
-
-    public class TelegramChatViewModel
-    {
-        public Dictionary<Guid, string> Products { get; } = new();
-
-        public Dictionary<Guid, string> Managers { get; } = new();
-
-
-        public string AuthorizationTime { get; }
-
-        public ConnectedChatType Type { get; }
-
-        public Guid SystemId { get; }
-
-        public long ChatId { get; }
-
-        public string Name { get; }
-
-
-        public TelegramChatViewModel(TelegramChat chat, TreeViewModel.TreeViewModel tree, IUserManager userManager)
-        {
-            SystemId = chat.Id;
-            ChatId = chat.ChatId.Identifier ?? 0L;
-            Name = chat.Name;
-            Type = chat.Type;
-            AuthorizationTime = chat.AuthorizationTime == DateTime.MinValue
-                ? "-"
-                : chat.AuthorizationTime.ToDefaultFormat();
-
-            Initialize(chat, tree, userManager);
-        }
-
-
-        private void Initialize(TelegramChat chat, TreeViewModel.TreeViewModel tree, IUserManager userManager)
-        {
-            foreach (var productId in chat.Products)
-                if (tree.Nodes.TryGetValue(productId, out var product))
-                    Products.Add(productId, product.Name);
-
-            // TODO : fix managers for private chats
-            foreach (var user in userManager.GetUsers())
-                foreach (var (productId, role) in user.ProductsRoles)
-                    if (Products.ContainsKey(productId) && role == Authentication.ProductRoleEnum.ProductManager && !Managers.ContainsKey(user.Id))
-                        Managers.Add(user.Id, user.Name);
         }
     }
 }
---
+++ src/server/HSMServer/Model/NotificationViewModels/TelegramChatViewModel.cs
@@ -0,0 +1,54 @@
+﻿using HSMServer.Authentication;
+using HSMServer.Extensions;
+using HSMServer.Notifications;
+using System;
+using System.Collections.Generic;
+
+namespace HSMServer.Model.NotificationViewModels
+{
+    public class TelegramChatViewModel
+    {
+        public Dictionary<Guid, string> Products { get; } = new();
+
+        public Dictionary<Guid, string> Managers { get; } = new();
+
+
+        public string AuthorizationTime { get; }
+
+        public ConnectedChatType Type { get; }
+
+        public Guid SystemId { get; }
+
+        public long ChatId { get; }
+
+        public string Name { get; }
+
+
+        public TelegramChatViewModel(TelegramChat chat, TreeViewModel.TreeViewModel tree, IUserManager userManager)
+        {
+            SystemId = chat.Id;
+            ChatId = chat.ChatId.Identifier ?? 0L;
+            Name = chat.Name;
+            Type = chat.Type;
+            AuthorizationTime = chat.AuthorizationTime == DateTime.MinValue
+                ? "-"
+                : chat.AuthorizationTime.ToDefaultFormat();
+
+            Initialize(chat, tree, userManager);
+        }
+
+
+        private void Initialize(TelegramChat chat, TreeViewModel.TreeViewModel tree, IUserManager userManager)
+        {
+            foreach (var productId in chat.Products)
+                if (tree.Nodes.TryGetValue(productId, out var product))
+                    Products.Add(productId, product.Name);
+
+            // TODO : fix managers for private chats
+            foreach (var user in userManager.GetUsers())
+                foreach (var (productId, role) in user.ProductsRoles)
+                    if (Products.ContainsKey(productId) && role == Authentication.ProductRoleEnum.ProductManager && !Managers.ContainsKey(user.Id))
+                        Managers.Add(user.Id, user.Name);
+        }
+    }
+}
