b3a4d10ab1bd138c0fd50ccb0eb978b8c4d5a6ca
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2023-10-13 16:24:49 +0300

    TAM-1286: Enum as strings have been added for Swagger

--- src/server/HSMServer/Program.cs
+++ src/server/HSMServer/Program.cs
@@ -64,6 +64,7 @@ builder.Services.AddMvc()
                 .AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.NumberHandling = JsonNumberHandling.AllowReadingFromString | JsonNumberHandling.AllowNamedFloatingPointLiterals;
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
                 });
 
 builder.Services.AddFluentValidationAutoValidation()
