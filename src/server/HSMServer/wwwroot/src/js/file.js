8da71e06040028a1b800083c51ef36ca9dc0aa29
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-10-20 19:09:45 +0300

    Fixed file preview + refactoring.

--- src/server/HSMServer/wwwroot/src/js/file.js
+++ src/server/HSMServer/wwwroot/src/js/file.js
@@ -38,6 +38,57 @@ window.openFileInBrowser = function (path, fileName, viewFileAction, time = unde
     });
 }
 
+window.previewFile = function(url, id, extension, time = null, fileNumber = null){
+    let fileId = '';
+    if (time === null)
+        url = `${url}?Selected=${id}`;
+    else
+    {
+        url = `${url}?Selected=${fileNumber}&datetime=${time}`
+        fileId = `_${id}`;
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: url,
+        cache: false,
+        contentType: "application/json",
+        success: function (file) {
+            let dataSet = [];
+            if (extension === 'csv') {
+                file.split('\n').forEach(el => {
+                    let splitted = el.split(',');
+                    let isEmpty = splitted.some(x => x !== '');
+
+                    if (isEmpty) {
+                        dataSet.push(splitted)
+                    }
+                });
+
+                let columns = [];
+
+                dataSet[0].forEach(el => {
+                    columns.push({ "title": el })
+                });
+
+                $(`#preview${fileId}`).removeClass('d-none container');
+                $(`#preview-content${fileId}`).html('<table class="display w-100" id="' + `example${fileId}"` + '></table>');
+
+                $(`#example${fileId}`).dataTable({
+                    "data": dataSet.splice(1),
+                    "columns": columns,
+                    "lengthMenu": [[5, 20, 50, 100], [5, 20, 50, 100]],
+                    "pageLength": 20
+                });
+            }
+            else if (extension === 'txt') {
+                $(`#preview${fileId}`).removeClass('d-none container');
+                $(`#preview-content_${fileId}`).addClass('w-100 mw-100 text-break').html(file);
+            }
+        }
+    })
+}
+
 //files functionality
 function getMimeType(fileName) {
     let extension = getExtensionFromName(fileName);
