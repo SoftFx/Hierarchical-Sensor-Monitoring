fa4968297f0cb2e6841e8b2cccc798115feb6674
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-10-25 15:14:15 +0300

    Code cleanup.

--- src/server/HSMServer/Model/History/Personal/HistoryProcessor/Processor/BarHistoryProcessor.cs
+++ src/server/HSMServer/Model/History/Personal/HistoryProcessor/Processor/BarHistoryProcessor.cs
@@ -33,15 +33,15 @@ namespace HSMServer.Model.History
         {
             if (values == null || values.Count == 0)
                 return new();
-        
+
             var result = new List<BaseValue>();
-        
+
             var oldestValue = values.First() as BarBaseValue<T>;
             DateTime nextBarTime = oldestValue.OpenTime + compressionInterval;
-        
+
             SummaryBarItem<T> summary = new(oldestValue.OpenTime, oldestValue.CloseTime, DefaultMax, DefaultMin, oldestValue.FirstValue ?? oldestValue.Min, oldestValue.LastValue);
             ProcessItem(oldestValue, summary);
-        
+
             for (int i = 1; i < values.Count; ++i)
             {
                 if (values[i] is not BarBaseValue<T> value || value.CloseTime == DateTime.MinValue)
@@ -50,19 +50,19 @@ namespace HSMServer.Model.History
                 if (summary.CloseTime + (value.CloseTime - value.OpenTime) > nextBarTime)
                 {
                     result.Add(Convert(summary, summary.Count != value.Count));
-        
+
                     summary = new(value.OpenTime, value.CloseTime, DefaultMax, DefaultMin, oldestValue.FirstValue ?? oldestValue.Min, oldestValue.LastValue);
                     ProcessItem(value, summary);
-        
+
                     while (nextBarTime <= summary.CloseTime)
                         nextBarTime += compressionInterval;
                 }
                 else
                     ProcessItem(value, summary);
             }
-        
+
             result.Add(Convert(summary, (values[^1] as BarBaseValue).Count != summary.Count));
-        
+
             return result;
         }
 
