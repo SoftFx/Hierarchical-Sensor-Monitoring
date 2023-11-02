cd60eec5e0a151928f230d3c5eccd6c2c8f54070
Palina Shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2023-11-02 11:31:10 +0200

    Added resizeable and draggable blocks in dashboard

--- src/server/HSMServer/wwwroot/src/js/dashboard.js
+++ src/server/HSMServer/wwwroot/src/js/dashboard.js
@@ -120,6 +120,66 @@ export function initDropzone(){
                 end: showEventInfo
             }
         })
+
+    window.interact('.resize-draggable')
+        .draggable({
+            inertia: true,
+            modifiers: [
+                interact.modifiers.restrictRect({
+                    restriction: 'parent',
+                    endOnly: true
+                })
+            ],
+            autoScroll: true,
+
+            listeners: {
+                move: dragMoveListener,
+
+                end(event) {
+                    var textEl = event.target.querySelector('p')
+
+                    textEl && (textEl.textContent =
+                        'moved a distance of ' +
+                        (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                            Math.pow(event.pageY - event.y0, 2) | 0))
+                            .toFixed(2) + 'px')
+                }
+            }
+        })
+        .resizable({
+            edges: { left: true, right: true, bottom: true, top: true },
+
+            listeners: {
+                move(event) {
+                    var target = event.target
+                    var x = (parseFloat(target.getAttribute('data-x')) || 0)
+                    var y = (parseFloat(target.getAttribute('data-y')) || 0)
+
+                    target.style.width = event.rect.width + 'px'
+                    target.style.height = event.rect.height + 'px'
+
+                    x += event.deltaRect.left
+                    y += event.deltaRect.top
+
+                    target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+                    target.setAttribute('data-x', x)
+                    target.setAttribute('data-y', y)
+                    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height)
+                }
+            },
+            modifiers: [
+                interact.modifiers.restrictEdges({
+                    outer: 'parent'
+                }),
+
+                interact.modifiers.restrictSize({
+                    min: { width: 100, height: 50 }
+                })
+            ],
+
+            inertia: true
+        })
 }
 
 window.updateColor = function (color, id) {
