76490758746fff9fde2532be25f82042c2cec548
Saveli.Valadzkou <saveli.valadzkou@soft-fx.lv>
2023-11-10 17:24:43 +0300

    Now panels can be dragged and resized only in edit mode.

--- src/server/HSMServer/wwwroot/src/js/dashboard.js
+++ src/server/HSMServer/wwwroot/src/js/dashboard.js
@@ -134,72 +134,90 @@ export function initDropzone(){
 }
 
 window.initDashboard = function () {
-    window.interact('.resize-draggable')
-        .draggable({
-            inertia: true,
-            modifiers: [
-                interact.modifiers.restrictRect({
-                    restriction: 'parent',
-                    endOnly: true
-                })
-            ],
-            autoScroll: true,
+    const interact = window.interact('.resize-draggable')
+    addDraggable(interact)
+    addResizable(interact)
+}
 
-            listeners: {
-                move: dragMoveListener,
+window.disableDragAndResize = function () {
+    interact('.resize-draggable').options.resize.enabled = false;
+    interact('.resize-draggable').options.drag.enabled = false;
+}
 
-                end(event) {
-                    var textEl = event.target.querySelector('p')
+window.enableDragAndResize = function () {
+    interact('.resize-draggable').options.resize.enabled = true;
+    interact('.resize-draggable').options.drag.enabled = true;
+}
 
-                    //textEl && (textEl.textContent =
-                    //    'moved a distance of ' +
-                    //    (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
-                    //        Math.pow(event.pageY - event.y0, 2) | 0))
-                    //        .toFixed(2) + 'px')
-                }
+function addDraggable(interactable) {
+    interactable.draggable({
+        inertia: true,
+        modifiers: [
+            interact.modifiers.restrictRect({
+                restriction: 'parent',
+                endOnly: true
+            })
+        ],
+        autoScroll: true,
+
+        listeners: {
+            move: dragMoveListener,
+
+            end(event) {
+                var textEl = event.target.querySelector('p')
+
+                //textEl && (textEl.textContent =
+                //    'moved a distance of ' +
+                //    (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                //        Math.pow(event.pageY - event.y0, 2) | 0))
+                //        .toFixed(2) + 'px')
             }
-        })
-        .resizable({
-            edges: { left: true, right: true, bottom: true, top: true },
+        }
+    })
+}
 
-            listeners: {
-                move(event) {
-                    var target = event.target
-                    var x = (parseFloat(target.getAttribute('data-x')) || 0)
-                    var y = (parseFloat(target.getAttribute('data-y')) || 0)
+function addResizable(interactable){
+    interactable.resizable({
+        edges: { left: true, right: true, bottom: true, top: true },
 
-                    target.style.width = event.rect.width + 'px'
-                    target.style.height = event.rect.height + 'px'
+        listeners: {
+            move(event) {
+                var target = event.target
+                var x = (parseFloat(target.getAttribute('data-x')) || 0)
+                var y = (parseFloat(target.getAttribute('data-y')) || 0)
 
-                    x += event.deltaRect.left
-                    y += event.deltaRect.top
+                target.style.width = event.rect.width + 'px'
+                target.style.height = event.rect.height + 'px'
 
-                    target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+                x += event.deltaRect.left
+                y += event.deltaRect.top
 
-                    target.setAttribute('data-x', x)
-                    target.setAttribute('data-y', y)
+                target.style.transform = 'translate(' + x + 'px,' + y + 'px)'
+
+                target.setAttribute('data-x', x)
+                target.setAttribute('data-y', y)
 
 
-                    var update = {
-                        width: event.rect.width,
-                        height: event.rect.height
-                    };
+                var update = {
+                    width: event.rect.width,
+                    height: event.rect.height
+                };
 
-                    Plotly.relayout(`panelChart_${event.target.id}`, update);
-                }
-            },
-            modifiers: [
-                interact.modifiers.restrictEdges({
-                    outer: 'parent'
-                }),
+                Plotly.relayout(`panelChart_${event.target.id}`, update);
+            }
+        },
+        modifiers: [
+            interact.modifiers.restrictEdges({
+                outer: 'parent'
+            }),
 
-                interact.modifiers.restrictSize({
-                    min: { width: 100, height: 50 }
-                })
-            ],
+            interact.modifiers.restrictSize({
+                min: { width: 100, height: 50 }
+            })
+        ],
 
-            inertia: true
-        })
+        inertia: true
+    })
 }
 
 window.updateSource = function (name, color, id){
