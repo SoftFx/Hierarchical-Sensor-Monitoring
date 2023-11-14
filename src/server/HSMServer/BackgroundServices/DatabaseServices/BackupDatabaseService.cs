a8b24eecfde820b33912e64c71be8aad95bf45fc
Palina Shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2023-11-14 11:52:04 +0200

    Added logic of copying files from EnvironmentDate to DatabasesBackups

--- src/server/HSMServer/BackgroundServices/DatabaseServices/BackupDatabaseService.cs
+++ src/server/HSMServer/BackgroundServices/DatabaseServices/BackupDatabaseService.cs
@@ -1,23 +1,57 @@
-﻿using HSMServer.ServerConfiguration;
+﻿using HSMDatabase.AccessManager;
+using HSMDatabase.Settings;
+using HSMServer.Extensions;
+using HSMServer.ServerConfiguration;
 using System;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace HSMServer.BackgroundServices.DatabaseServices
+namespace HSMServer.BackgroundServices
 {
     public class BackupDatabaseService : BaseDelayedBackgroundService
     {
+        private readonly IDatabaseSettings _dbSettings = new DatabaseSettings();
+
+
         public override TimeSpan Delay { get; }
 
 
         public BackupDatabaseService(IServerConfig config)
         {
-            Delay = TimeSpan.FromHours(config.BackupDatabase.PeriodHours);
+            Delay = TimeSpan.FromMinutes(config.BackupDatabase.PeriodHours); // TODO: change to fromhours
         }
 
 
-        protected override Task ServiceAction()
+        protected override async Task ServiceAction()
         {
-            return Task.CompletedTask;
+            try
+            {
+                var backupPath = Path.Combine(_dbSettings.DatabaseBackupsFolder, $"{_dbSettings.EnvironmentDatabaseName}_{DateTime.UtcNow.ToWindowsFormat()}");
+
+                var environmentDb = new DirectoryInfo(_dbSettings.PathToEnvironmentDb);
+                var backupDb = Directory.CreateDirectory(backupPath);
+
+                foreach (var fileInfo in environmentDb.GetFiles())
+                {
+                    try
+                    {
+                        using var file = File.Open(fileInfo.FullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                        using var reader = new StreamReader(file);
+                        using var writer = new StreamWriter(Path.Combine(backupDb.FullName, fileInfo.Name));
+
+                        await writer.WriteAsync(await reader.ReadToEndAsync());
+                    }
+                    catch(Exception ex)
+                    {
+                        _logger.Error($"Copying files from Environment database error: {ex}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Environment database backup error: {ex}");
+            }
         }
     }
 }
