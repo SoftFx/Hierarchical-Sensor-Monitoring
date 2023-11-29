1b77d9bfd4150736daea9ae8b49dac0d2fc5b8c1
Savely.Volodkov <savely.volodkov@gmail.com>
2023-11-29 14:08:09 +0300

    Refactoring.

---
+++ src/server/HSMServer/Model/Dashboards/DatasourceViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HSMServer.Core.Model;
+using HSMServer.Dashboards;
+using HSMServer.DTOs.SensorInfo;
+using HSMServer.Extensions;
+
+namespace HSMServer.Model.Dashboards;
+
+public class DatasourceViewModel
+{
+    public Guid Id { get; set; }
+
+    public Guid SensorId { get; set; }
+
+    public SensorType Type { get; set; }
+
+    public Unit? Unit { get; set; }
+
+    public string Color { get; set; }
+
+    public string Path { get; set; }
+
+    public string Label { get; set; }
+
+    public List<object> Values { get; set; }
+
+    public SensorInfoDto SensorInfo { get; set; }
+
+
+    public DatasourceViewModel() { }
+
+    public DatasourceViewModel(PanelDatasource dataSource)
+    {
+        var result = dataSource.Source.Initialize().GetAwaiter().GetResult();
+
+        Id = dataSource.Id;
+        SensorId = dataSource.SensorId;
+        Color = dataSource.Color.ToRGB();
+        Label = dataSource.Label;
+        (Path, Type, Unit) = dataSource.Source.GetSourceInfo();
+        Values = result.Values.Cast<object>().ToList();
+        SensorInfo = new SensorInfoDto(Type, Type, Unit?.ToString());
+    }
+}
--- src/server/HSMServer/Model/Dashboards/PanelViewModel.cs
+++ src/server/HSMServer/Model/Dashboards/PanelViewModel.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using HSMServer.Core.Model;
 using HSMServer.Model.TreeViewModel;
 using HSMCommon.Collections;
 using HSMDatabase.AccessManager.DatabaseEntities.VisualEntity;
 using HSMServer.Dashboards;
-using HSMServer.DTOs.SensorInfo;
-using HSMServer.Extensions;
 
 namespace HSMServer.Model.Dashboards;
 
@@ -95,42 +92,4 @@ public class PanelViewModel
                                                  type != Core.Model.SensorType.IntegerBar &&
                                                  type != Core.Model.SensorType.Boolean &&
                                                  type != Core.Model.SensorType.Version;
-}
-
-public class DatasourceViewModel
-{
-    public Guid Id { get; set; }
-
-    public Guid SensorId { get; set; }
-
-    public SensorType Type { get; set; }
-
-    public Unit? Unit { get; set; }
-
-    public string Color { get; set; }
-
-    public string Path { get; set; }
-
-    public string Label { get; set; }
-
-    public List<object> Values { get; set; }
-
-    public SensorInfoDto SensorInfo { get; set; }
-
-
-    public DatasourceViewModel() { }
-
-    public DatasourceViewModel(PanelDatasource dataSource)
-    {
-        var (from, to) = dataSource.GetFromTo();
-        var result = dataSource.Source.Initialize(from ,to).GetAwaiter().GetResult();
-
-        Id = dataSource.Id;
-        SensorId = dataSource.SensorId;
-        Color = dataSource.Color.ToRGB();
-        Label = dataSource.Label;
-        (Path, Type, Unit) = dataSource.Source.GetSourceInfo();
-        Values = result.Values.Cast<object>().ToList();
-        SensorInfo = new SensorInfoDto(Type, Type, Unit?.ToString());
-    }
 }
