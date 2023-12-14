f714bdc8b575da6265c5ada4f4698f72cfd1c04c
andrei.khloptsau.vs <andrei.khloptsau@soft-fx.lv>
2023-12-14 18:03:07 +0200

    TAM-1478: Some factory methods has been added

--- src/server/HSMServer/Datasources/DatasourceFactory.cs
+++ src/server/HSMServer/Datasources/DatasourceFactory.cs
@@ -1,32 +1,67 @@
 ﻿using HSMServer.Core.Model;
 using HSMServer.Dashboards;
 using System;
+using System.Numerics;
 
 namespace HSMServer.Datasources
 {
     public static class DatasourceFactory
     {
-        public static SensorDatasourceBase Build(SensorType type, PlottedProperty property) => type switch
+        public static SensorDatasourceBase Build(BaseSensorModel sensor, PlottedProperty property)
         {
-            SensorType.Integer when property.IsLineProperty() => new LineDatasource<int>(),
+            SensorDatasourceBase source = sensor.Type switch
+            {
+                SensorType.Integer when property.IsInstantView() => new LineDatasource<int>(),
 
-            SensorType.Double when property.IsLineProperty() => new LineDatasource<double>(),
+                SensorType.Double when property.IsInstantView() => new LineDatasource<double>(),
 
-            SensorType.TimeSpan when property.IsLineProperty() => new TimespanDatasource(),
+                SensorType.TimeSpan when property.IsInstantView() => new TimespanDatasource(),
 
-            SensorType.IntegerBar when property.IsLineProperty() => new LineDatasource<int>(),
-            SensorType.IntegerBar when property is PlottedProperty.Bar => new BarsDatasource(),
+                SensorType.IntegerBar when property.IsBarView() => new BarsDatasource(),
+                SensorType.IntegerBar when property.IsBarLine() => new LineDatasource<int>(),
+                SensorType.IntegerBar when property.IsBarIntLine() => new LineDatasource<int>(),
 
-            SensorType.DoubleBar when property.IsLineProperty() => new LineDatasource<double>(),
-            SensorType.DoubleBar when property is PlottedProperty.Bar => new BarsDatasource(),
+                SensorType.DoubleBar when property.IsBarView() => new BarsDatasource(),
+                SensorType.DoubleBar when property.IsBarLine() => new LineDatasource<double>(),
+                SensorType.DoubleBar when property.IsBarIntLine() => new LineDatasource<int>(),
 
-            SensorType.Boolean => new PointDatasource(),
+                SensorType.Boolean => new PointDatasource(),
 
-            _ => throw new Exception($"History visualization for {type} sensor is not supported")
-        };
+                _ => throw new Exception($"History visualization for {sensor.Type} sensor is not supported")
+            };
+
+            return source.AttachSensor(sensor, property);
+        }
 
 
-        private static bool IsLineProperty(this PlottedProperty property) =>
-            property is PlottedProperty.Value or PlottedProperty.Min or PlottedProperty.Mean or PlottedProperty.Max or PlottedProperty.Count;
+        public static Func<BaseValue, T> GetNumberValueFactory<T>(PlottedProperty property) where T : struct, INumber<T> =>
+            property switch
+            {
+                PlottedProperty.Value => v => ((BaseValue<T>)v).Value,
+                PlottedProperty.Min => v => ((BarBaseValue<T>)v).Min,
+                PlottedProperty.Max => v => ((BarBaseValue<T>)v).Max,
+                PlottedProperty.Mean => v => ((BarBaseValue<T>)v).Mean,
+                _ => throw BuildException<T>(property)
+            };
+
+
+        public static Func<BaseValue, int> GetIntValueFactory<T>(PlottedProperty property) where T : struct, INumber<T> =>
+            property switch
+            {
+                PlottedProperty.Count => v => ((BarBaseValue<T>)v).Count
+            };
+
+
+        private static bool IsInstantView(this PlottedProperty property) => property is PlottedProperty.Value;
+
+
+        private static bool IsBarView(this PlottedProperty property) => property is PlottedProperty.Bar;
+
+        private static bool IsBarLine(this PlottedProperty property) => property is PlottedProperty.Min or PlottedProperty.Mean or PlottedProperty.Max;
+
+        private static bool IsBarIntLine(this PlottedProperty property) => property is PlottedProperty.Count;
+
+
+        private static Exception BuildException<T>(PlottedProperty property) => new Exception($"Invalid property {property} for {typeof(T).FullName}");
     }
 }
