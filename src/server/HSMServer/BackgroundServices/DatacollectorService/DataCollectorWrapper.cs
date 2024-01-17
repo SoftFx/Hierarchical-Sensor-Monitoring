c7a7e9f9acd91b6d278489794b79b541e8d60196
Palina Shymanouskaya <palina.shymanouskaya@soft-fx.lv>
2024-01-17 14:20:52 +0200

    TAM-1546. HSM, Self monitoring: All db sensors should be decorated

--- src/server/HSMServer/BackgroundServices/DatacollectorService/DataCollectorWrapper.cs
+++ src/server/HSMServer/BackgroundServices/DatacollectorService/DataCollectorWrapper.cs
@@ -18,8 +18,6 @@ namespace HSMServer.BackgroundServices
     {
         private const int DigitsCnt = 2;
 
-        private const double MbDivisor = 1 << 20;
-
         private const string SelfMonitoringProductName = "HSM Server Monitoring";
 
         private const string RequestsCountPath = "Load/Requests per second";
@@ -28,19 +26,12 @@ namespace HSMServer.BackgroundServices
         private const string ResponseSizePath = "Load/Sent data per second KB";
         private const string RequestSizePath = "Load/Received data per second KB";
 
-        private const string EnvironmentDbSizePath = "Database/Environment data size MB";
-        private const string SnaphotsDbSizePath = "Database/Snapshots data size MB";
-        private const string HistoryDbSizePath = "Database/Monitoring data size MB";
-        private const string TotalDbSizePath = "Database/All database size MB";
-
-        private readonly IInstantValueSensor<double> _environmentDbSizeSensor;
-        private readonly IInstantValueSensor<double> _snapshotsDbSizeSensor;
-        private readonly IInstantValueSensor<double> _historyDbSizeSensor;
-        private readonly IInstantValueSensor<double> _dbSizeSensor;
         private readonly IDataCollector _collector;
-        private readonly IDatabaseCore _database;
 
         private readonly TimeSpan _barInterval = new(0, 1, 0);
+
+
+        internal DatabaseSize DbSizeSensors { get; }
 
 
         internal IParamsFuncSensor<double, double> RequestSizeSensor { get; }
@@ -54,8 +45,6 @@ namespace HSMServer.BackgroundServices
 
         public DataCollectorWrapper(IDatabaseCore database, ITreeValuesCache cache)
         {
-            _database = database;
-
             var productVersion = Assembly.GetEntryAssembly()?.GetName().GetVersion();
             var loggerOptions = new LoggerOptions()
             {
@@ -75,10 +64,7 @@ namespace HSMServer.BackgroundServices
             ReceivedDataCountSensor = RegisterParamSensor<double>(DataCountPath);
             RequestsCountSensor = RegisterParamSensor<double>(RequestsCountPath);
 
-            _dbSizeSensor = _collector.CreateDoubleSensor(TotalDbSizePath);
-            _environmentDbSizeSensor = _collector.CreateDoubleSensor(EnvironmentDbSizePath);
-            _snapshotsDbSizeSensor = _collector.CreateDoubleSensor(SnaphotsDbSizePath);
-            _historyDbSizeSensor = _collector.CreateDoubleSensor(HistoryDbSizePath);
+            DbSizeSensors = new DatabaseSize(_collector, database);
         }
 
 
@@ -89,18 +75,7 @@ namespace HSMServer.BackgroundServices
         internal Task Stop() => _collector.Stop();
 
 
-        internal void SendDbInfo()
-        {
-            static double GetRoundedDouble(long sizeInBytes)
-            {
-                return Math.Round(sizeInBytes / MbDivisor, DigitsCnt, MidpointRounding.AwayFromZero);
-            }
-
-            _dbSizeSensor.AddValue(GetRoundedDouble(_database.TotalDbSize));
-            _environmentDbSizeSensor.AddValue(GetRoundedDouble(_database.EnviromentDbSize));
-            _historyDbSizeSensor.AddValue(GetRoundedDouble(_database.SensorHistoryDbSize));
-            _snapshotsDbSizeSensor.AddValue(GetRoundedDouble(_database.Snapshots.Size));
-        }
+        internal void SendDbInfo() => DbSizeSensors.SendInfo();
 
 
         private static string GetSelfMonitoringKey(ITreeValuesCache cache)
---
+++ src/server/HSMServer/BackgroundServices/DatacollectorService/Nodes/DatabaseSize.cs
@@ -0,0 +1,82 @@
+﻿using HSMDataCollector.Core;
+using HSMDataCollector.Options;
+using HSMDataCollector.PublicInterface;
+using HSMServer.Core.DataLayer;
+using System;
+using System.Collections.Generic;
+
+namespace HSMServer.BackgroundServices
+{
+    internal sealed class DatabaseSize
+    {
+        private const double MbDivisor = 1 << 20;
+        private const int DigitsCnt = 2;
+
+        private const string EnvironmentDbSizePath = "Environment data size";
+        private const string HistoryDbSizePath = "SensorValues data size";
+        private const string SnaphotsDbSizePath = "Snapshots data size";
+        private const string TotalDbSizePath = "All database size";
+
+
+        private readonly IDataCollector _collector;
+        private readonly IDatabaseCore _database;
+
+        private readonly IInstantValueSensor<double> _environmentDbSizeSensor;
+        private readonly IInstantValueSensor<double> _snapshotsDbSizeSensor;
+        private readonly IInstantValueSensor<double> _historyDbSizeSensor;
+        private readonly IInstantValueSensor<double> _dbSizeSensor;
+
+
+        private readonly Dictionary<string, string> Descriptions = new()
+        {
+            { EnvironmentDbSizePath, "The database contains all server entities meta information (folders, products, sensors, users, etc.)." },
+            { SnaphotsDbSizePath, "The database contains current state of tree (last update time of sensors, timeouts, etc.)." },
+            { HistoryDbSizePath, "The database contains sensors history divided into weekly folders." },
+            { TotalDbSizePath, "All database size is the sum of the sizes of Environment, SensorValues, Snapshots, ServerLayouts and Journal databases." },
+        };
+
+
+        internal DatabaseSize(IDataCollector collector, IDatabaseCore database)
+        {
+            _collector = collector;
+            _database = database;
+
+            _environmentDbSizeSensor = CreateDataSizeSensor(EnvironmentDbSizePath);
+            _snapshotsDbSizeSensor = CreateDataSizeSensor(SnaphotsDbSizePath);
+            _historyDbSizeSensor = CreateDataSizeSensor(HistoryDbSizePath);
+            _dbSizeSensor = CreateDataSizeSensor(TotalDbSizePath);
+        }
+
+
+        internal void SendInfo()
+        {
+            static double GetRoundedDouble(long sizeInBytes)
+            {
+                return Math.Round(sizeInBytes / MbDivisor, DigitsCnt, MidpointRounding.AwayFromZero);
+            }
+
+            _environmentDbSizeSensor.AddValue(GetRoundedDouble(_database.EnviromentDbSize));
+            _historyDbSizeSensor.AddValue(GetRoundedDouble(_database.SensorHistoryDbSize));
+            _snapshotsDbSizeSensor.AddValue(GetRoundedDouble(_database.Snapshots.Size));
+            _dbSizeSensor.AddValue(GetRoundedDouble(_database.TotalDbSize));
+        }
+
+        private IInstantValueSensor<double> CreateDataSizeSensor(string sensorName)
+        {
+            var databaseName = sensorName[..sensorName.IndexOf(' ')];
+            var databaseDescription = Descriptions[sensorName];
+
+            var options = new InstantSensorOptions
+            {
+                Alerts = [],
+                TTL = TimeSpan.MaxValue,
+                EnableForGrafana = true,
+                SensorUnit = HSMSensorDataObjects.SensorRequests.Unit.MB,
+
+                Description = $"The sensor sends information about {databaseName} database size. {databaseDescription}"
+            };
+
+            return _collector.CreateDoubleSensor($"Database/{sensorName}", options);
+        }
+    }
+}
