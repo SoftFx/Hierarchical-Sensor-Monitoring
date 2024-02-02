55f9b91992a93f0897229fbc4a237e8d7aab6be0
Savely.Volodkov <savely.volodkov@gmail.com>
2024-02-02 19:00:15 +0300

    Fixed blinking and returning to top in search tree.

--- src/server/HSMServer/Model/TreeViewModels/SearchPattern.cs
+++ src/server/HSMServer/Model/TreeViewModels/SearchPattern.cs
@@ -6,17 +6,19 @@ public record SearchPattern(string SearchParameter = "", bool IsSearchRefresh = 
 {
     private string _searchParameter;
     private bool _isMatchWord;
+    private bool _isSearchRefresh = IsSearchRefresh;
     
     
     public bool IsSearch => !string.IsNullOrEmpty(_searchParameter);
     
-    public bool ShouldClearOpenedNodes => !IsSearchRefresh && IsSearch;
+    public bool ShouldClearOpenedNodes => !_isSearchRefresh && IsSearch;
     
     
     public bool IsNameFits(string name) => _isMatchWord ? name.Equals(_searchParameter) : name.Contains(_searchParameter, StringComparison.OrdinalIgnoreCase);
 
     public void Recalculate(SearchPattern pattern)
     {
+        _isSearchRefresh = pattern.IsSearchRefresh;
         _isMatchWord = pattern.SearchParameter is not null && pattern.SearchParameter.Length >= 2 && pattern.SearchParameter.StartsWith('"') && pattern.SearchParameter.EndsWith('"');
         _searchParameter = _isMatchWord ? pattern.SearchParameter[1..^1] : pattern.SearchParameter;
     }
