d7b3b28129c824245740a6d0a106961f56670279
andrei.khloptsau.vs <andrei.khloptsau@soft-fx.lv>
2024-02-14 17:26:11 +0200

    Stat calculation has been fixed?

--- src/server/HSMServer/Dashboards/Panels/PanelSensorScanTask.cs
+++ src/server/HSMServer/Dashboards/Panels/PanelSensorScanTask.cs
@@ -5,14 +5,14 @@ using System.Threading.Tasks;
 
 namespace HSMServer.Dashboards
 {
-    public record SensorScanResult(List<string> MathedSensors, long TotalScanned, long TotalMatched, bool IsFinish);
+    public record SensorScanResult(List<string> MatсhedSensors, long TotalScanned, long TotalMatched, bool IsFinish);
 
 
     public sealed class PanelSensorScanTask : TaskCompletionSource
     {
         private const int BatchSize = 10;//50;
 
-        private readonly List<string> _mathedSensorsPaths = new(1 << 5);
+        private readonly List<string> _matсhedSensorsPaths = new(1 << 5);
 
         private readonly CancellationTokenSource _tokenSource = new();
         private long _totalScannedSensors, _totalMatchedSensors;
@@ -23,47 +23,28 @@ namespace HSMServer.Dashboards
 
         public async Task StartScanning(IEnumerable<BaseSensorModel> sensors, PanelSubscription subscription)
         {
-            var currentMatch = 0L;
-            var currentScan = 0L;
-
-            void FlushResults()
-            {
-                Interlocked.Add(ref _totalScannedSensors, currentScan);
-                Interlocked.Add(ref _totalMatchedSensors, currentMatch);
-
-                currentScan = 0;
-                currentMatch = 0;
-            }
-
             foreach (var sensor in sensors)
             {
                 if (_tokenSource.IsCancellationRequested)
                     break;
-                await Task.Delay(500);
-                if (++currentScan % BatchSize == 0)
-                {
-                    FlushResults();
 
+                await Task.Delay(500);
+
+                if (Interlocked.Increment(ref _totalScannedSensors) % BatchSize == 0)
                     await Task.Yield();
-                }
 
                 if (subscription.IsMatch(sensor.FullPath))
-                {
-                    _mathedSensorsPaths.Add(sensor.FullPath);
-                    currentMatch++;
-                }
+                    _matсhedSensorsPaths.Add(sensor.FullPath);
             }
-
-            FlushResults();
 
             IsFinish = true;
         }
 
         public SensorScanResult GetResult()
         {
-            var result = new SensorScanResult([.. _mathedSensorsPaths], Interlocked.Read(ref _totalScannedSensors), Interlocked.Read(ref _totalMatchedSensors), IsFinish);
+            var result = new SensorScanResult([.. _matсhedSensorsPaths], Interlocked.Read(ref _totalScannedSensors), _matсhedSensorsPaths.Count, IsFinish);
 
-            _mathedSensorsPaths.Clear();
+            _matсhedSensorsPaths.Clear();
 
             return result;
         }
