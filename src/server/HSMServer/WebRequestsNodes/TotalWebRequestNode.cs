dc04726a17ed13f14ce0bbda3d323476d6c381d2
pawgyrocopter <savely.volodkov@gmail.com>
2024-03-27 13:09:07 +0300

    Refactoring.

--- src/server/HSMServer/WebRequestsNodes/TotalWebRequestNode.cs
+++ src/server/HSMServer/WebRequestsNodes/TotalWebRequestNode.cs
@@ -7,18 +7,17 @@ namespace HSMServer.WebRequestsNodes;
 public record TotalWebRequestNode : WebRequestNode
 {
     private const string RequestPerSecondNode = "RPS";
-    
-    public IInstantValueSensor<double> RPS { get; init; }
+
+    private readonly IInstantValueSensor<double> _rps;
 
     public override void AddRequestData(HttpRequest request)
     {
-        RPS?.AddValue(1);
+        _rps?.AddValue(1);
         base.AddRequestData(request);
     }
 
     public TotalWebRequestNode(IDataCollector collector, string id) : base(collector, id)
     {
-        RPS = collector.CreateM1RateSensor($"{ClientNode}/{id}/{RequestPerSecondNode}",
-            "Number of requests that were receive");
+        _rps = collector.CreateM1RateSensor($"{ClientNode}/{id}/{RequestPerSecondNode}", "Number of requests that were receive");
     }
 }
--- src/server/HSMServer/WebRequestsNodes/WevRequestNode.cs
+++ src/server/HSMServer/WebRequestsNodes/WevRequestNode.cs
@@ -14,37 +14,34 @@ public record WebRequestNode
 
     private protected const string ClientNode = "Clients";
 
-    public IInstantValueSensor<double> SentBytes { get; init; }
-    
-    public IInstantValueSensor<double> ReceiveBytes { get; init; }
-    
-    public IInstantValueSensor<double> SentSensors { get; init; }
-    
-    public IInstantValueSensor<double> ReceiveSensors { get; init; }
+    private readonly IInstantValueSensor<double> _sentBytes;
+    private readonly IInstantValueSensor<double> _receiveBytes;
+    private readonly IInstantValueSensor<double> _sentSensors;
+    private readonly IInstantValueSensor<double> _receiveSensors;
 
 
     public WebRequestNode(IDataCollector collector, string id)
     {
-        SentBytes = collector.CreateM1RateSensor($"{ClientNode}/{id}/{SentBytesNode}", "Number of bytes that were sent from server to client");
-        ReceiveBytes = collector.CreateM1RateSensor($"{ClientNode}/{id}/{RecvBytesNode}", "Number of bytes that were received from client");
-        SentSensors = collector.CreateM1RateSensor($"{ClientNode}/{id}/{SentSensorsNode}", "Number of sensors that were sent from server to client");
-        ReceiveSensors = collector.CreateM1RateSensor($"{ClientNode}/{id}/{RecvSensorsNode}", "Number of sensors that were received from client");
+        _sentBytes = collector.CreateM1RateSensor($"{ClientNode}/{id}/{SentBytesNode}", "Number of bytes that were sent from server to client");
+        _receiveBytes = collector.CreateM1RateSensor($"{ClientNode}/{id}/{RecvBytesNode}", "Number of bytes that were received from client");
+        _sentSensors = collector.CreateM1RateSensor($"{ClientNode}/{id}/{SentSensorsNode}", "Number of sensors that were sent from server to client");
+        _receiveSensors = collector.CreateM1RateSensor($"{ClientNode}/{id}/{RecvSensorsNode}", "Number of sensors that were received from client");
     }
 
 
     public virtual void AddRequestData(HttpRequest request)
     {
-        ReceiveBytes.AddValue((request.ContentLength ?? 0) / KbDivisor);
+        _receiveBytes.AddValue((request.ContentLength ?? 0) / KbDivisor);
     }
 
     public void AddResponseResult(HttpResponse response)
     {
-        SentBytes.AddValue((response.ContentLength ?? 0) / KbDivisor);
+        _sentBytes.AddValue((response.ContentLength ?? 0) / KbDivisor);
     }
 
     public void AddReceiveData(int count)
     {
         if (count != 0)
-            ReceiveSensors.AddValue(count);
+            _receiveSensors.AddValue(count);
     }
 }
