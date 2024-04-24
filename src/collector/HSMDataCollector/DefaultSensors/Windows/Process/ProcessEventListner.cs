31960a6739e92f41a214a6bcb08a159307919764
siarhei.hanich <siarhei.hanich@soft-fx.lv>
2024-04-24 19:46:22 +0300

    HSM, Datacollector: Process time in GC should be migrated to net8

---
+++ src/collector/HSMDataCollector/DefaultSensors/Windows/Process/ProcessEventListner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+
+namespace HSMDataCollector.DefaultSensors.Windows.Process
+{
+    internal class ProcessEventListener : EventListener
+    {
+        public double TimeInGC { get; private set; }
+
+        protected override void OnEventSourceCreated(EventSource source)
+        {
+            if (source.Name.Equals("System.Runtime"))
+            {
+                EnableEvents(source, EventLevel.Critical, (EventKeywords)(-1), null);
+            }
+        }
+
+        private void UpdateTimeInGc(IDictionary<string, object> eventPayload)
+        {
+            if (eventPayload["Name"].ToString() != "time-in-gc")
+                return;
+            TimeInGC = (double)eventPayload["Mean"];
+        }
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData)
+        {
+            if (eventData.EventName.Equals("EventCounters"))
+            {
+                for (int i = 0; i < eventData.Payload.Count; i++)
+                {
+                    if (eventData.Payload[i] is IDictionary<string, object> eventPayload)
+                    {
+                        UpdateTimeInGc(eventPayload);
+                    }
+                }
+            }
+        }
+    }
+}
--- src/collector/HSMDataCollector/DefaultSensors/Windows/Process/WindowsProcessTimeInGC.cs
+++ src/collector/HSMDataCollector/DefaultSensors/Windows/Process/WindowsProcessTimeInGC.cs
@@ -1,7 +1,11 @@
 ﻿using HSMDataCollector.Options;
+using System.Threading.Tasks;
+using System;
+using HSMDataCollector.DefaultSensors.Windows.Process;
 
 namespace HSMDataCollector.DefaultSensors.Windows
 {
+#if !NETSTANDARD2_0
     internal sealed class WindowsProcessTimeInGC : WindowsTimeInGCBase
     {
         protected override string InstanceName => ProcessInfo.CurrentProcessName;
@@ -9,4 +13,38 @@ namespace HSMDataCollector.DefaultSensors.Windows
 
         internal WindowsProcessTimeInGC(BarSensorOptions options) : base(options) { }
     }
+#else
+    internal sealed class WindowsProcessTimeInGC : CollectableBarMonitoringSensorBase<DoubleMonitoringBar, double>
+    {
+        private ProcessEventListener _listener;
+
+        internal WindowsProcessTimeInGC(BarSensorOptions options) : base(options) { }
+
+        internal override Task<bool> Init()
+        {
+            try
+            {
+                _listener = new ProcessEventListener();
+            }
+            catch (Exception ex)
+            {
+                ThrowException(new Exception($"Error initializing performance counter: {WindowsTimeInGCBase.Category}/{WindowsTimeInGCBase.Counter} instance {ProcessInfo.CurrentProcessName}: {ex}"));
+
+                return Task.FromResult(false);
+            }
+
+            return base.Init();
+        }
+
+
+        internal override Task Stop()
+        {
+            _listener?.Dispose();
+
+            return base.Stop();
+        }
+
+        protected override double GetBarData() => _listener.TimeInGC;
+    }
+#endif
 }
