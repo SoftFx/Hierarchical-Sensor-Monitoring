69916c18b6cf8c7883dc5ef75220f32526480f72
siarhei.hanich <siarhei.hanich@soft-fx.lv>
2024-05-03 13:58:36 +0300

    after review fixes

---
+++ src/server/HSMServer/TagHelpers/IsVisibleTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using System.Text.Encodings.Web;
+
+namespace HSMServer.TagHelpers
+{
+    [HtmlTargetElement(Attributes = TagHelperName)]
+    public class IsVisibleClassTagHelper : TagHelper
+    {
+        private const string TagHelperName = "is-visible";
+
+        [HtmlAttributeName("")]
+        public bool IsVisible { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (IsVisible)
+            {
+                output.RemoveClass("d-none", HtmlEncoder.Default);
+                output.AddClass("d-flex", HtmlEncoder.Default);
+            }
+            else
+            {
+                output.RemoveClass("d-flex", HtmlEncoder.Default);
+                output.AddClass("d-none", HtmlEncoder.Default);
+            }
+        }
+    }
+}
