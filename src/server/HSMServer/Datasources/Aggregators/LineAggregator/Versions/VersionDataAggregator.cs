ea6176992a7ea845aa5aef8fb75200a225fc2e4a
andrei.khloptsau.vs <andrei.khloptsau@soft-fx.lv>
2024-05-03 17:37:01 +0300

    TAM-1785: Aggregation function for Version points has been added

--- src/server/HSMServer/Datasources/Aggregators/LineAggregator/Versions/VersionDataAggregator.cs
+++ src/server/HSMServer/Datasources/Aggregators/LineAggregator/Versions/VersionDataAggregator.cs
@@ -1,13 +1,20 @@
-﻿using HSMServer.Dashboards;
+﻿using HSMServer.Core.Model;
+using HSMServer.Dashboards;
 using System;
 
 namespace HSMServer.Datasources.Aggregators
 {
     public sealed class VersionDataAggregator : LineDataAggregator<Version, VersionChartValue, VersionPointState>
     {
-        protected override Func<VersionPointState, VersionPointState, VersionPointState> GetAggrStateFactory(PlottedProperty property) => property switch
-        {
-            _ => throw BuildNotSupportedException(property)
-        };
+        protected override VersionPointState BuildState(BaseValue rawValue) =>
+            base.BuildState(rawValue).SaveState();
+
+        protected override Func<VersionPointState, VersionPointState, VersionPointState> GetAggrStateFactory(PlottedProperty property)
+            => property switch
+            {
+                PlottedProperty.Value => VersionPointState.GetLastState,
+
+                _ => throw BuildNotSupportedException(property)
+            };
     }
 }
--- src/server/HSMServer/Datasources/Aggregators/LineAggregator/Versions/VersionPointState.cs
+++ src/server/HSMServer/Datasources/Aggregators/LineAggregator/Versions/VersionPointState.cs
@@ -1,17 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HSMServer.Datasources.Aggregators
 {
     public readonly struct VersionPointState : ILinePointState<Version>
     {
+        private HashSet<(DateTime, Version)> AggrState { get; init; } = [];
+
+
         public DateTime Time { get; init; }
 
         public Version Value { get; init; }
 
 
-        public long Count { get; init; } = 1;
-
-
         public VersionPointState() { }
+
+
+        internal VersionPointState SaveState()
+        {
+            AggrState.Add((Time, Value));
+
+            return this;
+        }
+
+
+        public static VersionPointState GetLastState(VersionPointState first, VersionPointState second)
+        {
+            var union = first.AggrState.Union(second.AggrState).ToHashSet();
+
+            var (lastUpdate, maxVersion) = union.Max();
+
+            return new()
+            {
+                AggrState = union,
+
+                Value = maxVersion,
+                Time = lastUpdate,
+            };
+        }
     }
 }
