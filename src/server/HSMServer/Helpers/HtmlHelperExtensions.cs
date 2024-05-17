316ab42b2a6bbb4c8b195f3223a05dffced33397
siarhei.hanich <siarhei.hanich@soft-fx.lv>
2024-05-17 16:39:26 +0300

    fixes

---
+++ src/server/HSMServer/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HSMServer.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static IEnumerable<SelectListItem> GetEnumSelectListWithDefaultValue<TEnum>(this IHtmlHelper htmlHelper, TEnum defaultValue)
+            where TEnum : struct
+        {
+            var selectList = htmlHelper.GetEnumSelectList<TEnum>().ToList();
+            selectList.Single(x => x.Value == $"{(int)(object)defaultValue}").Selected = true;
+            return selectList;
+        }
+    }
+}
