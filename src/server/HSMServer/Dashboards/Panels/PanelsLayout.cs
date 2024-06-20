c60a4d7e4eaa73af1f86f590b901f409d6d4617d
pawgyrocopter <savely.volodkov@gmail.com>
2024-06-20 14:30:03 +0300

    Added proper relayout for singleMode panels.
    TODO: Add check for height.

--- src/server/HSMServer/Dashboards/Panels/PanelsLayout.cs
+++ src/server/HSMServer/Dashboards/Panels/PanelsLayout.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using HSMCommon.Extensions;
 
 namespace HSMServer.Dashboards
 {
@@ -16,13 +17,28 @@ namespace HSMServer.Dashboards
         {
             try
             {
-                var panels = panelsDict.OrderBy(x => x.Value.Name?.ToLower()).ToList();
-
+                var (panels, singleModePanels) = panelsDict.OrderBy(x => x.Value.Name?.ToLower()).SplitByCondition(x => x.Value.Settings.IsSingleMode);
+                
                 var defaultRowsCount = panels.Count / panelsInRow;
                 var lastRowSize = panels.Count % panelsInRow;
 
                 Relayout(panels.Take(defaultRowsCount * panelsInRow).ToList(), panelsInRow);
-                Relayout(panels.TakeLast(lastRowSize).ToList(), lastRowSize, defaultRowsCount);
+                Relayout(panels.TakeLast(lastRowSize).ToList(), lastRowSize, defaultRowsCount); // + 1
+                
+                var rowsBefore = defaultRowsCount;
+
+                if (lastRowSize != 0)
+                    rowsBefore++;
+                
+                var defaultRowsCount2 = singleModePanels.Count / (panelsInRow * 2);
+                var lastRowSize2 = singleModePanels.Count % (panelsInRow * 2);
+                
+                Relayout(singleModePanels.Take(defaultRowsCount2 * panelsInRow * 2).ToList(), panelsInRow * 2, rowsBefore); // +1 from pred
+                
+                if (defaultRowsCount2 * panelsInRow * 2 != 0)
+                    rowsBefore++;
+                
+                Relayout(singleModePanels.TakeLast(lastRowSize2).ToList(), lastRowSize2, rowsBefore + defaultRowsCount2);
 
                 return true;
             }
@@ -30,6 +46,11 @@ namespace HSMServer.Dashboards
             {
                 return false;
             }
+        }
+
+        private static void SingleModeRelayout(List<KeyValuePair<Guid, Panel>> pairs, int panelsInRow, int rowsExist = 0)
+        {
+            
         }
 
         private static void Relayout(List<KeyValuePair<Guid, Panel>> pairs, int panelsInRow, int rowsExist = 0)
