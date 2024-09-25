35702c315829a94752afdb87e05247596b7a74ef
pawgyrocopter <savely.volodkov@gmail.com>
2024-09-25 07:32:05 +0300

    Fixed logged value.

--- src/server/HSMServer.Core/Cache/TreeValuesCache.cs
+++ src/server/HSMServer.Core/Cache/TreeValuesCache.cs
@@ -848,7 +848,7 @@ namespace HSMServer.Core.Cache
 
             if (sensor == null)
             {
-                _logger.Info($"Creating new sensor - Name = {sensor.DisplayName}, Path = {sensor.Path}, CurrentNumber of sensors in cache = {_sensors.Count}");
+                _logger.Info($"Creating new sensor - Name = {storeInfo.SensorName}, Path = {storeInfo.Path}, CurrentNumber of sensors in cache = {_sensors.Count}");
                 sensor = AddSensor(storeInfo, value.Type, parentProduct, DefaultAlertsOptions.None);
             }
             else if (sensor.State == SensorState.Blocked)
