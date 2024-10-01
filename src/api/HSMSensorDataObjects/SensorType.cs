ec62dee41642087fecc765f7530a2d8673712888
siarhei.hanich <siarhei.hanich@soft-fx.lv>
2024-10-01 14:58:14 +0300

    description fix

--- src/api/HSMSensorDataObjects/SensorType.cs
+++ src/api/HSMSensorDataObjects/SensorType.cs
@@ -62,7 +62,7 @@
         RateSensor = 9,
 
         /// <summary>
-        /// Simple sensor which collects data of Enum type in speed format and sends the collected data instantly
+        /// Simple sensor which collects data of Enum type and sends the collected data instantly
         /// </summary>
         EnumSensor = 10,
 
