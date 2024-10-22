c56204862f441258987857dcfa3f4f312e1a05bd
pawgyrocopter <savely.volodkov@gmail.com>
2024-10-22 16:39:20 +0300

    Added spinner for Restart button. Added current bot status.

--- src/server/HSMServer/Model/Configuration/TelegramSettingsViewModel.cs
+++ src/server/HSMServer/Model/Configuration/TelegramSettingsViewModel.cs
@@ -13,15 +13,19 @@ namespace HSMServer.Model.Configuration
 
         [Display(Name = "Enable messages")]
         public bool IsEnabled { get; set; }
+        
+        [Display(Name = "Current Bot Status")]
+        public string Status { get; set; }
 
 
         public TelegramSettingsViewModel() { }
 
-        public TelegramSettingsViewModel(IServerConfig config)
+        public TelegramSettingsViewModel(IServerConfig config, bool isBotRunning)
         {
             IsEnabled = config.Telegram.IsRunning;
             BotToken = config.Telegram.BotToken;
             BotName = config.Telegram.BotName;
+            Status = isBotRunning ? "Running" : "Stopped";
         }
     }
 }
