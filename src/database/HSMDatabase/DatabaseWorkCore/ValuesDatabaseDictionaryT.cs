d236fab3487b0273baa19e1efd5da3855b93f958
Siarhei Hanich <siarhei.hanich@soft-fx.lv>
2026-02-12 21:32:14 +0300

    remove lazy databases

--- src/database/HSMDatabase/DatabaseWorkCore/ValuesDatabaseDictionaryT.cs
+++ src/database/HSMDatabase/DatabaseWorkCore/ValuesDatabaseDictionaryT.cs
@@ -1,18 +1,15 @@
-﻿using HSMDatabase.AccessManager;
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Threading;
-
+using HSMDatabase.AccessManager;
 
 namespace HSMDatabase.DatabaseWorkCore
 {
     internal abstract class ValuesDatabaseDictionary<T> : IEnumerable<T> where T : IDisposable
     {
-        private readonly SortedDictionary<long, Lazy<T>> _dbs = new(Comparer<long>.Create((x, y) => y.CompareTo(x)));
+        private readonly SortedDictionary<long, T> _dbs = new(Comparer<long>.Create((x, y) => y.CompareTo(x)));
         private readonly object _lock = new();
-
 
         protected readonly IDatabaseSettings _dbSettings;
 
@@ -40,7 +37,7 @@ namespace HSMDatabase.DatabaseWorkCore
                     (var from, var to) = GetDatesFromFolderName(directory);
                     if (!_dbs.ContainsKey(from))
                     {
-                        var db = new Lazy<T>(() => CreateDb.Invoke(directory, from, to));
+                        var db = CreateDb.Invoke(directory, from, to);
                         _dbs.Add(from, db);
                     }
                 }
@@ -50,25 +47,23 @@ namespace HSMDatabase.DatabaseWorkCore
         internal T GetDatabaseByTime(long time)
         {
             var from = DateTimeMethods.GetStartOfWeekTicks(time);
-            var to   = DateTimeMethods.GetEndOfWeekTicks(time);
+            var to = DateTimeMethods.GetEndOfWeekTicks(time);
+
+            if (_dbs.TryGetValue(from, out var existingDb))
+                return existingDb;
 
             lock (_lock)
             {
-                if (!_dbs.TryGetValue(from, out var lazyDb))
+                if (!_dbs.TryGetValue(from, out var db))
                 {
-                    lazyDb = new Lazy<T>(() =>
-                        {
-                            string name = GetDbPath.Invoke(from, to);
-                            return CreateDb.Invoke(name, from, to);
-                        }, LazyThreadSafetyMode.ExecutionAndPublication);
-                    
-                    _dbs.Add(from, lazyDb);
-
+                    string name = GetDbPath.Invoke(from, to);
+                    db = CreateDb.Invoke(name, from, to);
+                    _dbs.Add(from, db);
                 }
-                return lazyDb.Value;
+
+                return db;
             }
         }
-
 
         private string[] GetSensorValuesDirectories()
         {
@@ -98,9 +93,10 @@ namespace HSMDatabase.DatabaseWorkCore
 
             return (from, to);
         }
+
         public IEnumerator<T> GetEnumerator()
         {
-            List<Lazy<T>> dbCopies;
+            List<T> dbCopies;
 
             lock (_lock)
             {
@@ -109,7 +105,7 @@ namespace HSMDatabase.DatabaseWorkCore
 
             foreach (var db in dbCopies)
             {
-                yield return db.Value;
+                yield return db;
             }
         }
 
