9c1cb6b98fa09d48569777cb0bd4a55cd069f063
saetto <saetto@gmail.com>
2026-03-11 09:59:20 +0100

    Update playwright.config.js

--- src/tests/Autotests/playwright.config.js
+++ src/tests/Autotests/playwright.config.js
@@ -1,39 +1,31 @@
-// @ts-check
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 export default defineConfig({
   testDir: '.',
   /* Run tests in files in parallel */
   testMatch: '**/*.spec.ts',
   
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  /* Fail the build on CI if you accidentally left test. only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI? 1: undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    // Явно указываем headless режим
+    // Если переменная CI существует (на GitHub), то true, иначе false (для локальных тестов)
+    headless: process.env.CI? true: false, 
+    
     // Takes URL from GitHub Actions
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'https://localhost:44333',
+    
     // Ignor HTTPErrors
     ignoreHTTPSErrors: true,
     trace: 'on-first-retry',
-    // ON GitHub always headless: true, Playwright do it themself
   },
 
   /* Configure projects for major browsers */
@@ -42,7 +34,8 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+    
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -52,16 +45,8 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+    */
+  ],
 
     /* Test against branded browsers. */
     // {
