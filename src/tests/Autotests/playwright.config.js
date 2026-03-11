e9d7b5d02743f92223d76b75675bcb41293090de
saetto <saetto@gmail.com>
2026-03-11 10:41:40 +0100

    Update playwright.config.js

--- src/tests/Autotests/playwright.config.js
+++ src/tests/Autotests/playwright.config.js
@@ -34,36 +34,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    
-    /*
-   // {
-   //   name: 'firefox',
-   //   use: { ...devices['Desktop Firefox'] },
-   // },
-
-   // {
-   //   name: 'webkit',
-   //   use: { ...devices['Desktop Safari'] },
-   // },
-   // */
-  ],
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  //],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+   ],
 });
 
